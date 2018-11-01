ed7ce99f6cbcae2bb41ac1e23128a2b3ccefdd2b
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2018-11-02 00:08:47 +0300

    feat(Layout): Drag and Drop components added to Layout container
    Drag and Drop components added to Layout container

--- src/client/containers/Layout.js
+++ src/client/containers/Layout.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
+import HTML5Backend from "react-dnd-html5-backend";
+import { DragDropContext } from "react-dnd";
 
 //import Script from 'react-load-script'
 import style from "../components/Layout/styles.scss";
@@ -9,34 +11,37 @@ import helpers from "../utils/helpers.scss";
 import { Header } from "../components/Layout";
 import Aside from "../components/Aside";
 
-const Layout = ({ children, main }) => (
-	<div style={style} className="layout">
-		<Helmet>
-			<meta
-				name="viewport"
-				content="width=device-width,initial-scale=1,shrink-to-fit=no"
-			/>
-		</Helmet>
-		<div className={layoutStyle.headerSide}>
-			<Header />
-		</div>
-		<div className={layoutStyle.bottomBorder} />
-		<div className={helpers.marginTop5}>
-			<div className={layoutStyle.aside}>
-				<Aside />
+const Layout = ({ children, main, router }) => {
+	return (
+		<div style={style} className="layout">
+			<Helmet>
+				<meta
+					name="viewport"
+					content="width=device-width,initial-scale=1,shrink-to-fit=no"
+				/>
+			</Helmet>
+			<div className={layoutStyle.headerSide}>
+				<Header />
 			</div>
-			<div className={layoutStyle.mainSide}>
-				{children}
-				{main}
+			<div className={layoutStyle.bottomBorder} />
+			<div className={helpers.marginTop5}>
+				<div className={layoutStyle.aside}>
+					<Aside router={router} />
+				</div>
+				<div className={layoutStyle.mainSide}>
+					{children}
+					{main}
+				</div>
 			</div>
+			{/* <Footer /> */}
 		</div>
-		{/* <Footer /> */}
-	</div>
-);
+	);
+};
 
 Layout.propTypes = {
 	children: PropTypes.object,
-	main: PropTypes.object
+	main: PropTypes.object,
+	router: PropTypes.object
 };
 
-export default Layout;
+export default DragDropContext(HTML5Backend)(Layout);
