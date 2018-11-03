083b38b5add0729af5eb71ff3469d954a35691a0
Vora Vor <voravor@gmail.com>
2018-11-03 18:11:28 -0400

    test: fix component tests

--- src/client/components/NavBar/NavBar.spec.js
+++ src/client/components/NavBar/NavBar.spec.js
@@ -6,40 +6,40 @@ import snapshot from 'snap-shot-it'
 import { create } from 'react-test-renderer';
 import NavBar from 'Components/NavBar'
 
-describe('NavBar Component', () => {   
-	let wrapper;
-	let wrapperInstance;
+describe('NavBar Component', () => {
+  let wrapper;
+  let wrapperInstance;
 
-	//spy(NavBar.prototype, 'handleLeftNavClick');
-	
-	beforeEach(() => {
-		wrapper = mount(
-			<NavBar 
-				items={ {} }
-		        handleClick={ () => {} }
-		        selected={ null }
-		        cta={ null }
-		        baseUrl={ null }/>);
+  //spy(NavBar.prototype, 'handleLeftNavClick');
 
-		wrapperInstance = wrapper.instance();
-		
-		wrapper.findByRef = ref => {
-	  		const refNode = wrapper.ref(ref);
-	  		return wrapper.findWhere(node => {
-	    		return node.getDOMNode() === refNode;
-	  		});
-		};
-	})
+  beforeEach(() => {
+    wrapper = mount(
+      <NavBar
+        items={ {} }
+        handleClick={ () => {} }
+        selected={ null }
+        cta={ null }
+        baseUrl={ null }/>);
 
-	describe('Rendering', () => {
-		it('matches snapshot', () => {
-			const testRenderer = 
-			create(<NavBar items={ {} }
-		        handleClick={ () => {} }
-		        selected={ null }
-		        cta={ null }
-		        baseUrl={ null }/>)
-    		snapshot(testRenderer.toJSON())
-		})
-	})
+    wrapperInstance = wrapper.instance();
+
+    wrapper.findByRef = ref => {
+        const refNode = wrapper.ref(ref);
+        return wrapper.findWhere(node => {
+          return node.getDOMNode() === refNode;
+        });
+    };
+  })
+
+  describe('Rendering', () => {
+    it('matches snapshot', () => {
+      const testRenderer =
+      create(<NavBar items={ {} }
+            handleClick={ () => {} }
+            selected={ null }
+            cta={ null }
+            baseUrl={ null }/>)
+        snapshot(testRenderer.toJSON())
+    })
+  })
 })
--- src/client/components/NavBar/index.js
+++ src/client/components/NavBar/index.js
@@ -1,35 +1,37 @@
-import React from "react";
-import { Link } from "react-router";
-
-import style from "./styles.scss";
-import { ucfirst } from "../../utils";
+import React from "react"
+import { Link } from "react-router"
+import PropTypes from 'prop-types'
+import style from "./styles.scss"
+import { ucfirst } from "../../utils"
 
 const NavBar = props => {
-	const { items, handleClick, baseUrl } = props;
+  const { items, handleClick, baseUrl } = props
 
-	return (
-		<div>
-			<nav className={style.navigation} role="navigation">
-				<div className={style.navlist}>
-					{items.map((key, idx) => {
-						const label = ucfirst(key);
-						return (
-							key != "undefined" && (
-								<Link
-									className={style.linkElement}
-									key={idx}
-									onClick={() => handleClick(key)}
-									to={`/${key}`}
-								>
-									<span>{label}</span>
-								</Link>
-							)
-						);
-					})}
-				</div>
-			</nav>
-		</div>
-	);
-};
+  return (
+    <div>
+      <nav className={style.navigation} role="navigation">
+        <div className={style.navlist}>
+          { items.length && items.map((key, idx) => {
+            const label = ucfirst(key);
+            return (
+              key != "undefined" && (
+                <Link
+                  className={style.linkElement}
+                  key={idx}
+                  onClick={() => handleClick(key)}
+                  to={`/${key}`}
+                >
+                  <span>{label}</span>
+                </Link>
+              )
+            );
+          })}
+        </div>
+      </nav>
+    </div>
+  )
+}
 
-export default NavBar;
+
+
+export default NavBar
