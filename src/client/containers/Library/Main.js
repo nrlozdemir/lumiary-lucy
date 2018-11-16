0158876901689907b11762ae2caaa34c83f76935
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2018-11-17 01:27:44 +0300

    feat(LibraryContainer): Library container has splitted 4 section after that added some styles.

--- src/client/containers/Library/Main.js
+++ src/client/containers/Library/Main.js
@@ -3,17 +3,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 // Components
-import { Pie, Line } from "react-chartjs-2";
 import "chartjs-plugin-datalabels";
-
+import Card from "../../components/Card";
+import TabBar from "../../components/TabBar";
+// Sections
+import VideoBrief from "./Sections/videoBrief";
+import VideoTabs from "./Sections/videoTabs";
+import ColorTone from "./Sections/colorTone";
+import AgeRangeAndGender from "./Sections/ageRangeAndGender";
 // Styles
 import style from "./styles.scss";
-import Card from "../../components/Card";
-import BarChart from "../../components/Charts/BarChart";
-import { pieData, lineData, lineWithCustomLabel } from "./options";
-import CustomLineGradient from "../../components/Charts/CustomLineGradient";
-import TabBar from "../../components/TabBar";
-import VideoBrief from "./Sections/videoBrief";
 
 class Library extends React.Component {
 	render() {
@@ -70,232 +69,24 @@ class Library extends React.Component {
 					<div className="col-5 mt-10">
 						<VideoBrief />
 					</div>
-					<div className="col-12 mt-10">
+					<div className="col-12 mt-10 pb-10">
 						<Card
 							title="Lumiere Data"
-							customHeaderClass="bg-tealish border-bt-dark"
-							customBodyClass="bg-dark-three box-shadow-black-1 color-white"
+							customHeaderClass="bg-charcoal-grey border-bt-dark color-white"
+							customBodyClass="bg-charcoal-grey color-white"
 						>
-							<div className="col-5 p-25 ">
-								<Pie
-									data={pieData}
-									options={{
-										responsive: true,
-										legend: {
-											position: "right",
-											labels: {
-												fontColor: "#fff"
-											}
-										},
-										plugins: {
-											datalabels: {
-												color: "white",
-												font: {
-													weight: "bold"
-												},
-												formatter: (value, ctx) => {
-													let sum = 0;
-													let dataArr = ctx.chart.data.datasets[0].data;
-													dataArr.map(data => {
-														sum += data;
-													});
-													let percentage =
-														((value * 100) / sum).toFixed(2) + "%";
-													return percentage;
-												}
-											}
-										},
-										tooltips: {
-											enabled: false
-										}
-									}}
-								/>
+							<div className="col-12 mt-25">
+								<VideoTabs />
 							</div>
-							<div className="col-7">
-								<Line
-									data={lineData}
-									options={{
-										legend: false,
-										scales: {
-											xAxes: [
-												{
-													gridLines: {
-														color: "#000",
-														borderDash: [5, 10.15]
-													}
-												}
-											],
-											yAxes: [
-												{
-													id: "right-y-axis",
-													type: "linear",
-													position: "right",
-													gridLines: {
-														display: false
-													}
-												}
-											]
-										},
-										ticks: {
-											display: false
-										},
-										layout: {
-											padding: {
-												left: 50,
-												right: 50,
-												bottom: 50,
-												top: 50
-											}
-										},
-
-										plugins: {
-											datalabels: {
-												display: false
-											}
-										}
-									}}
-								/>
+							<div className="col-12 mt-25">
+								<div className="containerMargin">
+									<ColorTone />
+								</div>
 							</div>
-						</Card>
-					</div>
-					<div className="col-12 mt-10">
-						<Card
-							title="Lumiere Data"
-							customHeaderClass="bg-tealish border-bt-dark"
-							customBodyClass="bg-charcoal-grey  color-white"
-						>
-							<div className="col-7">
-								<Line
-									data={lineData}
-									options={{
-										legend: false,
-										scales: {
-											xAxes: [
-												{
-													gridLines: {
-														color: "#000",
-														borderDash: [5, 10.15]
-													}
-												}
-											],
-											yAxes: [
-												{
-													id: "right-y-axis",
-													type: "linear",
-													position: "right",
-													gridLines: {
-														display: false
-													}
-												}
-											]
-										},
-										ticks: {
-											display: false
-										},
-										layout: {
-											padding: {
-												left: 50,
-												right: 50,
-												bottom: 50,
-												top: 50
-											}
-										},
-
-										plugins: {
-											datalabels: {
-												display: false
-											}
-										}
-									}}
-								/>
-							</div>
-						</Card>
-					</div>
-					<div className="col-12 mt-10">
-						<Card
-							title="Lumiere Data"
-							customHeaderClass="bg-tealish border-bt-dark"
-							customBodyClass="bg-charcoal-grey  color-white"
-						>
-							<div className="col-7">
-								<Line
-									data={lineWithCustomLabel}
-									options={{
-										legend: {
-											display: true
-										},
-										scales: {
-											xAxes: [
-												{
-													gridLines: {
-														color: "#000",
-														borderDash: [5, 10.15]
-													}
-												}
-											],
-											yAxes: [
-												{
-													position: "right",
-													gridLines: {
-														display: false
-													},
-													ticks: {
-														min: 0,
-														max: this.max,
-														callback: function(value) {
-															return (
-																((value / this.max) * 100).toFixed(0) + "%"
-															);
-														}
-													}
-												}
-											]
-										},
-
-										layout: {
-											padding: {
-												left: 50,
-												right: 50,
-												bottom: 50,
-												top: 50
-											}
-										},
-										tooltips: {
-											enabled: false
-										},
-										plugins: {
-											datalabels: {
-												color: "#fff",
-												font: {
-													size: 14,
-													weight: "bold"
-												},
-												// eslint-disable-next-line no-unused-vars
-												formatter: (value, ctx, i = 0) => {
-													const arr = ctx.dataset.data.filter(
-														x => !Number.isNaN(x)
-													);
-													const val = arr[Math.floor((arr.length - 1) / 2)];
-													if (value === val) {
-														return "12%...";
-													}
-													return null;
-												}
-											}
-										}
-									}}
-								/>
-							</div>
-						</Card>
-					</div>
-					<div className="col-12 mt-10">
-						<Card
-							title="Lumiere Data"
-							customHeaderClass="bg-tealish border-bt-dark"
-							customBodyClass="bg-dark-three box-shadow-black-1 color-white"
-						>
-							<div className="col-12 p-25">
-								<CustomLineGradient />
+							<div className="col-12 mt-25 mb-25">
+								<div className="containerMargin">
+									<AgeRangeAndGender />
+								</div>
 							</div>
 						</Card>
 					</div>
