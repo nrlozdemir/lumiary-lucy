d6631ec88979fdcb5b21eaf0cee7cf647a256552
mefallit <sezgisensoz@gmail.com>
2018-11-17 15:20:23 +0300

    feat: Added default market view containers.

--- src/client/containers/MarketView/index.js
+++ src/client/containers/MarketView/index.js
@@ -1,7 +1,27 @@
 if (typeof require.ensure !== 'function') require.ensure = (d, c) => c(require)
 
-export default function(location, cb) {
-  require.ensure([], (require) => {
-    cb(null, require('./Main').default, location.params.type )
-  })
+export default function (location, cb) {
+  //TODO: need refactor later. Somehow require makes trouble.
+  switch (location.params.type) {
+    case 'audience':
+      return require.ensure([], (require) => {
+        cb(null, require('./Audience').default)
+      })
+    case 'competitor':
+      return require.ensure([], (require) => {
+        cb(null, require('./Competitor').default)
+      })
+    case 'time':
+      return require.ensure([], (require) => {
+        cb(null, require('./Time').default)
+      })
+    case 'use-case':
+      return require.ensure([], (require) => {
+        cb(null, require('./UseCase').default)
+      })
+    default:
+      return require.ensure([], (require) => {
+        cb(null, require('./Platform').default)
+      })
+  }
 }
