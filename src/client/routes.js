e1a6b2d74d89a1fe03fe284afd9533b457c18a57
mefallit <sezgisensoz@gmail.com>
2018-11-17 22:57:56 +0300

    feat: Added quickview default container and route.

--- src/client/routes.js
+++ src/client/routes.js
@@ -12,6 +12,7 @@ import NotFound from "./containers/NotFound";
 import Library from "./containers/Library";
 import MarketView from "./containers/MarketView";
 import Panoptic from "./containers/Panoptic";
+import Quickview from "./containers/Quickview";
 
 const requireAuth = (nextState, replace) => {
 	const state = loadState() || {};
@@ -26,6 +27,7 @@ const routes = (
 		<Route path="library(/video/:id)" getComponent={Library} sidebar/>
 		<Route path="marketview(/:type)" getComponent={ MarketView }  />
 		<Route path="panoptic" getComponent={ Panoptic } />
+		<Route path="quickview" getComponent={ Quickview } />
 		<Route path="*" components={NotFound}/>
 	</Route>
 );
