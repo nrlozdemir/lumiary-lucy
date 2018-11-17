f02ba9407f61929f5cf67e00476935cbdc0eef07
mefallit <sezgisensoz@gmail.com>
2018-11-17 23:43:37 +0300

    feat: Added container dom element for marketview.

--- src/client/containers/MarketView/pages/Competitor.js
+++ src/client/containers/MarketView/pages/Competitor.js
@@ -4,14 +4,18 @@ import { bindActionCreators } from 'redux'
 import { actions } from 'Reducers/home'
 import { actions as marketActions } from 'Reducers/marketview'
 import SubNav from '../views/subNav'
+import cx from 'classnames'
+import style from '../styles.scss'
 // import PropTypes from 'prop-types'
 
 class Competitor extends Component {
   render() {
     return (
-      <div>
+      <div className={cx(style.marketView, style.competitor)}>
         <SubNav/>
-        Competitor
+        <div className={style.container}>
+          Competitor
+        </div>
       </div>
     )
   }
