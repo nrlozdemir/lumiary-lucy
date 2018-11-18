ea85e0fbd165d0dec574d11d1f6d1498aa31dc0c
Mustafa Demirkent <musdy@Mustafas-MBP.connect>
2018-11-18 00:32:52 +0000

    fix: linechart added.

--- src/client/containers/MarketView/pages/Platform.js
+++ src/client/containers/MarketView/pages/Platform.js
@@ -6,6 +6,11 @@ import { actions as marketActions } from 'Reducers/marketview'
 import SubNav from '../views/subNav'
 import cx from 'classnames'
 import style from '../styles.scss'
+import LineChart from "./../../../components/Charts/LineChart";
+import Card from "../../../components/Card";
+import VideoTabs from "../../Library/Sections/videoTabs";
+import ColorTone from "../../Library/Sections/colorTone";
+import AgeRangeAndGender from "../../Library/Sections/ageRangeAndGender";
 // import PropTypes from 'prop-types'
 
 class Platform extends Component {
@@ -13,8 +18,34 @@ class Platform extends Component {
     return (
       <div className={cx(style.marketView, style.platform)}>
         <SubNav/>
+
         <div className={style.container}>
-          Platform
+
+					<div className="col-12 mt-10 pb-10">
+						<div className="mb-25">
+							<LineChart height="40px" />
+						</div>
+
+						<Card
+							title="Lumiere Data"
+							customHeaderClass="bg-charcoal-grey border-bt-dark color-white"
+							customBodyClass="bg-charcoal-grey color-white"
+						>
+							<div className="col-12 mt-25">
+								<VideoTabs />
+							</div>
+							<div className="col-12 mt-25">
+								<div className="containerMargin">
+									<ColorTone />
+								</div>
+							</div>
+							<div className="col-12 mt-25 mb-25">
+								<div className="containerMargin">
+									<AgeRangeAndGender />
+								</div>
+							</div>
+						</Card>
+					</div>
         </div>
       </div>
     )
