521786c7dd3cfcf25d8afac031961961b7e22339
Yasar Demirkent <yasar@saypr.com>
2018-11-18 19:58:10 +0200

    style: panoptoc style changes.

--- src/client/containers/Panoptic/Main.js
+++ src/client/containers/Panoptic/Main.js
@@ -1,12 +1,60 @@
 import React, { Component } from 'react'
 
+
+// Styles
+import style from './styles.scss'
+
+
+
 class Panoptic extends Component {
   render() {
     return (
-      <div>
-        Panoptic
+			<div>
+					<div className={style.chartPanel + " col-6"}>
+
+						<div className={style.controlGroup}>
+							<label className={style.control + " controlCheckbox"}>2D Animation
+								<input type="checkbox" checked="checked"/>
+								<div className={style.controlIndicator + " first"}></div>
+							</label>
+
+							<label className={style.control + " controlCheckbox"}>Stop Motion
+								<input type="checkbox" checked="checked"/>
+								<div className={style.controlIndicator+ " second"}></div>
+							</label>
+
+							<label className={style.control + " controlCheckbox"}>Motion Graphics
+								<input type="checkbox" checked="checked"/>
+								<div className={style.controlIndicator}></div>
+							</label>
+
+							<label className={style.control + " controlCheckbox"}>Live Action
+								<input type="checkbox" checked="checked"/>
+								<div className={style.controlIndicator}></div>
+							</label>
+						</div>
+
+					</div>
+
+					<figure>
+							<img src={require('../../assets/group.png')} />
+					</figure>
+
+					<div className={style.resultsPanel + " col-3"}>
+					tatatata
+
+					</div>
+					<div className={style.rightPanel + " col-3"}>
+					tatatatatata
+					</div>
       </div>
-    )
+		)
+
+
+
+
+
+
   }
 }
 
