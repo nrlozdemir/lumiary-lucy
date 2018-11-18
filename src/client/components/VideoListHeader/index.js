311eec56d2cc6f328ae77b35a2a6e0c251cd8290
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2018-11-19 01:08:12 +0300

    style: videolist style fix

--- src/client/components/VideoListHeader/index.js
+++ src/client/components/VideoListHeader/index.js
@@ -6,13 +6,9 @@ import style from "./style.scss";
 const VideoListHeader = () => (
 	<React.Fragment>
 		<div className={style.container}>
-			<div className="col-6">
-				<div className={style.header}>Videos</div>
-			</div>
-			<div className="col-6">
-				<div className={style.addNewButton}>
-					<span className="qf-iconAdd" />
-				</div>
+			<div className={style.header}>Videos</div>
+			<div className={style.addNewButton}>
+				<span className="qf-iconAdd" />
 			</div>
 		</div>
 	</React.Fragment>
