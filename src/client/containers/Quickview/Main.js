9398b1e17389122f3a4018a4e14cf6cf7ff263ad
Cem Gencer <cem.gencer@me.com>
2018-11-18 21:44:22 +0300

    created VersusList component to render 2 videos in a row

--- src/client/containers/Quickview/Main.js
+++ src/client/containers/Quickview/Main.js
@@ -8,32 +8,104 @@ import CircularProgressBar from "react-circular-progressbar";
 import StyledProgressbar from "../../components/RadialPercentage/StyledProgressbar";
 import SegmentedProgressbar from "../../components/RadialPercentage/SegmentedProgressbar";
 import RadialPercentage from "../../components/RadialPercentage";
+import VersusList from "../../components/VersusList";
 
 class Quickview extends Component {
 
 	render() {
-		const items = ['Duration', 'Scenes', 'Product', 'Color', 'Gender', 'FPS'];
-		const { width, height, color, percentage } = this.props;
+		const { width, height, color, percentage, router } = this.props;
+
+		const versus = [
+			{
+				title: "Duration",
+				vl: {
+					id: 1,
+					subtitle: "",
+					src: "https://picsum.photos/300/200"
+				},
+				vr: {
+					id: 2,
+					subtitle: "",
+					src: "https://picsum.photos/300/200"
+				},
+				diff: 45
+			},
+			{
+				title: "Scenes",
+				vl: {
+					id: 3,
+					subtitle: "",
+					src: "https://picsum.photos/300/200"
+				},
+				vr: {
+					id: 4,
+					subtitle: "",
+					src: "https://picsum.photos/300/200"
+				},
+				diff: 23
+			},
+			{
+				title: "Product",
+				vl: {
+					id: 5,
+					subtitle: "",
+					src: "https://picsum.photos/300/200"
+				},
+				vr: {
+					id: 6,
+					subtitle: "",
+					src: "https://picsum.photos/300/200"
+				},
+				diff: 76
+			},
+			{
+				title: "Color",
+				vl: {
+					id: 7,
+					subtitle: "",
+					src: "https://picsum.photos/300/200"
+				},
+				vr: {
+					id: 8,
+					subtitle: "",
+					src: "https://picsum.photos/300/200"
+				},
+				diff: 97
+			},
+			{
+				title: "Gender",
+				vl: {
+					id: 9,
+					subtitle: "",
+					src: "https://picsum.photos/300/200"
+				},
+				vr: {
+					id: 10,
+					subtitle: "",
+					src: "https://picsum.photos/300/200"
+				},
+				diff: 38
+			},
+			{
+				title: "FPS",
+				vl: {
+					id: 11,
+					subtitle: "",
+					src: "https://picsum.photos/300/200"
+				},
+				vr: {
+					id: 12,
+					subtitle: "",
+					src: "https://picsum.photos/300/200"
+				},
+				diff: 67
+			},
+		];
 
 		return (
 			<div className={style.main}>
 				<div className="col-8">
-					<div style={{ color: '#fff' }} className={`col-4 bordered listBlockLeft`}>
-						testarea
-					</div>
-					<div style={{ color: '#800', height: '135px' }} className={`col-4 bordered listBlockMid`}>
-						<RadialPercentage 
-							width="135px" 
-							height="135px" 
-							color1="#2f2e3d"
-							color2="#21bcd5"
-							percentage="16" 
-							fontsize="30"
-						/>
-					</div>
-					<div className={`col-4 bordered listBlockRight`}>
-
-					</div>
+					<VersusList router={router} videos={versus} />
 				</div>
 				<div className="col-4">
 
