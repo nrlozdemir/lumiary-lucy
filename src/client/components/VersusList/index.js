8b86c644ea94e2106727601504319795cac07033
Cem Gencer <cem.gencer@me.com>
2018-11-21 15:53:49 +0300

    FIX: some css cleanup

--- src/client/components/VersusList/index.js
+++ src/client/components/VersusList/index.js
@@ -19,25 +19,28 @@ class VersusRow extends React.Component {
 		const video = this.props.video;
 		return (
 			<React.Fragment>
-			<div className={style.versusRow} style={{ backgroundColor: '#22212f'}}>
-				<div className={style.versusCellLeft}>
-					<Video styleOverride={style.customVideo} key={video.vl.id} video={video.vl} />
+				<div className={style.versusRow} style={{ backgroundColor: '#22212f'}}>
+					<div className={style.versusCellLeft}>
+						<Video styleOverride={style.customVideo} key={video.vl.id} video={video.vl} />
+					</div>
+					<div className={style.versusCellMid} style={{ backgroundColor: '#2f2e3c', height: '175px'}}>
+						<RadialPercentage fontsize="30" percentage={video.diff} />
+					</div>
+					<div className={style.versusCellRight}>
+						<Video styleOverride={style.customVideo} key={video.vr.id} video={video.vr} />
+					</div>
 				</div>
-				<div className={style.versusCellMid} style={{ backgroundColor: '#2f2e3c'}}>
-					<RadialPercentage 
-						fontsize="30"
-						percentage={video.diff} 
-					/>
+				<div className={style.versusRow}>
+					<div className={style.versusCellLeft}>
+						<h3 className={style.versusSubTitle}>{video.vl.subtitle}</h3>
+					</div>
+					<div className={style.versusCellMid}>
+						<h3 className={style.versusCategory}>{video.title}</h3>
+					</div>
+					<div className={style.versusCellRight}>
+						<h3 className={style.versusSubTitle}>{video.vr.subtitle}</h3>
+					</div>
 				</div>
-				<div className={style.versusCellRight}>
-					<Video styleOverride={style.customVideo} key={video.vr.id} video={video.vr} />
-				</div>
-			</div>
-			<div className={style.versusRow}>
-				<div className={style.versusSubTitle}>{video.vl.subtitle}</div>
-				<div className={style.versusCategory}>{video.title}</div>
-				<div className={style.versusSubTitle}>{video.vr.subtitle}</div>
-			</div>
 			</React.Fragment>
 		);
 	}
@@ -54,9 +57,15 @@ class VersusList extends React.Component {
 		return (
 			<React.Fragment>
 				<div className={style.versusRow}>
-					<div className={style.versusTitleBold}>Best Performing</div>
-					<div className={style.versusTitle}>% difference</div>
-					<div className={style.versusTitleBold}>Worst Performing</div>
+					<div className={style.versusCellLeft}>
+						<h3 className={style.versusTitleBold}>Best Performing</h3>
+					</div>
+					<div className={style.versusCellMid}>
+						<h3 className={style.versusTitle}>% difference</h3>
+					</div>
+					<div className={style.versusCellRight}>
+						<h3 className={style.versusTitleBold}>Worst Performing</h3>
+					</div>
 				</div>
 				{videos.map(video => (
 					<React.Fragment key={video.key}>
