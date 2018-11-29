68028b29cb294acdc313342537592aab73b51055
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2018-11-29 13:14:03 +0300

    fix: versus component fixed

--- src/client/components/VersusList/index.js
+++ src/client/components/VersusList/index.js
@@ -4,42 +4,43 @@ import React from "react";
 import PropTypes from "prop-types";
 
 // Styles
-import helpers from "../../scss/helpers.scss";
-import variables from "../../scss/variables.scss";
 import style from "./style.scss";
 // Components
-import CircularProgressBar from "react-circular-progressbar";
-import StyledProgressbar from "../../components/RadialPercentage/StyledProgressbar";
 import SegmentedProgressbar from "../../components/RadialPercentage/SegmentedProgressbar";
-import RadialPercentage from "../../components/RadialPercentage";
-import Video from "../Video";
+import VideoPlayer from "../VideoPlayer";
 
 class VersusRow extends React.Component {
 	render() {
 		const video = this.props.video;
 		return (
 			<React.Fragment>
-				<div className={style.versusRow} style={{ backgroundColor: '#22212f'}}>
-					<div className={style.versusShadowed}>
-					<div className={style.versusCellLeft}>
-						<Video styleOverride={style.customVideo} key={video.vl.id} video={video.vl} />
+				<div className={style.versusContainer + " " + style.versusShadow}>
+					<div className={style.versusVideoContainer}>
+						<VideoPlayer
+							data={video.vl.video}
+							videoId={video.vl.id}
+							fontSize={60}
+						/>
 					</div>
-					<div className={style.versusCellMid} style={{ backgroundColor: '#2f2e3c', height: '175px'}}>
-						<RadialPercentage fontsize="30" percentage={video.diff} />
+					<div className={style.versusMiddle + " bg-charcoal-grey"}>
+						<SegmentedProgressbar fontsize="30" percentage={video.diff} />
 					</div>
-					<div className={style.versusCellRight}>
-						<Video styleOverride={style.customVideo} key={video.vr.id} video={video.vr} />
-					</div>
+					<div className={style.versusVideoContainer}>
+						<VideoPlayer
+							data={video.vr.video}
+							videoId={video.vr.id}
+							fontSize={60}
+						/>
 					</div>
 				</div>
-				<div className='versusRow endOfRow'>
-					<div className={style.versusCellLeft}>
+				<div className={style.versusContainer}>
+					<div className={style.versusVideoContainer}>
 						<h3 className={style.versusSubTitle}>{video.vl.subtitle}</h3>
 					</div>
-					<div className={style.versusCellMid}>
+					<div className={style.versusMiddle}>
 						<h3 className={style.versusCategory}>{video.title}</h3>
 					</div>
-					<div className={style.versusCellRight}>
+					<div className={style.versusVideoContainer}>
 						<h3 className={style.versusSubTitle}>{video.vr.subtitle}</h3>
 					</div>
 				</div>
@@ -58,15 +59,15 @@ class VersusList extends React.Component {
 		const { videos } = this.props;
 		return (
 			<React.Fragment>
-				<div className={style.versusRow}>
-					<div className={style.versusCellLeft}>
-						<h3 className={style.versusTitleBold} style={{fontSize: '18px', fontFamily: 'clanOT'}}>Best Performing</h3>
+				<div className={style.versusContainer}>
+					<div className={style.versusVideoContainer}>
+						<h3 className={style.versusTitleBold}>Best Performing</h3>
 					</div>
-					<div className={style.versusCellMid}>
+					<div className={style.versusMiddle}>
 						<h3 className={style.versusTitle}>% difference</h3>
 					</div>
-					<div className={style.versusCellRight}>
-						<h3 className={style.versusTitleBold} style={{fontSize: '18px', fontFamily: 'clanOT'}}>Worst Performing</h3>
+					<div className={style.versusVideoContainer}>
+						<h3 className={style.versusTitleBold}>Worst Performing</h3>
 					</div>
 				</div>
 				{videos.map(video => (
