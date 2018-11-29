f89421dc596e5a1f1b1fec1b7dc436fd31da4ea4
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2018-11-29 13:14:51 +0300

    feat: videoplayer component has developed

--- src/client/components/VideoPlayer/index.js
+++ src/client/components/VideoPlayer/index.js
@@ -1,34 +1,32 @@
-import React, { PureComponent } from 'react'
-import style from './styles.scss'
+import React from "react";
+import style from "./styles.scss";
 
-class VideoPlayer extends PureComponent {
+const VideoPlayer = ({ data, videoId, fontSize }) => {
+	return (
+		<div className={style.videoContainer}>
+			<video className={style.video} id={videoId}>
+				<source src={data} type="video/mp4" />
+			</video>
+			<span
+				onClick={() => {
+					const video = document.getElementById(videoId);
+					if (video.paused) video.play();
+					else video.pause();
+				}}
+				className={style.videoIcon + " qf-iconPlay"}
+				style={{
+					fontSize: fontSize
+				}}
+			>
+				<span className="path1" />
+				<span className="path2" />
+				<span className="path3" />
+				<span className="path4" />
+				<span className="path5" />
+				<span className="path6" />
+			</span>
+		</div>
+	);
+};
 
-  onIconClicked() {
-    console.log('thik')
-    debugger;
-    this.refs.vidRef.play();
-  }
-
-  render() {
-    return (
-      <div className={style.videoContainer}>
-        <video width="100%" ref="vidRef">
-          <source src={this.props.data} type="video/mp4" />
-        </video>
-        <span
-          onClick={() => this.onIconClicked()}
-          className={style.videoIcon + " qf-iconPlay"}
-        >
-          <span className="path1" />
-          <span className="path2" />
-          <span className="path3" />
-          <span className="path4" />
-          <span className="path5" />
-          <span className="path6" />
-        </span>
-      </div>
-    )
-  }
-}
-
-export default VideoPlayer
+export default VideoPlayer;
