acbf99c8845923bace4abce03bedb3a0df1b29e3
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2018-11-29 16:22:13 +0300

    feat: quickview page header side has done

--- src/client/containers/Quickview/Main.js
+++ src/client/containers/Quickview/Main.js
@@ -2,15 +2,21 @@
 
 import React, { Component } from "react";
 
-import variables from "../../scss/variables.scss";
 import style from "./style.scss";
-import PropTypes from "prop-types";
 import VersusList from "../../components/VersusList";
 import RankingsList from "../../components/RankingsList";
-import Dropdown from "../../components/Dropdown";
-import { AgeIcon, SexIcon } from "../../components/Form/Controls/Radio";
+import Select from "./../../components/Form/Controls/Select/index";
 
 class Quickview extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			ageRange: { value: "18-24", label: "18-24" }
+		};
+	}
+	onChange(e) {
+		this.setState({ ageRange: e });
+	}
 	render() {
 		const versus = [
 			{
@@ -119,33 +125,50 @@ class Quickview extends Component {
 
 		return (
 			<React.Fragment>
-				<div className={style.quickviewHeader}>
-					<div className={style.quickviewHeader_cellLeft}>
+				<div className="grid-container mt-50">
+					<div className="col-3">
 						<div className={style.genders}>
-							<icon className="qf-iconMale" />
-							<span>Male</span>
+							<div className={style.gender}>
+								<div>
+									<icon className="qf-iconMale" />
+									<span>Male</span>
+								</div>
+							</div>
+							<div className={style.gender}>
+								<div>
+									<icon className="qf-iconFemale" />
+									<span>Female</span>
+								</div>
+							</div>
 						</div>
-						<div className={style.genders}>
-							<icon className="qf-iconFemale" />
-							<span>Female</span>
+					</div>
+					<div className="col-6">
+						<div className={style.ageContainer}>
+							<div>
+								<img src={require("./../../assets/crosshair.png")} />
+							</div>
+							<div className={style.ageRangeText}>Age Range:</div>
+							<div>
+								<Select
+									className={style.ageRangeDropdownElement}
+									options={[
+										{ value: "18-24", label: "18-24" },
+										{ value: "24-30", label: "24-30" },
+										{ value: "30-43", label: "30-43" }
+									]}
+									onChange={e => this.onChange(e)}
+									value={this.state.ageRange}
+								/>
+							</div>
 						</div>
 					</div>
-					<div className={style.quickviewHeader_cellMid}>
-						<table>
-							<tr>
-								<td>
-									<img src={require("./../../assets/crosshair.png")} />
-								</td>
-								<td>Age range:</td>
-								<td>18-24</td>
-							</tr>
-						</table>
+					<div className="col-3">
+						<p className={style.viewContainer}>
+							<span className="qf-iconLeft-Arrow" />
+							<span> views</span>
+							<span className="qf-iconRight-Arrow" />
+						</p>
 					</div>
-					<div className={style.quickviewHeader_cellRight}>
-						<span className="qf-iconLeft-Arrow" /> views
-						<span className="qf-iconRight-Arrow" />
-					</div>
-					<hr />
 				</div>
 				<div className="grid-container">
 					<div className="col-8">
