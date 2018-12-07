385917e93d0a0b28bb1aa9d26b4cafbb175dc59d
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2018-12-07 15:43:44 +0300

    fix: versus list moved options.js

--- src/client/containers/Library/options.js
+++ src/client/containers/Library/options.js
@@ -441,6 +441,111 @@ const videos = [
 		video: "//media.quickframe.com/video/video/6324.mp4"
 	}
 ];
+
+const versus = [
+	{
+		key: 1,
+		title: "Duration",
+		vl: {
+			subtitle: '0:15"',
+			poster: "//static.quickframe.com/homepage/lumascape/4.jpg",
+			id: "lumascape4",
+			video: "//media.quickframe.com/video/video/13430.mp4"
+		},
+		vr: {
+			subtitle: '3:30"',
+			poster: "//static.quickframe.com/homepage/lumascape/12.jpg",
+			id: "lumascape12",
+			video: "//media.quickframe.com/video/video/13436.mp4"
+		},
+		diff: "45"
+	},
+	{
+		key: 2,
+		title: "Scenes",
+		vl: {
+			poster: "//static.quickframe.com/homepage/lumascape/3.jpg",
+			id: "kumascape3",
+			video: "//media.quickframe.com/video/video/13432.mp4",
+			subtitle: "3 Total"
+		},
+		vr: {
+			poster: "//static.quickframe.com/homepage/lumascape/1.jpg",
+			id: "lumascape1",
+			video: "//media.quickframe.com/video/video/13433.mp4",
+			subtitle: "8 Scenes"
+		},
+		diff: "23"
+	},
+	{
+		key: 3,
+		title: "Product",
+		vl: {
+			poster: "//static.quickframe.com/homepage/lumascape/1.jpg",
+			id: "lumascape1",
+			video: "//media.quickframe.com/video/video/7485.mp4",
+			subtitle: "Appearing for 80% of video"
+		},
+		vr: {
+			subtitle: "No apperance",
+			poster: "//static.quickframe.com/homepage/lumascape/4.jpg",
+			id: "lumascape4",
+			video: "//media.quickframe.com/video/video/13450.mp4"
+		},
+		diff: "76"
+	},
+	{
+		key: 4,
+		title: "Color",
+		vl: {
+			subtitle: "Vibrant - Warm",
+			poster: "//static.quickframe.com/homepage/lumascape/12.jpg",
+			id: "lumascape12",
+			video: "//media.quickframe.com/video/video/15991.mp4"
+		},
+		vr: {
+			subtitle: "Cool - Dull",
+			poster: "//static.quickframe.com/homepage/lumascape/1.jpg",
+			id: "lumascape1",
+			video: "//media.quickframe.com/video/video/15992.mp4"
+		},
+		diff: "97"
+	},
+	{
+		key: 5,
+		title: "Gender",
+		vl: {
+			subtitle: "Mostly Female",
+			poster: "//static.quickframe.com/homepage/lumascape/12.jpg",
+			id: "lumascape12",
+			video: "//media.quickframe.com/video/video/15993.mp4"
+		},
+		vr: {
+			subtitle: "Mostly Male",
+			poster: "//static.quickframe.com/homepage/lumascape/1.jpg",
+			id: "lumascape1",
+			video: "//media.quickframe.com/video/video/15995.mp4"
+		},
+		diff: "38"
+	},
+	{
+		key: 6,
+		title: "FPS",
+		vl: {
+			subtitle: "240 FPS at 4K",
+			poster: "//static.quickframe.com/homepage/lumascape/12.jpg",
+			id: "lumascape12",
+			video: "//media.quickframe.com/video/video/7480.mp4"
+		},
+		vr: {
+			subtitle: "30 FPS at 1080p",
+			poster: "//static.quickframe.com/homepage/lumascape/1.jpg",
+			id: "lumascape1",
+			video: "//media.quickframe.com/video/video/16000.mp4"
+		},
+		diff: "67"
+	}
+];
 export {
 	lineData,
 	lineWithCustomLabel,
@@ -450,5 +555,6 @@ export {
 	barChartCompare,
 	videoTabsDataCompare,
 	videoTabsDataBottom,
-	videos
+	videos,
+	versus
 };
