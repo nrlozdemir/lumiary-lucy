1a8373c9720beff8a6c64318f5158740f61eb7b3
Yusuf Bakır <f.yusufbakir@gmail.com>
2019-01-04 21:57:11 +0300

    Update client files

--- src/client/configureStore.js
+++ src/client/configureStore.js
@@ -1,36 +1,26 @@
-import { compose, createStore, applyMiddleware } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import createSagaMiddleware from "redux-saga";
-import { autoRehydrate } from "redux-persist";
+import { routerMiddleware } from "connected-react-router";
+
 import rootReducer from "Reducers/rootReducer";
 import rootSaga from "Sagas/rootSaga";
+import history from "./history";
 
-/* eslint-disable no-underscore-dangle */
 const composeEnhancers =
-	process.env.NODE_ENV !== "production" &&
-	typeof window === "object" &&
-	window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+	typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
 		: compose;
-/* eslint-enable */
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-	rootReducer,
-	composeEnhancers(
-		autoRehydrate({ log: false }),
-		applyMiddleware(sagaMiddleware)
-	)
-);
+const store = function configureStore() {
+	const enhancer = composeEnhancers(
+		applyMiddleware(sagaMiddleware),
+		applyMiddleware(routerMiddleware(history))
+	);
+	const store = createStore(rootReducer(history), enhancer);
+	sagaMiddleware.run(rootSaga);
+	return store;
+};
 
-sagaMiddleware.run(rootSaga);
-
-if (module.hot) {
-	// Enable Webpack hot module replacement for reducers
-	module.hot.accept("./reducers/rootReducer", () => {
-		const nextRootReducer = require("Reducers/rootReducer").default;
-		store.replaceReducer(nextRootReducer);
-	});
-}
-
-export default store;
+export default store();
--- src/client/index.js
+++ src/client/index.js
@@ -1,84 +1,10 @@
-import "babel-polyfill";
-import "isomorphic-fetch";
 import React from "react";
-import { match, browserHistory } from "react-router";
-import { useBasename } from "history";
-import { AppContainer } from "react-hot-loader";
 import ReactDOM from "react-dom";
-import routes from "./Routes";
-import store from "./configureStore";
-import { persistStore, storages } from "redux-persist";
-import Root from "./root";
-import throttle from "lodash/throttle";
-import { loadState, saveState } from "Utils/persistence";
 
-import "./App.scss";
+import App from "./App";
 
 if (typeof window === "undefined") {
 	global.window = new Object();
 }
 
-const persistedState = loadState() ? loadState() : window.__INITIAL_STATE__;
-
-store.subscribe(
-	throttle(() => {
-		saveState({
-			user: store.getState().user,
-			app: store.getState().app,
-			auth: store.getState().auth,
-			signup: store.getState().signup,
-			buyer: store.getState().buyer,
-			brand: store.getState().brand,
-			project: store.getState().project,
-			home: store.getState().home,
-			creator: store.getState().creator,
-			explore: store.getState().explore,
-			social: store.getState().social,
-			cart: store.getState().cart
-		});
-	}, 1000)
-);
-
-const render = () => {
-	match(
-		{ history: browserHistory, routes },
-		(error, redirectLocation, renderProps) => {
-			ReactDOM.render(
-				<AppContainer warnings={false}>
-					<Root
-						routes={routes}
-						store={store}
-						history={browserHistory}
-						{...renderProps}
-					/>
-				</AppContainer>,
-				document.getElementById("app")
-			);
-		}
-	);
-};
-persistStore(
-	store,
-	{
-		whitelist: [
-			"app",
-			"auth",
-			"brand",
-			"buyer",
-			"signup",
-			"user",
-			"project",
-			"home",
-			"creator",
-			"explore",
-			"social",
-			"cart"
-		],
-		storage: storages.localStorage
-	},
-	() => render()
-);
-
-if (module.hot) {
-	module.hot.accept();
-}
+ReactDOM.render(<App />, document.getElementById("root"));
