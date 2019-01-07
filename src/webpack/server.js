733e24d67b5f2362686b7c727f90fa6ada5e1d84
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2019-01-07 16:08:56 +0300

    some fixes

--- src/webpack/server.js
+++ src/webpack/server.js
@@ -1,26 +1,32 @@
-var webpack = require('webpack')
-var WebpackDevServer = require('webpack-dev-server')
-var config = require('./dev.config')
-const fs = require('fs')
-const path = require('path')
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
+var config = require("./dev.config");
+const fs = require("fs");
+const path = require("path");
 
 new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  https: true,
-  key: fs.readFileSync(`${ path.join(__dirname, '..', 'server', 'ssl') }/server.key`),
-  cert: fs.readFileSync(`${ path.join(__dirname, '..', 'server', 'ssl') }/server.crt`),
-  hot: true,
-  inline: true,
-  historyApiFallback: true,
-  port: 9090,
-  disableHostCheck: true,
-  headers: {
-    "Access-Control-Allow-Origin": "*"
-  }
-}).listen(9090, '0.0.0.0', function (err, result) {
-  if (err) {
-    console.log(err)
-  }
+	publicPath: config.output.publicPath,
+	https: true,
+	key: fs.readFileSync(
+		`${path.join(__dirname, "..", "server", "ssl")}/server.key`
+	),
+	cert: fs.readFileSync(
+		`${path.join(__dirname, "..", "server", "ssl")}/server.crt`
+	),
+	hot: true,
+	inline: true,
+	historyApiFallback: true,
+	port: 9090,
+	disableHostCheck: true,
+	headers: {
+		"Access-Control-Allow-Origin": "*"
+	}
+}).listen(9090, "0.0.0.0", function(err, result) {
+	if (err) {
+		console.log(err);
+	}
 
-  console.log('WDS Listening securely on port 9090; browser: https://lumiary-local.quickframe.com:9000')
-})
+	console.log(
+		"WDS Listening securely on port 9090; browser: https://lumiary-local.quickframe.com:9000"
+	);
+});
