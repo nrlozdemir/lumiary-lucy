4a2029fea8167d732ff26ee793cf21986b11f08c
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2019-01-07 14:29:10 +0300

    server config done

--- src/client/containers/Layout.js
+++ src/client/containers/Layout.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Navbar from "Components/Navbar/index";
-
+import "../scss/app.scss";
 const Layout = props => {
-  const Main = props.component;
-  const { removeNavbar } = props;
-  return (
-    <React.Fragment>
-      {!removeNavbar ? <Navbar /> : null}
-      <Main match={props.match} />
-    </React.Fragment>
-  );
+	const Main = props.component;
+	const { removeNavbar } = props;
+	return (
+		<React.Fragment>
+			{!removeNavbar ? <Navbar /> : null}
+			<Main match={props.match} />
+		</React.Fragment>
+	);
 };
 export default Layout;
