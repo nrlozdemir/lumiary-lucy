c1ee2275595768039fc58e788f74bd89596095dd
hami çakır <hmi.cakir@gmail.com>
2019-02-01 00:06:10 +0300

    feat: Radial Chart and Stacked chart card.

--- src/client/containers/Panoptic/dummySummary.js
+++ src/client/containers/Panoptic/dummySummary.js
@@ -1,3 +1,6 @@
+import { Doughnut } from "react-chartjs-2";
+import React from "react";
+
 const dummySummary = [
   { description: 'Video Views', title: '7.64m'},
   { description: 'Engagement Rate', title: '53%'},
@@ -50,6 +53,102 @@ export const platforms = [
 	{name: 'Snapchat', color: '#acb0be'},
 	{name: 'Youtube', color: '#ffffff'},
 	{name: 'Pinterest', color: '#242b49'},
-]
+];
+
+export const doughnutData =
+	{
+		title: "Frame Rate",
+		secondTitle: "24fps",
+		average: [30, 12, 6, 52]
+	};
+
+export const doughnutRoundData = [
+	{data: '0-15 seconds', color: '#51adc0'},
+	{data: '15-30 seconds', color: '#8567f0'},
+	{data: '30-45 seconds', color: '#ff556f'},
+	{data: '45-60 seconds', color: '#acb0be'},
+];
+
+export const doughnutOptions = {
+	responsive: true,
+	cutoutPercentage: 60,
+	tooltips: {
+		enabled: false
+	},
+	legend: {
+		display: false
+	},
+	layout: {
+		padding: 0
+	}
+};
+
+export const stackedChartData = {
+	labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
+	datasets: [{
+		backgroundColor: '#ff556f',
+		data: [
+			25, 10, 14, 51
+		]
+	}, {
+		backgroundColor: '#8567f0',
+		data: [
+			12, 24, 56, 9
+		]
+	}, {
+		backgroundColor: '#acb0be',
+		data: [
+			42, 18, 18, 25
+		]
+	},{
+		backgroundColor: '#51adc0',
+		data: [
+			21, 48, 12, 15
+		]
+	}]
+};
+
+export const stackedChartOptions = {
+	titleFontColor: '#242b49',
+	bodyFontColor: '#242b49',
+	footerFontColor: '#242b49',
+	title: {
+		display: true,
+	},
+	legend: {
+		display: false
+	},
+	tooltips: {
+		mode: 'index',
+		intersect: false
+	},
+	responsive: true,
+	scales: {
+		xAxes: [{
+			stacked: true,
+			ticks: {
+				fontColor: "#fff",
+				fontSize: 12,
+				stepSize: 1,
+				beginAtZero: true,
+				callback: function(value, index, values) {
+					return value + '%';
+				}
+			}
+		}],
+		yAxes: [{
+			stacked: true,
+			ticks: {
+				fontColor: "#fff",
+				fontSize: 12,
+				stepSize: 25,
+				beginAtZero: true,
+				callback: function(value, index, values) {
+					return value + '%';
+				}
+			}
+		}]
+	}
+};
 
 export default dummySummary;
--- src/client/containers/Panoptic/index.js
+++ src/client/containers/Panoptic/index.js
@@ -10,14 +10,24 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { compose } from "redux";
 import { Field, reduxForm } from 'redux-form';
+import { Bar, Doughnut } from 'react-chartjs-2';
 
 import PanopticSummaryCard from "Components/PanopticSummaryCard";
 import LineChart from "Components/LineChart";
 import Select from "Components/Form/Select";
 import ColorTemperatureChart from "Components/ColorTemperatureChart";
+import Button from 'Components/Form/Button';
 import makeSelectPanoptic from "Selectors/Panoptic.js";
 
-import dummySummary, { colorTempData, selectOptions, platforms } from "./dummySummary";
+import dummySummary, {
+	colorTempData,
+	selectOptions,
+	platforms,
+	doughnutData,
+	doughnutRoundData,
+	stackedChartData,
+	doughnutOptions, stackedChartOptions
+} from "./dummySummary";
 import style from "./style.scss";
 
 /* eslint-disable react/prefer-stateless-function */
@@ -25,7 +35,8 @@ export class Panoptic extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			isColorTempVisible: true
+			isColorTempVisible: true,
+			isStackedChartSidebarVisible: false,
 		}
 	}
 
@@ -40,15 +51,22 @@ export class Panoptic extends React.Component {
 			);
 		});
 	}
+
+	setSidebarVisible(type){
+		this.setState({
+			isStackedChartSidebarVisible: type
+		})
+	}
+
 	render() {
-		const { isColorTempVisible } = this.state;
+		const { isColorTempVisible, isStackedChartSidebarVisible } = this.state;
 		return (
 			<React.Fragment>
 				<div className="grid-container mr-20 ml-20 mt-72">
 					{this.renderDummySummary()}
 				</div>
 				<LineChart />
-				<div className="col-12 shadow-1 mv-72 bg-dark-grey-blue">
+				<div className="col-12 shadow-1 mt-72 bg-dark-grey-blue">
 					<div className={style.radialChartsContainer}>
 						<div className={style.temperatureHeader}>
 							<div>
@@ -112,6 +130,137 @@ export class Panoptic extends React.Component {
 								))
 							}
 						</div>
+					</div>
+				</div>
+				<div className="col-12 shadow-1 mt-72 mb-72 bg-dark-grey-blue">
+					<div className={style.radialChartsContainer}>
+						<div className={style.temperatureHeader}>
+							<div>
+								<h2>Color Temperature / Sentiment Comparison</h2>
+							</div>
+							<div className={style.inputWrapper}>
+								<Button
+									onClick={() => this.setSidebarVisible(true)}
+									customClass="float-right font-secondary-first text-bold"
+									buttonText="Filter Videos"
+									iconRight="qf-iconAdd"
+								/>
+							</div>
+						</div>
+						<div className="d-flex align-items-center justify-space-between ph-48">
+							<div className={style.radialAndStackChartWrapper}>
+								<div>
+									<Doughnut
+										options={doughnutOptions}
+										width={270}
+										height={270}
+										data={{
+											labels: [...doughnutData.average],
+											datasets: [
+												{
+													data: [...doughnutData.average],
+													borderColor: "#303a5d",
+													backgroundColor: ["#acb0be", "#8567f0", "#ff556f", "#51adc0"],
+													hoverBackgroundColor: ["#acb0be", "#8567f0", "#ff556f", "#51adc0"]
+												}
+											]
+										}}
+									/>
+								</div>
+								<div>
+									{
+										doughnutRoundData && doughnutRoundData.map((roundData, index) => (
+											<div className="d-flex align-items-center pv-8" key={index}>
+												<span className={style.round} style={{backgroundColor: `${roundData.color}`}}></span>
+												<span className={style.secondsText}>{roundData.data}</span>
+											</div>
+										))
+									}
+								</div>
+							</div>
+							<div className={style.stackedChart}>
+								<Bar
+									width={550}
+									height={300}
+									data={stackedChartData}
+									backgroundColor="#242b49"
+									options={stackedChartOptions}
+								/>
+							</div>
+						</div>
+						{
+							isStackedChartSidebarVisible &&
+							<div className={style.stackedChartSideBar}>
+								<div className={style.stackedChartSideBarContent}>
+									<div className={style.closeIconWrapper}>
+										<i className="qf-iconX" onClick={() => this.setSidebarVisible(false)}></i>
+									</div>
+									<div className={style.filterAreaWrapper}>
+										<div className="pb-40">
+											<p className={style.label}>Video Format</p>
+											<Field
+												component={Select}
+												options={selectOptions}
+												id="NumberOfScenes"
+												name="NumberOfScenes"
+												placeholder="Select One"
+												label="Number of Scenes"
+												className={style.formWrapper}
+											/>
+										</div>
+										<div className="pb-40">
+											<p className={style.label}>Age Audience</p>
+											<Field
+												component={Select}
+												options={selectOptions}
+												id="NumberOfScenes"
+												name="NumberOfScenes"
+												placeholder="Select One"
+												label="Number of Scenes"
+											/>
+										</div>
+										<div className="pb-40">
+											<p className={style.label}>Gender Audience</p>
+											<Field
+												component={Select}
+												options={selectOptions}
+												id="NumberOfScenes"
+												name="NumberOfScenes"
+												placeholder="Select One"
+												label="Number of Scenes"
+											/>
+										</div>
+										<div className={style.dividedSelects}>
+											<div>
+												<p className={style.label}>Start Date</p>
+												<Field
+													component={Select}
+													options={selectOptions}
+													id="NumberOfScenes"
+													name="NumberOfScenes"
+													placeholder="Select One"
+													label="Number of Scenes"
+													className={style.formWrapper}
+												/>
+											</div>
+
+											<div>
+												<p className={style.label}>End Date</p>
+												<Field
+													component={Select}
+													options={selectOptions}
+													id="NumberOfScenes"
+													name="NumberOfScenes"
+													placeholder="Select One"
+													label="Number of Scenes"
+													className={style.formWrapper}
+												/>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						}
 					</div>
 				</div>
 			</React.Fragment>
