a50a1e251427ab163e68da4bf42ca0b47757871f
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2019-02-06 20:45:47 +0300

    feat: quickview reducers added

--- src/client/reducers/Quickview.js
+++ src/client/reducers/Quickview.js
@@ -1,23 +1,89 @@
 /*
-*
-* Quickview reducer
-*
-*/
+ *
+ * Quickview reducer
+ *
+ */
 
+import { fromJS } from "immutable";
 
-import { fromJS } from 'immutable';
+export const types = {
+	GET_QUICKVIEW_ITEMS_REQUEST: "Quickview/GET_QUICKVIEW_ITEMS_REQUEST",
+	GET_QUICKVIEW_ITEMS_SUCCESS: "Quickview/GET_QUICKVIEW_ITEMS_SUCCESS",
+	GET_QUICKVIEW_ITEMS_FAILURE: "Quickview/GET_QUICKVIEW_ITEMS_FAILURE",
+	GET_QUICKVIEW_PLATFORM_SELECTED_REQUEST:
+		"Quickview/GET_QUICKVIEW_PLATFORM_SELECTED_REQUEST",
+	GET_QUICKVIEW_PLATFORM_SELECTED_SUCCESS:
+		"Quickview/GET_QUICKVIEW_PLATFORM_SELECTED_SUCCESS",
+	GET_QUICKVIEW_PLATFORM_SELECTED_FAILURE:
+		"Quickview/GET_QUICKVIEW_PLATFORM_SELECTED_FAILURE"
+};
 
+export const actions = {
+	getQuickviewItemsRequest: () => ({
+		type: types.GET_QUICKVIEW_ITEMS_REQUEST
+	}),
+	getQuickviewItemsSuccess: payload => ({
+		type: types.GET_QUICKVIEW_ITEMS_SUCCESS,
+		payload
+	}),
+	getQuickviewItemsFailure: error => ({
+		type: types.GET_QUICKVIEW_ITEMS_FAILURE,
+		error
+	}),
+	getQuickviewPlatformSelectedRequest: payload => ({
+		type: types.GET_QUICKVIEW_PLATFORM_SELECTED_REQUEST,
+		payload
+	}),
+	getQuickviewPlatformSelectedSuccess: payload => ({
+		type: types.GET_QUICKVIEW_PLATFORM_SELECTED_SUCCESS,
+		payload
+	}),
+	getQuickviewPlatformSelectedFailure: error => ({
+		type: types.GET_QUICKVIEW_PLATFORM_SELECTED_FAILURE,
+		error
+	})
+};
 
-
-export const types = {};
-export const actions = {};
-export const initialState = fromJS({});
+export const initialState = fromJS({
+	quickviewItems: [],
+	selectedPlatform: {
+		id: null,
+		platformsValues: []
+	},
+	error: false,
+	loading: false
+});
 
 const quickviewReducer = (state = initialState, action) => {
-switch (action.type) {
-default:
-return state;
-}
-}
+	switch (action.type) {
+		case types.GET_QUICKVIEW_ITEMS_REQUEST:
+			return state.set("loading", fromJS(true));
+		case types.GET_QUICKVIEW_ITEMS_SUCCESS:
+			return state
+				.set("quickviewItems", fromJS(action.payload))
+				.set("loading", fromJS(false));
+		case types.GET_QUICKVIEW_ITEMS_FAILURE:
+			return state
+				.set("error", fromJS(action.error))
+				.set("loading", fromJS(false));
+
+		case types.GET_QUICKVIEW_PLATFORM_SELECTED_REQUEST:
+			return state.set("loading", fromJS(true));
+		case types.GET_QUICKVIEW_PLATFORM_SELECTED_SUCCESS:
+			return state
+				.setIn(["selectedPlatform", "id"], fromJS(action.payload.id))
+				.setIn(
+					["selectedPlatform", "platformsValues"],
+					fromJS(action.payload.platformsValues)
+				)
+				.set("loading", fromJS(false));
+		case types.GET_QUICKVIEW_PLATFORM_SELECTED_FAILURE:
+			return state
+				.set("error", fromJS(action.error))
+				.set("loading", fromJS(false));
+		default:
+			return state;
+	}
+};
 
 export default quickviewReducer;
--- src/client/reducers/rootReducer.js
+++ src/client/reducers/rootReducer.js
@@ -3,7 +3,6 @@ import { reducer as formReducer } from "redux-form";
 
 import Library from "./Library";
 import Quickview from "./Quickview";
-import QuickviewDetail from "./QuickviewDetail";
 import Marketview from "./Marketview";
 import Panoptic from "./Panoptic";
 import LibraryDetail from "./LibraryDetail";
@@ -14,7 +13,6 @@ const rootReducer = history =>
 		app,
 		Library,
 		Quickview,
-		QuickviewDetail,
 		Marketview,
 		Panoptic,
 		LibraryDetail,
