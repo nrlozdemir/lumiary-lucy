21cbba22282fe1f7759d2b16471c08d4870bccf5
Hami Çakır <hami.cakr@iyzi-sfd-32.local>
2019-02-06 17:54:12 +0300

    fix: All indentations fixed.

--- src/client/components/Dropdown/index.js
+++ src/client/components/Dropdown/index.js
@@ -9,67 +9,67 @@ import { Field } from "redux-form";
 import Select from 'Components/Form/Select'
 
 class Dropdown extends Component{
-	constructor(props){
-		super(props);
-		this.state = {
-			isDropDownVisible: false,
-			listItems: props.listItems,
-			selectedItem: null,
-			dateRange: {
-				selection: {
-					startDate: new Date(),
-					endDate: new Date(),
-					key: "selection"
-				}
-			}
-		}
-	}
+  constructor(props){
+    super(props);
+    this.state = {
+      isDropDownVisible: false,
+      listItems: props.listItems,
+      selectedItem: null,
+      dateRange: {
+        selection: {
+          startDate: new Date(),
+          endDate: new Date(),
+          key: "selection"
+        }
+      }
+    }
+  }
 
-	handleHoverDropdown(){
-		this.setState(prevState => {
-			return {
-				isDropDownVisible: !prevState.isDropDownVisible
-			}
-		})
-	};
+  handleHoverDropdown(){
+    this.setState(prevState => {
+      return {
+        isDropDownVisible: !prevState.isDropDownVisible
+      }
+    })
+  };
 
-	handleSelectItem(index){
-		this.setState({
-			selectedItem: index,
-		});
-	}
+  handleSelectItem(index){
+    this.setState({
+      selectedItem: index,
+    });
+  }
 
-	handleChange = (selectedOption, name) => {
-		this.setState({ [name]: selectedOption });
-	};
+  handleChange = (selectedOption, name) => {
+    this.setState({ [name]: selectedOption });
+  };
 
-	render(){
-		const { title, dateElementIndex } = this.props;
-		const { isDropDownVisible, selectedItem, listItems, dateRange, selectDate } = this.state;
+  render(){
+    const { title, dateElementIndex } = this.props;
+    const { isDropDownVisible, selectedItem, listItems, dateRange, selectDate } = this.state;
 
-		const selectedTitle = ( selectedItem && listItems[selectedItem].name ) || title;
-		return (
-			<div onMouseEnter={() => this.handleHoverDropdown()} onMouseLeave={() => this.handleHoverDropdown()}>
-				<div className={style.ddWrapper} >
-					<div className={style.ddHeader}>
-						<p className={style.ddHeaderTitle}>{selectedTitle}</p>
-					</div>
-					<ul className={style.ddList}>
-						{
-							listItems && isDropDownVisible && listItems.map((item, index) => (
-								<li
-									onClick={() => this.handleSelectItem(index)}
-									key={`ddwn-${index}`}
-									className={classnames(style.ddListItem, { [style.active]: selectedItem === index, })}>
-									{item.name}
-								</li>
-							))
-						}
-					</ul>
-				</div>
-			</div>
-		)
-	}
+    const selectedTitle = ( selectedItem && listItems[selectedItem].name ) || title;
+    return (
+      <div onMouseEnter={() => this.handleHoverDropdown()} onMouseLeave={() => this.handleHoverDropdown()}>
+        <div className={style.ddWrapper} >
+          <div className={style.ddHeader}>
+            <p className={style.ddHeaderTitle}>{selectedTitle}</p>
+          </div>
+          <ul className={style.ddList}>
+            {
+              listItems && isDropDownVisible && listItems.map((item, index) => (
+                <li
+                  onClick={() => this.handleSelectItem(index)}
+                  key={`ddwn-${index}`}
+                  className={classnames(style.ddListItem, { [style.active]: selectedItem === index, })}>
+                  {item.name}
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Dropdown;
--- src/client/containers/Panoptic/dummySummary.js
+++ src/client/containers/Panoptic/dummySummary.js
@@ -1,224 +1,224 @@
 import React from "react";
 
 const dummySummary = [
-	{ description: 'Video Views', title: '7.64m'},
-	{ description: 'Engagement Rate', title: '53%'},
-	{ description: 'Completion Rate', title: '29%'}
+  { description: 'Video Views', title: '7.64m'},
+  { description: 'Engagement Rate', title: '53%'},
+  { description: 'Completion Rate', title: '29%'}
 ];
 
 export const selectOptions = [
-	{ value: "chocolate", label: "Chocolate" },
-	{ value: "strawberry", label: "Strawberry" },
-	{ value: "vanilla", label: "Vanilla" }
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" }
 ];
 
 export const colorTempData = [
-	{
-		data: [
-			{x: -50, y:82, color: '#ff556f'},
-			{x: 50,y:-25, color: '#51adc0'},
-			{x: 75, y: -30, color: '#8567f0'},
-			{x: 60, y: 30, color: '#ffffff'},
-			{x: -12, y: -30, color: '#242b49'},
-		]
-	},
-	{
-		data: [
-			{x: -50, y:12, color: '#ff556f'},
-			{x: 50,y:25, color: '#51adc0'},
-			{x: 75, y: -30, color: '#8567f0'},
-		]
-	},
-	{
-		data: [
-			{x: -50, y:12, color: '#ff556f'},
-			{x: 50,y:-75, color: '#51adc0'},
-			{x: 75, y: -30, color: '#8567f0'},
-		]
-	},
-	{
-		data: [
-			{x: -50, y:12, color: '#ff556f'},
-			{x: 50,y:-75, color: '#51adc0'},
-			{x: 75, y: -30, color: '#8567f0'},
-		]
-	}
+  {
+    data: [
+      {x: -50, y:82, color: '#ff556f'},
+      {x: 50,y:-25, color: '#51adc0'},
+      {x: 75, y: -30, color: '#8567f0'},
+      {x: 60, y: 30, color: '#ffffff'},
+      {x: -12, y: -30, color: '#242b49'},
+    ]
+  },
+  {
+    data: [
+      {x: -50, y:12, color: '#ff556f'},
+      {x: 50,y:25, color: '#51adc0'},
+      {x: 75, y: -30, color: '#8567f0'},
+    ]
+  },
+  {
+    data: [
+      {x: -50, y:12, color: '#ff556f'},
+      {x: 50,y:-75, color: '#51adc0'},
+      {x: 75, y: -30, color: '#8567f0'},
+    ]
+  },
+  {
+    data: [
+      {x: -50, y:12, color: '#ff556f'},
+      {x: 50,y:-75, color: '#51adc0'},
+      {x: 75, y: -30, color: '#8567f0'},
+    ]
+  }
 ];
 
 export const platforms = [
-	{name: 'Facebook', color: '#51adc0'},
-	{name: 'Instagram', color: '#ff556f'},
-	{name: 'Twitter', color: '#8567f0'},
-	{name: 'Snapchat', color: '#acb0be'},
-	{name: 'Youtube', color: '#ffffff'},
-	{name: 'Pinterest', color: '#242b49'},
+  {name: 'Facebook', color: '#51adc0'},
+  {name: 'Instagram', color: '#ff556f'},
+  {name: 'Twitter', color: '#8567f0'},
+  {name: 'Snapchat', color: '#acb0be'},
+  {name: 'Youtube', color: '#ffffff'},
+  {name: 'Pinterest', color: '#242b49'},
 ];
 
 export const doughnutData =
-	{
-		title: "Frame Rate",
-		secondTitle: "24fps",
-		average: [30, 12, 6, 52]
-	};
+  {
+    title: "Frame Rate",
+    secondTitle: "24fps",
+    average: [30, 12, 6, 52]
+  };
 
 export const doughnutRoundData = [
-	{data: '0-15 seconds', color: '#51adc0'},
-	{data: '15-30 seconds', color: '#8567f0'},
-	{data: '30-45 seconds', color: '#ff556f'},
-	{data: '45-60 seconds', color: '#acb0be'},
+  {data: '0-15 seconds', color: '#51adc0'},
+  {data: '15-30 seconds', color: '#8567f0'},
+  {data: '30-45 seconds', color: '#ff556f'},
+  {data: '45-60 seconds', color: '#acb0be'},
 ];
 
 export const doughnutOptions = {
-	responsive: true,
-	cutoutPercentage: 60,
-	tooltips: {
-		enabled: false
-	},
-	legend: {
-		display: false
-	},
-	layout: {
-		padding: 0
-	},
-	plugins: {
-		datalabels: {
-			formatter: (value, ctx) => {
+  responsive: true,
+  cutoutPercentage: 60,
+  tooltips: {
+    enabled: false
+  },
+  legend: {
+    display: false
+  },
+  layout: {
+    padding: 0
+  },
+  plugins: {
+    datalabels: {
+      formatter: (value, ctx) => {
 
-				let sum = 0;
-				let dataArr = ctx.chart.data.datasets[0].data;
-				dataArr.map(data => {
-					sum += data;
-				});
-				let percentage = ( value * 100 / sum ).toFixed(0) + "%";
-				return percentage;
-			},
-			color: '#fff',
-		}
-	}
+        let sum = 0;
+        let dataArr = ctx.chart.data.datasets[0].data;
+        dataArr.map(data => {
+          sum += data;
+        });
+        let percentage = ( value * 100 / sum ).toFixed(0) + "%";
+        return percentage;
+      },
+      color: '#fff',
+    }
+  }
 };
 
 export const stackedChartData = {
-	labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
-	datasets: [{
-		backgroundColor: '#ff556f',
-		data: [
-			25, 10, 14, 51
-		]
-	}, {
-		backgroundColor: '#8567f0',
-		data: [
-			12, 24, 56, 9
-		]
-	}, {
-		backgroundColor: '#acb0be',
-		data: [
-			42, 18, 18, 25
-		]
-	},{
-		backgroundColor: '#51adc0',
-		data: [
-			21, 48, 12, 15
-		]
-	}],
-	beforeDraw: function(chart, easing) {
-		if (
-			chart.config.options.chartArea &&
-			chart.config.options.chartArea.backgroundColor
-		) {
-			const ctx = chart.chart.ctx;
-			const chartArea = chart.chartArea;
+  labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
+  datasets: [{
+    backgroundColor: '#ff556f',
+    data: [
+      25, 10, 14, 51
+    ]
+  }, {
+    backgroundColor: '#8567f0',
+    data: [
+      12, 24, 56, 9
+    ]
+  }, {
+    backgroundColor: '#acb0be',
+    data: [
+      42, 18, 18, 25
+    ]
+  },{
+    backgroundColor: '#51adc0',
+    data: [
+      21, 48, 12, 15
+    ]
+  }],
+  beforeDraw: function(chart, easing) {
+    if (
+      chart.config.options.chartArea &&
+      chart.config.options.chartArea.backgroundColor
+    ) {
+      const ctx = chart.chart.ctx;
+      const chartArea = chart.chartArea;
 
-			ctx.save();
-			ctx.fillStyle = chart.config.options.chartArea.backgroundColor;
-			ctx.fillRect(
-				chartArea.left,
-				chartArea.top,
-				chartArea.right - chartArea.left,
-				chartArea.bottom - chartArea.top
-			);
-			ctx.restore();
-		}
-	}
+      ctx.save();
+      ctx.fillStyle = chart.config.options.chartArea.backgroundColor;
+      ctx.fillRect(
+        chartArea.left,
+        chartArea.top,
+        chartArea.right - chartArea.left,
+        chartArea.bottom - chartArea.top
+      );
+      ctx.restore();
+    }
+  }
 };
 
 export const stackedChartOptions = {
-	titleFontColor: '#242b49',
-	bodyFontColor: '#242b49',
-	footerFontColor: '#242b49',
-	title: {
-		display: true,
-	},
-	legend: {
-		display: false
-	},
-	tooltips: {
-		mode: 'index',
-		intersect: false
-	},
-	responsive: true,
-	scales: {
-		xAxes: [{
-			stacked: true,
-			ticks: {
-				fontColor: "#fff",
-				fontSize: 12,
-				stepSize: 1,
-				beginAtZero: true,
-				callback: function(value, index, values) {
-					return value ;
-				}
-			}
-		}],
-		yAxes: [{
-			stacked: true,
-			ticks: {
-				fontColor: "#fff",
-				fontSize: 12,
-				stepSize: 25,
-				beginAtZero: true,
-				callback: function(value, index, values) {
-					return value + '%';
-				}
-			}
-		}]
-	}
+  titleFontColor: '#242b49',
+  bodyFontColor: '#242b49',
+  footerFontColor: '#242b49',
+  title: {
+    display: true,
+  },
+  legend: {
+    display: false
+  },
+  tooltips: {
+    mode: 'index',
+    intersect: false
+  },
+  responsive: true,
+  scales: {
+    xAxes: [{
+      stacked: true,
+      ticks: {
+        fontColor: "#fff",
+        fontSize: 12,
+        stepSize: 1,
+        beginAtZero: true,
+        callback: function(value, index, values) {
+          return value ;
+        }
+      }
+    }],
+    yAxes: [{
+      stacked: true,
+      ticks: {
+        fontColor: "#fff",
+        fontSize: 12,
+        stepSize: 25,
+        beginAtZero: true,
+        callback: function(value, index, values) {
+          return value + '%';
+        }
+      }
+    }]
+  }
 };
 
 export const dropdownLists = {
-	duration: [
-		{name: "0 - 15ms"},
-		{name: "15 - 30ms"},
-		{name: "30 - 45ms"},
-		{name: "45 - 60ms"},
-	],
-	views: [
-		{name: "Views"},
-		{name: "Likes"},
-		{name: "Shares"},
-		{name: "Comments"}
-	],
-	socialMedia: [
-		{name: "Facebook"},
-		{name: "Instagram"},
-		{name: "Twitter"},
-		{name: "Snapchat"},
-		{name: "Youtube"},
-		{name: "Pinterest"}
-	],
-	dateRange: [
-		{name: "Past 24 Hours"},
-		{name: "Past Week"},
-		{name: "Past Month"},
-		{name: "Past 3 Month"},
-	],
+  duration: [
+    {name: "0 - 15ms"},
+    {name: "15 - 30ms"},
+    {name: "30 - 45ms"},
+    {name: "45 - 60ms"},
+  ],
+  views: [
+    {name: "Views"},
+    {name: "Likes"},
+    {name: "Shares"},
+    {name: "Comments"}
+  ],
+  socialMedia: [
+    {name: "Facebook"},
+    {name: "Instagram"},
+    {name: "Twitter"},
+    {name: "Snapchat"},
+    {name: "Youtube"},
+    {name: "Pinterest"}
+  ],
+  dateRange: [
+    {name: "Past 24 Hours"},
+    {name: "Past Week"},
+    {name: "Past Month"},
+    {name: "Past 3 Month"},
+  ],
 };
 
 export const dateSelectOptions = [
-	{ value: "Today", label: "Today" },
-	{ value: "Past Week", label: "Past Week" },
-	{ value: "Past Month", label: "Past Month" },
-	{ value: "Past 3 Months", label: "Past 3 Months" },
-	{ value: "custom", label: "Custom" }
+  { value: "Today", label: "Today" },
+  { value: "Past Week", label: "Past Week" },
+  { value: "Past Month", label: "Past Month" },
+  { value: "Past 3 Months", label: "Past 3 Months" },
+  { value: "custom", label: "Custom" }
 ];
 
 export default dummySummary;
