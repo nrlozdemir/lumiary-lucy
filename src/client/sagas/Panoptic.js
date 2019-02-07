ffdd7c27e769bc7b87ee1ee5883ee189eff3d8c2
Hami Çakır <hami.cakr@iyzi-sfd-32.local>
2019-02-07 23:44:20 +0300

    feat: api functions added.

--- src/client/sagas/Panoptic.js
+++ src/client/sagas/Panoptic.js
@@ -1,43 +1,11 @@
 import { call, put, takeLatest, all } from "redux-saga/effects";
 import { actions, types } from "Reducers/Panoptic";
+import { getPanopticData } from "Api/Panoptic";
 
-export function* getData() {
+function* getData() {
 	try{
-		const data = {
-			colorTempData: [
-				{
-					data: [
-						{x: -50, y:82, color: '#ff556f'},
-						{x: 50,y:-25, color: '#51adc0'},
-						{x: 75, y: -30, color: '#8567f0'},
-						{x: 60, y: 30, color: '#ffffff'},
-						{x: -12, y: -30, color: '#242b49'},
-					]
-				},
-				{
-					data: [
-						{x: -50, y:12, color: '#ff556f'},
-						{x: 50,y:25, color: '#51adc0'},
-						{x: 75, y: -30, color: '#8567f0'},
-					]
-				},
-				{
-					data: [
-						{x: -50, y:12, color: '#ff556f'},
-						{x: 50,y:-75, color: '#51adc0'},
-						{x: 75, y: -30, color: '#8567f0'},
-					]
-				},
-				{
-					data: [
-						{x: -50, y:12, color: '#ff556f'},
-						{x: 50,y:-75, color: '#51adc0'},
-						{x: 75, y: -30, color: '#8567f0'},
-					]
-				}
-			],
-		};
-		yield put(actions.getDataSuccess(data));
+		const payload = yield call(getPanopticData);
+		yield put(actions.getDataSuccess(payload));
 	}catch (err){
 		yield put(actions.getDataError(err))
 	}
