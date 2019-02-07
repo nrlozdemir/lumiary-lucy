2cb46a9c00a4d99c88475a909f6fe7f5dd164a32
Hami Çakır <hami.cakr@iyzi-sfd-32.local>
2019-02-07 21:34:53 +0300

    feat: Panoptic page reducer and saga.

--- src/client/sagas/Panoptic.js
+++ src/client/sagas/Panoptic.js
@@ -1,4 +1,17 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import { call, put, takeLatest, all } from "redux-saga/effects";
+import { actions, types } from "Reducers/Panoptic";
+
+export function* getData() {
+	try{
+		const data = {
+			//Fill inside
+		};
+		yield put(actions.getDataSuccess(data));
+	}catch (err){
+		yield put(actions.getDataError(e))
+	}
+}
 
 export default function* panopticSaga() {
+	yield takeLatest(types.GET_DATA, getData)
 }
