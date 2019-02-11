710cb686afe3479aeba307a84ad3989cef12c8a6
Yusuf Bakır <f.yusufbakir@gmail.com>
2019-02-11 18:03:13 +0300

    fix: indent

--- src/client/containers/Quickview/index.js
+++ src/client/containers/Quickview/index.js
@@ -23,137 +23,137 @@ import Main from "./views/main";
 import Detail from "./views/detail";
 
 export class Quickview extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			dateRange: {
-				selection: {
-					startDate: new Date(),
-					endDate: new Date(),
-					key: "selection"
-				}
-			}
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      dateRange: {
+        selection: {
+          startDate: new Date(),
+          endDate: new Date(),
+          key: "selection"
+        }
+      }
+    };
+  }
 
-	componentDidMount() {
-		const { getQuickviewItemsRequest } = this.props;
-		getQuickviewItemsRequest();
-	}
+  componentDidMount() {
+    const { getQuickviewItemsRequest } = this.props;
+    getQuickviewItemsRequest();
+  }
 
-	handleChange = (selectedOption, name) => {
-		this.setState({ [name]: selectedOption });
-	};
+  handleChange = (selectedOption, name) => {
+    this.setState({ [name]: selectedOption });
+  };
 
-	render() {
-		const {
-			selectViews,
-			selectDate,
-			dateRange: { selection: dateRange }
-		} = this.state;
+  render() {
+    const {
+      selectViews,
+      selectDate,
+      dateRange: { selection: dateRange }
+    } = this.state;
 
-		const {
-			quickview: { quickviewItems: quickviewItems }
-		} = this.props;
+    const {
+      quickview: { quickviewItems: quickviewItems }
+    } = this.props;
 
-		return (
-			<React.Fragment>
-				<div className="grid-container col-12">
-					<div className={style.headerContainer}>
-						<div>
-							<span className="dot-item">
-								<span className="bg-cool-blue" />
-								Best Videos
-							</span>
-							<span className="dot-item">
-								<span className="bg-coral-pink" />
-								Worst Videos
-							</span>
-						</div>
-						<div>
-							<h1 className="alpha color-white text-center font-primary text-bold">
-								Quickview
-							</h1>
-						</div>
-						<div className="headerRight">
-							<Select
-								name="selectViews"
-								customClass="custom-select"
-								placeholder="Select Views"
-								value={selectViews || ""}
-								onChange={option => this.handleChange(option, "selectViews")}
-								options={[
-									{ value: "Card", label: "Card" },
-									{ value: "Table", label: "Table" }
-								]}
-							/>
-							<Select
-								name="selectDate"
-								customClass="custom-select"
-								placeholder="Select Date"
-								value={selectDate || ""}
-								onChange={option => this.handleChange(option, "selectDate")}
-								options={[
-									{ value: "Today", label: "Today" },
-									{ value: "Past Week", label: "Past Week" },
-									{ value: "Past Month", label: "Past Month" },
-									{ value: "Past 3 Months", label: "Past 3 Months" },
-									{ value: "custom", label: "Custom" }
-								]}
-							/>
-							{selectDate && selectDate.value === "custom" && (
-								<Datepicker
-									type={"range"}
-									apply={() =>
-										this.setState({
-											selectDate: {
-												value:
-													moment(dateRange.startDate).format("DD/MM/YYYY") +
-													" - " +
-													moment(dateRange.endDate).format("DD/MM/YYYY"),
-												label:
-													moment(dateRange.startDate).format("DD/MM/YYYY") +
-													" - " +
-													moment(dateRange.endDate).format("DD/MM/YYYY")
-											}
-										})
-									}
-									back={() =>
-										this.setState({
-											selectDate: null
-										})
-									}
-								/>
-							)}
-						</div>
-					</div>
-					<Switch>
-						<Route
-							path="/quickview"
-							exact
-							render={() => <Main quickviewItems={quickviewItems} />}
-						/>
-						<Route path="/quickview/:id/:platform" component={Detail} />
-					</Switch>
-				</div>
-			</React.Fragment>
-		);
-	}
+    return (
+      <React.Fragment>
+        <div className="grid-container col-12">
+          <div className={style.headerContainer}>
+            <div>
+              <span className="dot-item">
+                <span className="bg-cool-blue" />
+                Best Videos
+              </span>
+              <span className="dot-item">
+                <span className="bg-coral-pink" />
+                Worst Videos
+              </span>
+            </div>
+            <div>
+              <h1 className="alpha color-white text-center font-primary text-bold">
+                Quickview
+              </h1>
+            </div>
+            <div className="headerRight">
+              <Select
+                name="selectViews"
+                customClass="custom-select"
+                placeholder="Select Views"
+                value={selectViews || ""}
+                onChange={option => this.handleChange(option, "selectViews")}
+                options={[
+                  { value: "Card", label: "Card" },
+                  { value: "Table", label: "Table" }
+                ]}
+              />
+              <Select
+                name="selectDate"
+                customClass="custom-select"
+                placeholder="Select Date"
+                value={selectDate || ""}
+                onChange={option => this.handleChange(option, "selectDate")}
+                options={[
+                  { value: "Today", label: "Today" },
+                  { value: "Past Week", label: "Past Week" },
+                  { value: "Past Month", label: "Past Month" },
+                  { value: "Past 3 Months", label: "Past 3 Months" },
+                  { value: "custom", label: "Custom" }
+                ]}
+              />
+              {selectDate && selectDate.value === "custom" && (
+                <Datepicker
+                  type={"range"}
+                  apply={() =>
+                    this.setState({
+                      selectDate: {
+                        value:
+                          moment(dateRange.startDate).format("DD/MM/YYYY") +
+                          " - " +
+                          moment(dateRange.endDate).format("DD/MM/YYYY"),
+                        label:
+                          moment(dateRange.startDate).format("DD/MM/YYYY") +
+                          " - " +
+                          moment(dateRange.endDate).format("DD/MM/YYYY")
+                      }
+                    })
+                  }
+                  back={() =>
+                    this.setState({
+                      selectDate: null
+                    })
+                  }
+                />
+              )}
+            </div>
+          </div>
+          <Switch>
+            <Route
+              path="/quickview"
+              exact
+              render={() => <Main quickviewItems={quickviewItems} />}
+            />
+            <Route path="/quickview/:id/:platform" component={Detail} />
+          </Switch>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 Quickview.propTypes = {
-	dispatch: PropTypes.func
+  dispatch: PropTypes.func
 };
 
 const mapStateToProps = createStructuredSelector({
-	quickview: makeSelectQuickview()
+  quickview: makeSelectQuickview()
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
 
 const withConnect = connect(
-	mapStateToProps,
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 );
 
 export default compose(withConnect)(Quickview);
