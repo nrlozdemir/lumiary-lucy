a7faa24537a04bbf7465ef1658df080f8c1d0226
Yusuf Bakır <f.yusufbakir@gmail.com>
2019-02-17 22:26:09 +0300

    fix: indent

--- src/client/containers/Marketview/index.js
+++ src/client/containers/Marketview/index.js
@@ -17,55 +17,55 @@ import TotalCompetitorViewsChart from 'Components/TotalCompetitorViewsChart';
 
 /* eslint-disable react/prefer-stateless-function */
 export class Marketview extends React.Component {
-	render() {
-		return (
-			<div className="grid-container col-12">
-				<div className={style.alignTabs}>
-					<Link to="/marketview/platform" className={style.tab}>
-						Platform
-					</Link>
-					<Link to="/marketview/competitor" className={style.tab}>
-						Competitor
-					</Link>
-					<Link to="/marketview/time" className={style.tab}>
-						Time
-					</Link>
-				</div>
-				<div className="grid-collapse">
-					<div className="col-4 mb-48">
-						<ColorCard />
-					</div>
+  render() {
+    return (
+      <div className="grid-container col-12">
+        <div className={style.alignTabs}>
+          <Link to="/marketview/platform" className={style.tab}>
+            Platform
+          </Link>
+          <Link to="/marketview/competitor" className={style.tab}>
+            Competitor
+          </Link>
+          <Link to="/marketview/time" className={style.tab}>
+            Time
+          </Link>
+        </div>
+        <div className="grid-collapse">
+          <div className="col-4 mb-48">
+            <ColorCard />
+          </div>
 
-					<div className="col-4 mb-48">
-						<PacingCard />
-					</div>
+          <div className="col-4 mb-48">
+            <PacingCard />
+          </div>
 
-					<div className="col-4 mb-48">
-						<FormatCard />
-					</div>
-				</div>
+          <div className="col-4 mb-48">
+            <FormatCard />
+          </div>
+        </div>
 
-				<div className="grid-collapse">
-					<TotalViewsChart />
-					<TotalCompetitorViewsChart />
-				</div>
-			</div>
-		);
-	}
+        <div className="grid-collapse">
+          <TotalViewsChart />
+          <TotalCompetitorViewsChart />
+        </div>
+      </div>
+    );
+  }
 }
 
 Marketview.propTypes = {
-	dispatch: PropTypes.func.isRequired
+  dispatch: PropTypes.func.isRequired
 };
 
 const mapStateToProps = createStructuredSelector({
-	marketview: makeSelectMarketview()
+  marketview: makeSelectMarketview()
 });
 
 function mapDispatchToProps(dispatch) {
-	return {
-		dispatch
-	};
+  return {
+    dispatch
+  };
 }
 
 const withConnect = connect(mapStateToProps, mapDispatchToProps);
