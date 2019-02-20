d5b7e7bb64599e6b81d756fd7722a2c4bc0b5260
hami çakır <hmi.cakir@gmail.com>
2019-02-20 21:50:38 +0300

    fix: Indentation

--- src/client/containers/Panoptic/sections/ColorTemperature/options.js
+++ src/client/containers/Panoptic/sections/ColorTemperature/options.js
@@ -1,21 +1,21 @@
 export const selectOneOptions = [
-	{ value: "happysad", label: "Happy/Sad" },
-	{ value: "energeticcalm", label: "Energetic/Calm" },
-	{ value: "syntheticnatural", label: "Sythentic/Natural" }
+  { value: "happysad", label: "Happy/Sad" },
+  { value: "energeticcalm", label: "Energetic/Calm" },
+  { value: "syntheticnatural", label: "Sythentic/Natural" }
 ];
 
 export const selectTwoOptions = [
-	{ value: "past24", label: "Past 24 Hours" },
-	{ value: "pastweek", label: "Past Week" },
-	{ value: "pastmonth", label: "Past Month" },
-	{ value: "past3month", label: "Past 3 Months" },
+  { value: "past24", label: "Past 24 Hours" },
+  { value: "pastweek", label: "Past Week" },
+  { value: "pastmonth", label: "Past Month" },
+  { value: "past3month", label: "Past 3 Months" },
 ];
 
 export const platforms = [
-	{name: 'Facebook', color: '#51adc0'},
-	{name: 'Instagram', color: '#ff556f'},
-	{name: 'Twitter', color: '#8567f0'},
-	{name: 'Snapchat', color: '#acb0be'},
-	{name: 'Youtube', color: '#ffffff'},
-	{name: 'Pinterest', color: '#242b49'},
+  {name: 'Facebook', color: '#51adc0'},
+  {name: 'Instagram', color: '#ff556f'},
+  {name: 'Twitter', color: '#8567f0'},
+  {name: 'Snapchat', color: '#acb0be'},
+  {name: 'Youtube', color: '#ffffff'},
+  {name: 'Pinterest', color: '#242b49'},
 ];
