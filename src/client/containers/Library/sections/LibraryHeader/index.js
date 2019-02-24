fbe4c780b6f1222c65213936aa17019b7497d667
Hami Çakır <hami.cakr@iyzi-sfd-32.local>
2019-02-24 15:54:07 +0300

    feat: Library page filter with search

--- src/client/containers/Library/sections/LibraryHeader/index.js
+++ src/client/containers/Library/sections/LibraryHeader/index.js
@@ -9,66 +9,65 @@ import { actions } from 'Reducers/library'
 import Button from 'Components/Form/Button'
 import Input from 'Components/Form/Input'
 
-class LibraryDetail extends React.Component {
-	constructor(props) {
-		super(props)
-	}
+class LibraryHeader extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-	async onChangeText(e) {
-		console.log('Event ', e.currentTarget);
-		const { getFilteredVideos } = this.props
-		await getFilteredVideos(e.target.value)
-	}
+  async onChangeText(e) {
+    e.preventDefault()
+    const { getFilteredVideos } = this.props
+    await getFilteredVideos(e.target[0].value)
+  }
 
-	render() {
-		const { setSidebarVisible } = this.props
-		console.log(this.props);
-		return (
-			<div className={style.headerContainer}>
-				<div>
-					<form onSubmit={(e) => this.onChangeText(e)}>
-					<Field
-						name="libraryFilterInput"
-						component={Input}
-						onChange={e => this.onChangeText(e)}
-						customClass={style.librarySearchInput}
-						placeholder="Search a video…"
-					/>
-					</form>
-				</div>
-				<div>
-					<h1 className="alpha color-white text-center font-primary text-bold">
-						Library
-					</h1>
-				</div>
-				<div>
-					<Button
-						onClick={() => setSidebarVisible(true)}
-						customClass="float-right font-secondary-first text-bold"
-						buttonText="Filter Videos"
-						iconRight="icon-Filter"
-					/>
-				</div>
-			</div>
-		)
-	}
+  render() {
+    const { setSidebarVisible } = this.props
+    console.log(this.props);
+    return (
+      <div className={style.headerContainer}>
+        <div>
+          <form onSubmit={ this.onChangeText.bind(this) }>
+            <Field
+              name="libraryFilterInput"
+              component={Input}
+              customClass={style.librarySearchInput}
+              placeholder="Search a video…"
+            />
+          </form>
+        </div>
+        <div>
+          <h1 className="alpha color-white text-center font-primary text-bold">
+            Library
+          </h1>
+        </div>
+        <div>
+          <Button
+            onClick={() => setSidebarVisible(true)}
+            customClass="float-right font-secondary-first text-bold"
+            buttonText="Filter Videos"
+            iconRight="icon-Filter"
+          />
+        </div>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => state;
 
 const mapDispatchToProps = dispatch => {
-	return {
-		getFilteredVideos: filterText => dispatch(actions.filterVideos(filterText))
-	}
+  return {
+    getFilteredVideos: filterText => dispatch(actions.filterVideos(filterText))
+  }
 }
 
 const withConnect = connect(
-	mapStateToProps,
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 );
 
 export default compose(
-	reduxForm({
-		form: 'LibraryHeaderForm'
-	}),
-	withConnect)(LibraryDetail)
+  reduxForm({
+    form: 'LibraryHeaderForm'
+  }),
+  withConnect)(LibraryHeader)
