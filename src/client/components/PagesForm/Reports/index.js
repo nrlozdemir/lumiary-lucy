ecd088419670e515e991e599e691a70bc82c8331
serdarbasol <serdarbasol111@gmail.com>
2019-02-25 12:43:36 +0300

    feat: added react-table and style

---
+++ src/client/components/PagesForm/Reports/index.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import { Field, reduxForm } from 'redux-form'
+import Select from 'Components/Form/Select'
+import Input from 'Components/Form/Input'
+import { compose } from 'redux'
+
+import style from './style.scss'
+
+import {
+  selectOptionsBrand,
+  selectOptionsSocial,
+  selectOptionsEngagement,
+  selectOptionsDateRange,
+} from './options'
+
+const ReportsForm = ({}) => {
+  return (
+    <form onSubmit={() => console.log('object')}>
+      <div className={style.formArea}>
+        <div className={style.selectionArea}>
+          <p className={style.label}>Choose a Brand</p>
+          <Field
+            component={Select}
+            options={selectOptionsBrand}
+            id="brand"
+            name="brand"
+            placeholder="Select One"
+            label="Choose a Brand"
+          />
+        </div>
+        <div className={style.selectionArea}>
+          <p className={style.label}>Social Platform</p>
+          <Field
+            component={Select}
+            options={selectOptionsSocial}
+            id="social"
+            name="social"
+            placeholder="Select One"
+            label="Social Platform"
+          />
+        </div>
+        <div className={style.selectionArea}>
+          <p className={style.label}>Engagement</p>
+          <Field
+            component={Select}
+            options={selectOptionsEngagement}
+            id="engagement"
+            name="engagement"
+            placeholder="Select One"
+            label="Engagement"
+          />
+        </div>
+        <div className={style.selectionArea}>
+          <p className={style.label}>Date Range</p>
+          <Field
+            component={Select}
+            options={selectOptionsDateRange}
+            id="date"
+            name="date"
+            placeholder="Select Date"
+            label="Date Range"
+          />
+        </div>
+        <div className={style.selectionArea}>
+          <p className={style.label}>Title</p>
+          <Field
+            component={Input}
+            options={selectOptionsDateRange}
+            id="title"
+            name="title"
+            placeholder="Enter title name"
+            label="Date Range"
+          />
+        </div>
+        <button className={style.selectionLink} type="submit" disabled={true}>
+          Generate Report
+          <div className={style.icon}>
+            <span className="icon-Right-Arrow-Circle">
+              <span className="path1" />
+              <span className="path2" />
+              <span className="path3" />
+            </span>
+          </div>
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default compose(
+  reduxForm({
+    form: 'ReportsForm',
+  })
+)(ReportsForm)
--- src/client/containers/Reports/index.js
+++ src/client/containers/Reports/index.js
@@ -1,30 +1,53 @@
 import React, { Component } from 'react'
-import { Field, reduxForm } from 'redux-form'
-import Select from 'Components/Form/Select'
-import Input from 'Components/Form/Input'
-import { compose } from 'redux'
 
 import style from './style.scss'
+import { selectOptions } from './options'
 
-import {
-  selectOptionsBrand,
-  selectOptionsSocial,
-  selectOptionsEngagement,
-  selectOptionsDateRange,
-} from './options'
+import Select from 'Components/Form/Select'
+
 import ReportsModal from 'Components/Modal/reports'
+import ReportsForm from 'Components/PagesForm/Reports'
+
+import ReactTable from 'react-table'
 
 class Reports extends Component {
   constructor(props) {
     super(props)
     this.state = {
       modalIsOpen: false,
-      title: '',
+      data: [
+        {
+          title: 'Show this report to Steve on Monday',
+          category: 'Brands Insights',
+          platform: 'Facebook',
+          date: '2/11/19',
+        },
+        {
+          title: 'Bleacher Report VS ESPN comparison',
+          category: 'Compare Brands',
+          platform: 'Instagram',
+          date: '2/10/19',
+        },
+        {
+          title: 'Fansided vs Scoutmedia on Facebook',
+          category: 'Compare Brands',
+          platform: 'Twitter',
+          date: '2/8/19',
+        },
+        {
+          title: 'Videos with baseball pitchers losing control',
+          category: 'Predefined Reports',
+          platform: 'Facebook',
+          date: '2/4/19',
+        },
+        {
+          title: 'Bleacher Report performance on Instagram',
+          category: 'Brands Insights',
+          platform: 'Facebook',
+          date: '2/1/19',
+        },
+      ],
     }
-
-    this.openModal = this.openModal.bind(this)
-    this.afterOpenModal = this.afterOpenModal.bind(this)
-    this.closeModal = this.closeModal.bind(this)
   }
 
   openModal() {
@@ -39,115 +62,139 @@ class Reports extends Component {
     this.setState({ modalIsOpen: false })
   }
 
-  onChange(e) {
-    const value = e.target.value
-    this.setState({
-      title: value,
-    })
+  editRow(value) {
+    console.log(value.viewIndex)
   }
 
   render() {
-    const { title, modalIsOpen } = this.state
+    const { modalIsOpen, data } = this.state
     console.log('modalIsOpen', modalIsOpen)
+
     return (
       <div className="grid-container col-12 mr-40 ml-40 mt-72 mb-72">
         <div className={style.reportsContainer}>
-          <h1>Reports Page</h1>
-          <button onClick={() => this.openModal()}>Open modal</button>
-
+          {/* <button onClick={() => this.openModal()}>Open modal</button> */}
           <ReportsModal
             width={440}
             isOpen={modalIsOpen}
             closeTimeoutMS={300}
-            onAfterOpen={this.afterOpenModal}
-            onRequestClose={this.closeModal}
+            onAfterOpen={() => this.afterOpenModal()}
+            onRequestClose={() => this.closeModal()}
             isClosable={true}
-            shouldCloseOnEsc={false}
+            shouldCloseOnEsc={true}
             shouldCloseOnOverlayClick={false}
             title="Brand Insights"
           >
-            <div>
-              <form onSubmit={() => console.log('object')}>
-                <div className={style.formArea}>
-                  <div className={style.selectionArea}>
-                    <p className={style.label}>Choose a Brand</p>
-                    <Field
-                      component={Select}
-                      options={selectOptionsBrand}
-                      id="brand"
-                      name="brand"
-                      placeholder="Select One"
-                      label="Choose a Brand"
-                    />
-                  </div>
-                  <div className={style.selectionArea}>
-                    <p className={style.label}>Social Platform</p>
-                    <Field
-                      component={Select}
-                      options={selectOptionsSocial}
-                      id="social"
-                      name="social"
-                      placeholder="Select One"
-                      label="Social Platform"
-                    />
-                  </div>
-                  <div className={style.selectionArea}>
-                    <p className={style.label}>Engagement</p>
-                    <Field
-                      component={Select}
-                      options={selectOptionsEngagement}
-                      id="engagement"
-                      name="engagement"
-                      placeholder="Select One"
-                      label="Engagement"
-                    />
-                  </div>
-                  <div className={style.selectionArea}>
-                    <p className={style.label}>Date Range</p>
-                    <Field
-                      component={Select}
-                      options={selectOptionsDateRange}
-                      id="date"
-                      name="date"
-                      placeholder="Select Date"
-                      label="Date Range"
-                    />
-                  </div>
-                  <div className={style.selectionArea}>
-                    <p className={style.label}>Title</p>
-                    <Input
-                      placeholder="Enter title name"
-                      onChange={(e) => this.onChange(e)}
-                      value={title}
-                      customClass={style.titleInput}
-                    />
-                  </div>
-                  <button
-                    className={style.selectionLink}
-                    type="submit"
-                    disabled={true}
-                  >
-                    Generate Report
-                    <div className={style.icon}>
-                      <span className="icon-Right-Arrow-Circle">
-                        <span className="path1" />
-                        <span className="path2" />
-                        <span className="path3" />
-                      </span>
-                    </div>
-                  </button>
-                </div>
-              </form>
-            </div>
+            <ReportsForm />
           </ReportsModal>
+
+          <div className={style.reportsCardContainer}>
+            <div className={style.reportsCard}>
+              <p className={style.cardTitle}>Brand Insights</p>
+              <div className={style.cardIcons}>
+                <img
+                  src="https://s3.amazonaws.com/quickframe-static-dev/lucy-assets/brand-insights-icon.svg"
+                  alt="brand-insights"
+                />
+              </div>
+              <p className={style.cardDescription}>
+                Get helpful, detailed engagement metrics about competitor brands
+              </p>
+            </div>
+            <div className={style.reportsCard}>
+              <p className={style.cardTitle}>Compare Brands</p>
+              <div className={style.cardIcons}>
+                <img
+                  src="https://s3.amazonaws.com/quickframe-static-dev/lucy-assets/compare-brands-icon.svg"
+                  alt="compare-brands"
+                />
+              </div>
+              <p className={style.cardDescription}>
+                Compare two brands to learn what’s driving their performance
+              </p>
+            </div>
+            <div className={style.reportsCard}>
+              <p className={style.cardTitle}>Properties</p>
+              <div className={style.cardIcons}>
+                <img
+                  src="https://s3.amazonaws.com/quickframe-static-dev/lucy-assets/properties-icon.svg"
+                  alt="properties"
+                />
+              </div>
+              <p className={style.cardDescription}>
+                View detailed, comparative data of video attributes across
+                brands
+              </p>
+            </div>
+            <div className={style.reportsCard}>
+              <p className={style.cardTitle}>Predefined Reports</p>
+              <div className={style.cardIcons}>
+                <img
+                  src="https://s3.amazonaws.com/quickframe-static-dev/lucy-assets/predefined-reports-icon.svg"
+                  alt="predefined-reports"
+                />
+              </div>
+              <p className={style.cardDescription}>
+                Access unique, relevant and invaluable customized data
+              </p>
+            </div>
+          </div>
+
+          <div className={style.reportsTableContainer}>
+            <div className={style.reportsTableHeader}>
+              <p>Saved Reports</p>
+              <div className={style.inputWrapper}>
+                <Select
+                  options={selectOptions}
+                  id="SelectReports"
+                  name="SelectReports"
+                  placeholder="Select One"
+                  label="Select a report"
+                />
+              </div>
+            </div>
+            <div className={style.reportsTableBody}>
+              <ReactTable
+                data={data}
+                showPagination={false}
+                defaultPageSize={4}
+                multiSort={true}
+                sortable={true}
+                columns={[
+                  {
+                    Header: 'Title',
+                    accessor: 'title',
+                  },
+                  {
+                    Header: 'Category',
+                    accessor: 'category',
+                  },
+                  {
+                    Header: 'Platform',
+                    accessor: 'platform',
+                  },
+                  {
+                    Header: 'Date',
+                    accessor: 'date',
+                  },
+                  {
+                    Header: null,
+                    width: 65,
+                    Cell: ({ viewIndex }) => (
+                      <span
+                        className={style.deleteIcon}
+                        onClick={() => this.editRow({ viewIndex })}
+                      />
+                    ),
+                  },
+                ]}
+              />
+            </div>
+          </div>
         </div>
       </div>
     )
   }
 }
 
-export default compose(
-  reduxForm({
-    form: 'Reports',
-  })
-)(Reports)
+export default Reports
