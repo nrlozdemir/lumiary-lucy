89836593abed6a3397b74bea34e3c4003bce886a
serdarbasol <serdarbasol111@gmail.com>
2019-02-27 02:50:01 +0300

    fix: fixed wrong pathname

--- src/client/components/Modal/Reports/index.js
+++ src/client/components/Modal/Reports/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Modal from 'react-modal'
-// import classnames from 'classnames'
 import style from './style.scss'
 
 const ReportsModal = ({
