f1ce4ab0aa6e91d4c1177b125c10358f7d7026c2
Yusuf Bakır <f.yusufbakir@gmail.com>
2019-03-07 20:47:23 +0300

    feat: Added title bar on the videoComponent

--- src/client/containers/LibraryDetail/sections/LibraryDetailChartHeader/index.js
+++ src/client/containers/LibraryDetail/sections/LibraryDetailChartHeader/index.js
@@ -5,77 +5,77 @@ import style from './style.scss'
 import { barDataOptions } from './options'
 import Video from '../VideoComponent'
 
-const LibraryDetailChartHeader = ({ barData, videoUrl }) => (
-  <div className="grid-container mr-20 ml-20 mt-72" style={{display: 'flex'}}>
-    <div className="col-6" style={{position: 'relative'}}>
-      <Video src={videoUrl} />
-    </div>
-    <div className="col-6 bg-dark-grey-blue shadow-1">
-      <div className={style.chartHeader}>
-        <div className="col-6-no-gutters">
-          <div className={style.socialIcons}>
-            <div className="col-4">Published</div>
-            <div className="col-8">
-              <span className="icon-Facebook-Bubble" />
-              <span className="icon-Instagram-Bubble" />
-              <span className="icon-Twitter-Bubble" />
-              <span className="icon-YouTube-Bubble" />
-              <span className="icon-Pinterest-Bubble" />
-            </div>
-          </div>
-        </div>
-        <div className="col-6">
-          <div className={style.legend}>
-            <div className="col-6-no-gutters">
-              <div className="float-right mr-16">
-                <span className="bg-coral-pink" />
-                This video
+const LibraryDetailChartHeader = ({ barData, videoUrl, title, socialIcon }) => (
+	<div className="grid-container mr-20 ml-20 mt-72" style={{ display: 'flex' }}>
+		<div className="col-6" style={{ position: 'relative' }}>
+			<Video src={videoUrl} title={title} socialIcon={socialIcon} />
+		</div>
+		<div className="col-6 bg-dark-grey-blue shadow-1">
+			<div className={style.chartHeader}>
+				<div className="col-6-no-gutters">
+					<div className={style.socialIcons}>
+						<div className="col-4">Published</div>
+						<div className="col-8">
+							<span className="icon-Facebook-Bubble" />
+							<span className="icon-Instagram-Bubble" />
+							<span className="icon-Twitter-Bubble" />
+							<span className="icon-YouTube-Bubble" />
+							<span className="icon-Pinterest-Bubble" />
+						</div>
+					</div>
+				</div>
+				<div className="col-6">
+					<div className={style.legend}>
+						<div className="col-6-no-gutters">
+							<div className="float-right mr-16">
+								<span className="bg-coral-pink" />
+								This video
               </div>
+						</div>
+						<div className="col-6-no-gutters">
+							<span className="bg-cool-blue" />
+							Average Video
             </div>
-            <div className="col-6-no-gutters">
-              <span className="bg-cool-blue" />
-              Average Video
-            </div>
-          </div>
-        </div>
-      </div>
-      <Bar data={barData} width={500} options={barDataOptions} height={185} />
-      <div className={style.chartLabels}>
-        <div className={style.label}>
-          <span className="font-primary text-bold font-size-24 display-block">
-            827.8k
+					</div>
+				</div>
+			</div>
+			<Bar data={barData} width={500} options={barDataOptions} height={185} />
+			<div className={style.chartLabels}>
+				<div className={style.label}>
+					<span className="font-primary text-bold font-size-24 display-block">
+						827.8k
           </span>
-          <span className="color-cool-grey font-secondary-second font-size-12 display-block">
-            Views
+					<span className="color-cool-grey font-secondary-second font-size-12 display-block">
+						Views
           </span>
-        </div>
-        <div className={style.label}>
-          <span className="font-primary text-bold font-size-24 display-block">
-            481.7k
+				</div>
+				<div className={style.label}>
+					<span className="font-primary text-bold font-size-24 display-block">
+						481.7k
           </span>
-          <span className="color-cool-grey font-secondary-second font-size-12 display-block">
-            Likes
+					<span className="color-cool-grey font-secondary-second font-size-12 display-block">
+						Likes
           </span>
-        </div>
-        <div className={style.label}>
-          <span className="font-primary text-bold font-size-24 display-block">
-            265.2k
+				</div>
+				<div className={style.label}>
+					<span className="font-primary text-bold font-size-24 display-block">
+						265.2k
           </span>
-          <span className="color-cool-grey font-secondary-second font-size-12 display-block">
-            Comments
+					<span className="color-cool-grey font-secondary-second font-size-12 display-block">
+						Comments
           </span>
-        </div>
-        <div className={style.label}>
-          <span className="font-primary text-bold font-size-24 display-block">
-            126.3k
+				</div>
+				<div className={style.label}>
+					<span className="font-primary text-bold font-size-24 display-block">
+						126.3k
           </span>
-          <span className="color-cool-grey font-secondary-second font-size-12 display-block">
-            Shares
+					<span className="color-cool-grey font-secondary-second font-size-12 display-block">
+						Shares
           </span>
-        </div>
-      </div>
-    </div>
-  </div>
+				</div>
+			</div>
+		</div>
+	</div>
 )
 
 export default LibraryDetailChartHeader
--- src/client/containers/LibraryDetail/sections/VideoComponent/index.js
+++ src/client/containers/LibraryDetail/sections/VideoComponent/index.js
@@ -1,61 +1,67 @@
 import React, { Component } from 'react'
 import classnames from "classnames"
 import styles from './style.scss'
+import { socialIconSelector } from "Utils";
 
 class Video extends Component {
-  componentDidMount() {
-    var videoPlayButton,
-      videoWrapper = document.getElementsByClassName('video-wrapper')[0],
-      video = document.getElementsByTagName('video')[0],
-      videoMethods = {
-        renderVideoPlayButton: function() {
-          if (videoWrapper.contains(video)) {
-            this.formatVideoPlayButton()
-            video.classList.add('has-media-controls-hidden')
-            videoPlayButton = document.getElementsByClassName(
-              'video-overlay-play-button'
-            )[0]
-            videoPlayButton.addEventListener('click', this.hideVideoPlayButton)
-          }
-        },
+	componentDidMount() {
+		var videoPlayButton,
+			videoWrapper = document.getElementsByClassName('video-wrapper')[0],
+			video = document.getElementsByTagName('video')[0],
+			videoMethods = {
+				renderVideoPlayButton: function () {
+					if (videoWrapper.contains(video)) {
+						this.formatVideoPlayButton()
+						video.classList.add('has-media-controls-hidden')
+						videoPlayButton = document.getElementsByClassName(
+							'video-overlay-play-button'
+						)[0]
+						videoPlayButton.addEventListener('click', this.hideVideoPlayButton)
+					}
+				},
 
-        formatVideoPlayButton: function() {
-          videoWrapper.insertAdjacentHTML(
-            'beforeend',
-            '\
+				formatVideoPlayButton: function () {
+					videoWrapper.insertAdjacentHTML(
+						'beforeend',
+						'\
                 <svg class="video-overlay-play-button" viewBox="0 0 200 200" alt="Play video">\
                     <circle cx="100" cy="100" r="90" fill="none" stroke-width="15" stroke="#fff"/>\
                     <polygon points="70, 55 70, 145 145, 100" fill="#fff"/>\
                 </svg>\
             '
-          )
-        },
+					)
+				},
 
-        hideVideoPlayButton: function() {
-          video.play()
-          videoPlayButton.classList.add('is-hidden')
-          video.classList.remove('has-media-controls-hidden')
-          video.setAttribute('controls', 'controls')
-        },
-      }
+				hideVideoPlayButton: function () {
+					video.play()
+					videoPlayButton.classList.add('is-hidden')
+					video.classList.remove('has-media-controls-hidden')
+					//video.setAttribute('controls', 'controls')
+				},
+			}
 
-    videoMethods.renderVideoPlayButton()
-  }
+		videoMethods.renderVideoPlayButton()
+	}
 
-  render() {
-		const { src, poster = '', style, className } = this.props;
+	render() {
+		const { src, poster = '', title, socialIcon, style, className } = this.props;
 		const classes = classnames('video-wrapper', className, styles.container);
 
-    return (
-      <div className={classes} style={{...style}}>
-        <video
+		const iconClass = classnames(
+			socialIconSelector(socialIcon) + " " + styles.icon
+		);
+
+		return (
+			<div className={classes} style={{ ...style }}>
+				<video
 					className={styles.video}
-          src={src}
-          poster={poster}
-        />
-      </div>
-    )
-  }
+					src={src}
+					poster={poster}
+				/>
+				<div className={styles.bar}><span className={iconClass} /> {title}</div>
+			</div>
+		)
+	}
 }
 
 export default Video
