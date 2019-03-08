1c2a6cac05166480b0946479d7dc0f8739fb2e8a
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2019-03-08 18:48:36 +0300

    feat: slider component styles fixed

--- src/client/components/Form/Range/index.js
+++ src/client/components/Form/Range/index.js
@@ -1,85 +1,112 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import classnames from "classnames";
-import PropTypes from "prop-types";
-import style from "./styles.scss";
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import classnames from 'classnames'
+import PropTypes from 'prop-types'
+import style from './styles.scss'
 
-import Slider from "rc-slider";
+import Slider from 'rc-slider'
 
-const RangeSlider = Slider.Range;
+const RangeSlider = Slider.Range
 
 const defaultHandleStyle = [
-	{
-		width: "32px",
-		height: "32px",
-		marginTop: "-8px",
-		borderColor: "#fff"
-	},
-	{
-		width: "32px",
-		height: "32px",
-		marginTop: "-8px",
-		borderColor: "#fff"
-	}
-];
+  {
+    width: '32px',
+    height: '32px',
+    marginTop: '-8px',
+    borderColor: '#fff',
+  },
+  {
+    width: '32px',
+    height: '32px',
+    marginTop: '-8px',
+    borderColor: '#fff',
+  },
+]
 
 const defaultTrackStyle = [
-	{
-		height: "16px",
-		backgroundColor: "#51adc0"
-	}
-];
+  {
+    height: '16px',
+    backgroundColor: '#51adc0',
+  },
+]
 
 const defaultRailStyle = {
-	height: "16px",
-	borderRadius: "8px",
-	backgroundColor: "#242b49"
-};
-
-class Range extends Component {
-	constructor(props) {
-		super(props);
-		const { minValue, maxValue } = this.props;
-		this.state = {
-			value: [minValue, maxValue]
-		};
-	}
-
-	onSliderChange(value) {
-		const {input: {onChange = () => {}}} = this.props;
-
-		this.setState({
-			value
-		});
-
-		onChange(value);
-	}
-
-	render() {
-		const { className, input: {onChange}, minValue, maxValue, handleStyle = defaultHandleStyle, trackStyle = defaultTrackStyle, railStyle = defaultRailStyle } = this.props;
-
-		const { value } = this.state;
-
-		return (
-			<div className="range-slider">
-				<RangeSlider
-					allowCross={false}
-					value={value}
-					defaultValue={value}
-					handleStyle={handleStyle}
-					trackStyle={trackStyle}
-					railStyle={railStyle}
-					onChange={this.onSliderChange.bind(this)}
-				/>
-				<div className="d-flex align-items-center justify-space-between mt-24 ticks">
-					<span>0-15s</span>
-					<span>15-30s</span>
-					<span>30-60s</span>
-					<span>60s+</span>
-				</div>
-			</div>
-		);
-	}
+  height: '16px',
+  borderRadius: '8px',
+  backgroundColor: '#242b49',
 }
 
-export default Range;
+class Range extends Component {
+  constructor(props) {
+    super(props)
+    const { minValue, maxValue } = this.props
+    this.state = {
+      value: [minValue, maxValue],
+    }
+  }
+
+  onSliderChange(value) {
+    const {
+      input: { onChange = () => {} },
+    } = this.props
+
+    this.setState({
+      value,
+    })
+
+    onChange(value)
+  }
+
+  render() {
+    const {
+      className,
+      input: { onChange },
+      minValue,
+      maxValue,
+      handleStyle = defaultHandleStyle,
+      trackStyle = defaultTrackStyle,
+      railStyle = defaultRailStyle,
+      dotStyle,
+      customTicksUnvisible,
+      min,
+      max,
+      step,
+      dots,
+      marks,
+      customClass,
+    } = this.props
+
+    const { value } = this.state
+
+    return (
+      <div className="range-slider">
+        <RangeSlider
+          className={customClass}
+          allowCross={false}
+          value={value}
+          defaultValue={value}
+          handleStyle={handleStyle}
+          trackStyle={trackStyle}
+          railStyle={railStyle}
+          dotStyle={dotStyle}
+          onChange={this.onSliderChange.bind(this)}
+          min={min}
+          max={max}
+          step={step}
+          dots={dots}
+          marks={marks}
+        />
+        {customTicksUnvisible ? null : (
+          <div className="d-flex align-items-center justify-space-between mt-24 ticks">
+            <span>0-15s</span>
+            <span>15-30s</span>
+            <span>30-60s</span>
+            <span>60s+</span>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default Range
--- src/client/containers/Audience/sections/Performance/index.js
+++ src/client/containers/Audience/sections/Performance/index.js
@@ -1,175 +1,326 @@
-import React from 'react';
-import { BubbleChart, Bubble, Visual, ToolTip } from "@saypr/bubble-chart/react";
+import React from 'react'
+import { BubbleChart, Bubble, Visual, ToolTip } from '@saypr/bubble-chart/react'
 // import { Range } from "rc-slider";
 
-import Select from 'Components/Form/Select';
-import Range from 'Components/Form/Range';
+import Select from 'Components/Form/Select'
+import Range from 'Components/Form/Range'
 
-import style from 'Containers/Audience/style.scss';
-import { socialIconSelector } from "Utils";
-
-const bubblesMales = [
-	{
-		value: 30,
-		visual: 'youtube'
-	},
-	{
-		value: 25,
-		visual: 'facebook'
-	},
-	{
-		value: 20,
-		visual: 'twitter'
-	},
-	{
-		value: 15,
-		visual: 'pinterest'
-	},
-	{
-		value: 10,
-		visual: 'instagram'
-	}
-];
-
-const bubblesFemales = [
-	{
-		value: 20,
-		visual: 'youtube'
-	},
-	{
-		value: 35,
-		visual: 'facebook'
-	},
-	{
-		value: 20,
-		visual: 'twitter'
-	},
-	{
-		value: 15,
-		visual: 'pinterest'
-	},
-	{
-		value: 10,
-		visual: 'instagram'
-	}
-];
-
-const bubblesBoth = [
-	{
-		value: 20,
-		visual: 'youtube'
-	},
-	{
-		value: 25,
-		visual: 'facebook'
-	},
-	{
-		value: 20,
-		visual: 'twitter'
-	},
-	{
-		value: 15,
-		visual: 'pinterest'
-	},
-	{
-		value: 20,
-		visual: 'instagram'
-	}
-];
+import style from 'Containers/Audience/style.scss'
+import { socialIconSelector } from 'Utils'
 
 export class Performance extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props)
+    this.state = {
+      slider: [0, 100],
+      bubblesMales: [
+        {
+          value: 30,
+          visual: 'youtube',
+        },
+        {
+          value: 25,
+          visual: 'facebook',
+        },
+        {
+          value: 20,
+          visual: 'twitter',
+        },
+        {
+          value: 15,
+          visual: 'pinterest',
+        },
+        {
+          value: 10,
+          visual: 'instagram',
+        },
+      ],
+      bubblesFemales: [
+        {
+          value: 20,
+          visual: 'youtube',
+        },
+        {
+          value: 35,
+          visual: 'facebook',
+        },
+        {
+          value: 20,
+          visual: 'twitter',
+        },
+        {
+          value: 15,
+          visual: 'pinterest',
+        },
+        {
+          value: 10,
+          visual: 'instagram',
+        },
+      ],
+      bubblesBoth: [
+        {
+          value: 20,
+          visual: 'youtube',
+        },
+        {
+          value: 25,
+          visual: 'facebook',
+        },
+        {
+          value: 20,
+          visual: 'twitter',
+        },
+        {
+          value: 15,
+          visual: 'pinterest',
+        },
+        {
+          value: 20,
+          visual: 'instagram',
+        },
+      ],
+    }
+  }
 
-		this.state = {
-			slider: [0, 100]
-		};
-	}
+  updateSlider() {
+    const randomOne = [
+      {
+        value: Math.floor(Math.random() * 101),
+        visual: 'youtube',
+      },
+      {
+        value: Math.floor(Math.random() * 101),
+        visual: 'facebook',
+      },
+      {
+        value: Math.floor(Math.random() * 101),
+        visual: 'twitter',
+      },
+      {
+        value: Math.floor(Math.random() * 101),
+        visual: 'pinterest',
+      },
+      {
+        value: Math.floor(Math.random() * 101),
+        visual: 'instagram',
+      },
+    ]
+    const randomTwo = [
+      {
+        value: Math.floor(Math.random() * 101),
+        visual: 'youtube',
+      },
+      {
+        value: Math.floor(Math.random() * 101),
+        visual: 'facebook',
+      },
+      {
+        value: Math.floor(Math.random() * 101),
+        visual: 'twitter',
+      },
+      {
+        value: Math.floor(Math.random() * 101),
+        visual: 'pinterest',
+      },
+      {
+        value: Math.floor(Math.random() * 101),
+        visual: 'instagram',
+      },
+    ]
+    const randomTree = [
+      {
+        value: Math.floor(Math.random() * 101),
+        visual: 'youtube',
+      },
+      {
+        value: Math.floor(Math.random() * 101),
+        visual: 'facebook',
+      },
+      {
+        value: Math.floor(Math.random() * 101),
+        visual: 'twitter',
+      },
+      {
+        value: Math.floor(Math.random() * 101),
+        visual: 'pinterest',
+      },
+      {
+        value: Math.floor(Math.random() * 101),
+        visual: 'instagram',
+      },
+    ]
+    this.setState({
+      bubblesMales: randomOne,
+      bubblesFemales: randomTwo,
+      bubblesBoth: randomTree,
+    })
+  }
 
-	updateSlider([min, max]) {
-		this.setState({slider: [min, max]});
-	}
+  render() {
+    const {
+      slider: [min, max],
+      bubblesBoth,
+      bubblesFemales,
+      bubblesMales,
+    } = this.state
+    console.log(this.state)
+    const handleStyle = [
+      {
+        width: '10px',
+        height: '10px',
+        borderStyle: 'solid',
+        borderWidth: '25px 10px 0 10px',
+        borderColor: ' #ffffff transparent transparent transparent',
+        borderRadius: 0,
+        backgroundColor: 'transparent',
+        boxShadow: 'none',
+      },
+      {
+        width: '10px',
+        height: '10px',
+        borderStyle: 'solid',
+        borderWidth: '25px 10px 0 10px',
+        borderColor: '#ffffff transparent transparent transparent',
+        backgroundColor: 'transparent',
+        borderRadius: 0,
+        boxShadow: 'none',
+        marginLeft: '-12px',
+      },
+    ]
+    const trackStyle = [
+      {
+        height: '16px',
+        backgroundColor: '#acb0be',
+      },
+    ]
+    const railStyle = {
+      height: '16px',
+      borderRadius: '8px',
+      backgroundColor: '#242b49',
+    }
 
-	render() {
-		const {slider: [min, max]} = this.state;
+    const dotStyle = {
+      border: '0',
+      width: '1px',
+      height: '16px',
+      backgroundColor: '#acb0be',
+      top: 0,
+      marginLeft: 0,
+    }
 
-		return (
-			<div className="grid-container mr-20 ml-20 mt-72 bg-dark-grey-blue shadow-1">
-				<div className={style.cardTitle + ' col-12'}>
-					<span>Performance By Age, Gender and Date</span>
-					<div className={style.selects}>
-						<Select
-							name="views"
-							customClass="custom-select"
-							placeholder="Select Views"
-							// value={views || ''}
-							value={''}
-							onChange={(option) => this.handleChange(option, 'views')}
-							options={[{ value: 'Views', label: 'Views' }, { value: 'Comments', label: 'Comments' }]}
-						/>
-						<Select
-							name="platforms"
-							customClass="custom-select"
-							placeholder="Select Platforms"
-							// value={platforms || ''}
-							value={''}
-							onChange={(option) => this.handleChange(option, 'platforms')}
-							options={[{ value: 'All Platforms', label: 'All Platforms' }]}
-						/>
-					</div>
-				</div>
-				<div className={'col-12'}>
-					<div className={'col-4'}>
-						<div className={style.bubbleCont}>
-							<BubbleChart size={[800, 600]} fromPercentages={true}>
-								{
-									bubblesMales.map((bubble, i) => (
-										<Bubble key={'bubble-' + i} radius={bubble.value} fill="#303a5d" stroke="#d0506c">
-											<Visual><span className={socialIconSelector(bubble.visual) + ' ' + style.bubbleVisual}></span></Visual>
-										</Bubble>
-									))
-								}
-							</BubbleChart>
-							<div className={style.label}>
-								<span>Males</span>
-							</div>
-						</div>
-					</div>
-					<div className={'col-4'}>
-						<div className={style.bubbleCont}>
-							<BubbleChart size={[800, 600]} fromPercentages={true}>
-								{
-									bubblesFemales.map((bubble, i) => (
-										<Bubble key={'bubble-' + i} radius={bubble.value} fill="#303a5d" stroke="#51adc0">
-											<Visual><span className={socialIconSelector(bubble.visual) + ' ' + style.bubbleVisual}></span></Visual>
-										</Bubble>
-									))
-								}
-							</BubbleChart>
-							<div className={style.label}>
-								<span>Females</span>
-							</div>
-						</div>
-					</div>
-					<div className={'col-4'}>
-						<div className={style.bubbleCont}>
-							<BubbleChart size={[800, 600]} fromPercentages={true}>
-								{
-									bubblesBoth.map((bubble, i) => (
-										<Bubble key={'bubble-' + i} radius={bubble.value} fill="#303a5d" stroke="#8567f0">
-											<Visual><span className={socialIconSelector(bubble.visual) + ' ' + style.bubbleVisual}></span></Visual>
-										</Bubble>
-									))
-								}
-							</BubbleChart>
-							<div className={style.label}>
-								<span>Both</span>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className="col-12" style={{paddingBottom: 40}}>
-					{/* <Range
+    return (
+      <div className="grid-container mr-20 ml-20 mt-72 bg-dark-grey-blue shadow-1">
+        <div className={style.cardTitle + ' col-12'}>
+          <span>Performance By Age, Gender and Date</span>
+          <div className={style.selects}>
+            <Select
+              name="views"
+              customClass="custom-select"
+              placeholder="Select Views"
+              // value={views || ''}
+              value={''}
+              onChange={(option) => this.handleChange(option, 'views')}
+              options={[
+                { value: 'Views', label: 'Views' },
+                { value: 'Comments', label: 'Comments' },
+              ]}
+            />
+            <Select
+              name="platforms"
+              customClass="custom-select"
+              placeholder="Select Platforms"
+              // value={platforms || ''}
+              value={''}
+              onChange={(option) => this.handleChange(option, 'platforms')}
+              options={[{ value: 'All Platforms', label: 'All Platforms' }]}
+            />
+          </div>
+        </div>
+        <div className={'col-12'}>
+          <div className={'col-4'}>
+            <div className={style.bubbleCont}>
+              <BubbleChart size={[800, 600]} fromPercentages={true}>
+                {bubblesMales.map((bubble, i) => (
+                  <Bubble
+                    key={'bubble-' + i}
+                    radius={bubble.value}
+                    fill="#303a5d"
+                    stroke="#d0506c"
+                  >
+                    <Visual>
+                      <span
+                        className={
+                          socialIconSelector(bubble.visual) +
+                          ' ' +
+                          style.bubbleVisual
+                        }
+                      />
+                    </Visual>
+                  </Bubble>
+                ))}
+              </BubbleChart>
+              <div className={style.label}>
+                <span>Males</span>
+              </div>
+            </div>
+          </div>
+          <div className={'col-4'}>
+            <div className={style.bubbleCont}>
+              <BubbleChart size={[800, 600]} fromPercentages={true}>
+                {bubblesFemales.map((bubble, i) => (
+                  <Bubble
+                    key={'bubble-' + i}
+                    radius={bubble.value}
+                    fill="#303a5d"
+                    stroke="#51adc0"
+                  >
+                    <Visual>
+                      <span
+                        className={
+                          socialIconSelector(bubble.visual) +
+                          ' ' +
+                          style.bubbleVisual
+                        }
+                      />
+                    </Visual>
+                  </Bubble>
+                ))}
+              </BubbleChart>
+              <div className={style.label}>
+                <span>Females</span>
+              </div>
+            </div>
+          </div>
+          <div className={'col-4'}>
+            <div className={style.bubbleCont}>
+              <BubbleChart size={[800, 600]} fromPercentages={true}>
+                {bubblesBoth.map((bubble, i) => (
+                  <Bubble
+                    key={'bubble-' + i}
+                    radius={bubble.value}
+                    fill="#303a5d"
+                    stroke="#8567f0"
+                  >
+                    <Visual>
+                      <span
+                        className={
+                          socialIconSelector(bubble.visual) +
+                          ' ' +
+                          style.bubbleVisual
+                        }
+                      />
+                    </Visual>
+                  </Bubble>
+                ))}
+              </BubbleChart>
+              <div className={style.label}>
+                <span>Both</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12" style={{ paddingBottom: 40 }}>
+          {/* <Range
 						allowCross={false}
 						value={[min, max]}
 						dots={true}
@@ -188,36 +339,36 @@ export class Performance extends React.Component {
 							}
 						]}
 					/> */}
-					<Range minValue={0} maxValue={100} input={{}} />
-				</div>
-			</div>
-		)
-	}
+          <Range
+            customClass={'customRangeSlider'}
+            minValue={0}
+            maxValue={100}
+            input={{ onChange: () => this.updateSlider() }}
+            handleStyle={handleStyle}
+            trackStyle={trackStyle}
+            railStyle={railStyle}
+            min={0}
+            max={100}
+            dotStyle={dotStyle}
+            step={10}
+            dots={true}
+            marks={{
+              0: 0,
+              10: 10,
+              20: 20,
+              30: 30,
+              40: 40,
+              50: 50,
+              60: 60,
+              70: 70,
+              80: 80,
+              90: 90,
+              100: 100,
+            }}
+            customTicksUnvisible
+          />
+        </div>
+      </div>
+    )
+  }
 }
-
-// handleStyle={[
-// 	{
-// 		width: "32px",
-// 		height: "32px",
-// 		marginTop: "-8px",
-// 		borderColor: "#fff"
-// 	},
-// 	{
-// 		width: "32px",
-// 		height: "32px",
-// 		marginTop: "-8px",
-// 		borderColor: "#fff"
-// 	}
-// ]}
-// trackStyle={[
-// 	{
-// 		height: "16px",
-// 		backgroundColor: "#51adc0"
-// 	}
-// ]}
-// railStyle={{
-// 	height: "16px",
-// 	borderRadius: "8px",
-// 	backgroundColor: "#242b49"
-// }}
-
