db880d46e29d1be252bcc9867788cd809c5bc36c
serdarbasol <serdarbasol111@gmail.com>
2019-03-14 22:59:01 +0300

    feat: added video progressbar and hovering

--- src/client/components/VideoCard/index.js
+++ src/client/components/VideoCard/index.js
@@ -4,55 +4,162 @@
  *
  */
 
-import React from "react";
-import classnames from "classnames";
-import PropTypes from "prop-types";
-import style from "./style.scss";
-import { socialIconSelector } from "../../utils";
-/* eslint-disable react/prefer-stateless-function */
-const VideoCard = ({ video, options = options || {}, muted = true }) => {
+import React, { Component } from 'react'
+import classnames from 'classnames'
+import PropTypes from 'prop-types'
+import style from './style.scss'
+import { socialIconSelector } from '../../utils'
+import { Link } from 'react-router-dom'
+export class VideoCard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      firstLoad: true,
+      showVideo: false,
+      width: 0,
+      startProgress: false,
+      duration: 0,
+      ctx: undefined,
+    }
+  }
 
-	const cardContainerClass = classnames(style.cardContainer, {
-		["bg-dusk"]: !options.barColor,
-		["col-3"]: !options.size,
-		[`col-${options.size}`]: options.size,
-		[`bg-${options.barColor}`]: options.barColor,
-		[options.customClass]: options.customClass
-	});
+  grabFrame = () => {
+    return this.state.ctx.drawImage(
+      this.video,
+      -60,
+      -60,
+      this.video.clientWidth + 60,
+      this.video.clientHeight + 60
+    )
+  }
 
-	const iconClass = classnames(
-		socialIconSelector(video.socialIcon),
-		style.iconClass
-	);
+  componentDidMount() {
+    this.setState({
+      ctx: this.canvas.getContext('2d'),
+    })
+  }
 
-	const videoEl = React.createRef();
-	return (
-		<div
-			key={video.id}
-			className={cardContainerClass}
-			onMouseEnter={() => videoEl.current && videoEl.current.play()}
-			onMouseLeave={() => videoEl.current && videoEl.current.pause()}>
-			<div className={style.cardImage}>
-				<video className="img-responsive" ref={videoEl} muted={muted}>
-					<source src={video.videoUrl}></source>
-				</video>
-				<div className={style.overlay} />
-			</div>
-			<div className={style.cardBody}>
-				<div className={style.bodyHeader}>
-					<span className={style.title}>{video.title}</span>
-					<span className={style.iconWrapper}>
-						<i className={iconClass} />
-					</span>
-				</div>
-			</div>
-		</div>
-	);
-};
+  componentDidUpdate() {
+    if (this.video && this.state.firstLoad) {
+      const _this = this
+      this.video.addEventListener('loadeddata', function() {
+        _this.grabFrame()
+      })
+      this.setState({
+        firstLoad: false,
+      })
+    }
+  }
 
-VideoCard.propTypes = {
-	video: PropTypes.object,
-	options: PropTypes.object
-};
+  pausedFunction = () => {
+    this.grabFrame()
+    this.video.pause()
+    this.video.removeEventListener('timeupdate', null)
+    this.setState({
+      startProgress: false,
+      showVideo: false,
+    })
+  }
 
-export default VideoCard;
+  playedFunction = () => {
+    this.video.play()
+    const _this = this
+    this.setState({
+      showVideo: true,
+      startProgress: true,
+      duration: this.video.duration,
+    })
+    this.video.addEventListener(
+      'timeupdate',
+      function(e) {
+        _this.setState({
+          width: this.currentTime,
+        })
+      },
+      false
+    )
+  }
+
+  render() {
+    const { video, options = options || {}, muted = true } = this.props
+    const cardContainerClass = classnames(style.cardContainer, {
+      ['bg-dusk']: !options.barColor,
+      // ['col-3']: !options.size,
+      // [`col-${options.size}`]: options.size,
+      [`bg-${options.barColor}`]: options.barColor,
+      [options.customClass]: options.customClass,
+    })
+
+    const iconClass = classnames(
+      socialIconSelector(video.socialIcon),
+      style.iconClass
+    )
+
+    return (
+      <div
+        key={video.id}
+        className={cardContainerClass}
+        onMouseEnter={() => this.playedFunction()}
+        onMouseLeave={() => this.pausedFunction()}
+      >
+        <div
+          className={style.cardInner}
+          ref={(cardInner) => (this.cardInner = cardInner)}
+        >
+          <div className={style.cardImage}>
+            <canvas
+              className={classnames(
+                'img-responsive',
+                this.state.showVideo && style.hidden
+              )}
+              ref={(canvas) => (this.canvas = canvas)}
+            />
+            <video
+              className={classnames(
+                'img-responsive',
+                !this.state.showVideo ? style.hidden : style.show
+              )}
+              muted={muted}
+              ref={(video) => (this.video = video)}
+            >
+              <source src={video.videoUrl} type="video/mp4" />
+            </video>
+
+            {this.state.startProgress && (
+              <span
+                className={style.progressBar}
+                style={{
+                  width: `${(this.state.width * 100) / this.state.duration}%`,
+                }}
+              />
+            )}
+          </div>
+          <div className={classnames('bg-dusk', style.cardBody)}>
+            <div className={style.bodyHeader}>
+              <div className={style.cardInfo}>
+                <span className={style.iconWrapper}>
+                  <i className={iconClass} />
+                </span>
+                <span className={style.title}>{video.title}</span>
+              </div>
+              <div className={style.cardLink}>
+                <Link to={`/library/build-report/${video.id}`}>
+                  View Video Report
+                  <div className={style.icon}>
+                    <span className="icon-Right-Arrow-Circle">
+                      <span className="path1" />
+                      <span className="path2" />
+                      <span className="path3" />
+                    </span>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default VideoCard
