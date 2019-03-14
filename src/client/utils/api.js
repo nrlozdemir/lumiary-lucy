6d591253f10b0451661554417ee7655177a5df8e
Yusuf Bakır <f.yusufbakir@gmail.com>
2019-03-14 20:01:08 +0300

    fix: indent

--- src/client/utils/api.js
+++ src/client/utils/api.js
@@ -2,82 +2,80 @@ import axios from "axios";
 import { API_ROOT, API_VERSION } from "Utils/globals";
 
 export function handleResponse(response) {
-	if (!response.ok) {
-		const err = new Error(response.statusText);
-		err.status = response.status;
-		throw err;
-	}
-	return response.json();
+  if (!response.ok) {
+    const err = new Error(response.statusText);
+    err.status = response.status;
+    throw err;
+  }
+  return response.json();
 }
 
 export const buildApiUrl = (url, params) => {
-	let requestUrl = `${API_ROOT}/${API_VERSION}${url}`;
+  let requestUrl = `${API_ROOT}/${API_VERSION}${url}`;
 
-	if (params) {
-		let q = [];
-		for (let key in params) {
-			if (params[key].length) {
-				let subkeys = [];
-				for (let subkey in params[key]) {
-					subkeys.push(`${JSON.stringify(params[key][subkey])}`);
-				}
-				q.push(`${key}=[${subkeys}]`);
-			} else {
-				q.push(`${key}=${params[key]}`);
-			}
-		}
+  if (params) {
+    let q = [];
+    for (let key in params) {
+      if (params[key].length) {
+        let subkeys = [];
+        for (let subkey in params[key]) {
+          subkeys.push(`${JSON.stringify(params[key][subkey])}`);
+        }
+        q.push(`${key}=[${subkeys}]`);
+      } else {
+        q.push(`${key}=${params[key]}`);
+      }
+    }
 
-		requestUrl += `?${q.join("&")}`;
-	}
+    requestUrl += `?${q.join("&")}`;
+  }
 
-	return requestUrl;
+  return requestUrl;
 };
 
 export const baseRequest = (token, method) => {
-	return {
-		method: method || "POST",
-		headers: {
-			Authorization: `Bearer ${token}`
-		}
-	};
+  return {
+    method: method || "POST",
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  };
 };
 
 export function ajax({ method, params, token, url }) {
-	// console.log(
-	//   '===== Utils/Api/Request =====',
-	//   '\n method: ', method,
-	//   '\n params: ', params,
-	//   '\n token: ', token,
-	//   '\n url: ', url,
-	// )
+  // console.log(
+  //   '===== Utils/Api/Request =====',
+  //   '\n method: ', method,
+  //   '\n params: ', params,
+  //   '\n token: ', token,
+  //   '\n url: ', url,
+  // )
 
-	const edittedUrl =
-		url.indexOf("https://") > -1 ? url : `${API_ROOT}/${API_VERSION}${url}`;
+  const edittedUrl =
+    url.indexOf("https://") > -1 ? url : `${API_ROOT}/${API_VERSION}${url}`;
 
-	return axios({
-		method: method || "post",
-		headers: token ? { Authorization: `Bearer ${token}` } : null,
-		url: edittedUrl,
-		data: params ? params : null
-	})
-		.then(res => {
-			// console.log('===== Utils/Api/ajax; axios success =====\n', res);
-			return res;
-		})
-		.catch(err => {
-			// console.log('===== Utils/Api/ajax; axios error =====\n', err);
-			return err.response;
-		});
+  return axios({
+    method: method || "post",
+    headers: token ? { Authorization: `Bearer ${token}` } : null,
+    url: edittedUrl,
+    data: params ? params : null
+  })
+    .then(res => {
+      // console.log('===== Utils/Api/ajax; axios success =====\n', res);
+      return res;
+    })
+    .catch(err => {
+      // console.log('===== Utils/Api/ajax; axios error =====\n', err);
+      return err.response;
+    });
 }
 
 export function findIdDetail(data, detailId, detailName) {
-	const currentId = data.filter(({ id }) => id == detailId);
+  const currentId = data.filter(({ id }) => id == detailId);
 
-	console.log(currentId)
+  if (currentId && currentId.length) {
+    return currentId[0][detailName];
+  }
 
-	if (currentId && currentId.length) {
-		return currentId[0][detailName];
-	}
-
-	return {};
+  return {};
 }
--- src/server/routes/createMock.js
+++ src/server/routes/createMock.js
@@ -6,77 +6,77 @@ const root = path.dirname(require.main.filename);
 const mocks = 'client/api/mocks';
 
 const readFiles = filenames => {
-	return filenames.reduce((object, filename) => {
-		object[filename] = fs.readFileSync(path.join(root, `${mocks}/${filename}.json`), 'utf8');
-		return object;
-	}, {});
+  return filenames.reduce((object, filename) => {
+    object[filename] = fs.readFileSync(path.join(root, `${mocks}/${filename}.json`), 'utf8');
+    return object;
+  }, {});
 }
 
 const randomNumber = (min, max) => {
-	return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 const randomNumberArray = (lenght, min, max) => {
-	return [...Array(lenght)].map(() => randomNumber(min, max));
+  return [...Array(lenght)].map(() => randomNumber(min, max));
 }
 
 const randomPercentage = (lenght) => {
-	const numbers = randomNumberArray(lenght, 0, 100);
-	const total = numbers.reduce((total, value) => total + value)
+  const numbers = randomNumberArray(lenght, 0, 100);
+  const total = numbers.reduce((total, value) => total + value)
 
-	return numbers.map(value => Number(((value / total) * 100).toFixed(2)));
+  return numbers.map(value => Number(((value / total) * 100).toFixed(2)));
 }
 
 const random = file => {
-	let data = file;
+  let data = file;
 
-	data = data.replace(/"rN#(.*),(.*)#"/g, ($0, $1, $2) => {
-		return randomNumber(parseInt($1), parseInt($2));
-	})
+  data = data.replace(/"rN#(.*),(.*)#"/g, ($0, $1, $2) => {
+    return randomNumber(parseInt($1), parseInt($2));
+  })
 
-	data = data.replace(/"rNA#(.*),(.*),(.*)#"/g, ($0, $1, $2, $3) => {
-		return JSON.stringify(randomNumberArray(parseInt($1), parseInt($2), parseInt($3)));
-	})
+  data = data.replace(/"rNA#(.*),(.*),(.*)#"/g, ($0, $1, $2, $3) => {
+    return JSON.stringify(randomNumberArray(parseInt($1), parseInt($2), parseInt($3)));
+  })
 
-	data = data.replace(/"rP#(.*)#"/g, ($0, $1) => {
-		return JSON.stringify(randomPercentage(parseInt($1)));
-	})
+  data = data.replace(/"rP#(.*)#"/g, ($0, $1) => {
+    return JSON.stringify(randomPercentage(parseInt($1)));
+  })
 
-	data = data.replace(/image=(.*)"/g, ($0, $1) => {
-		return 'image=' + randomNumber(1, 100) + '"';
-	})
+  data = data.replace(/image=(.*)"/g, ($0, $1) => {
+    return 'image=' + randomNumber(1, 100) + '"';
+  })
 
-	return data;
+  return data;
 }
 
 const createMock = async (req, res) => {
-	let {
-		libraryMock,
-		libraryDetailColorTempMock: ColorTempMock,
-		libraryDetailDoughnutChartMock: DoughnutChartMock,
-		libraryDetailHeaderBarChartMock: HeaderBarChartMock,
-		libraryDetailShotByShotMock: ShotByShotMock,
-	} = readFiles([
-		"libraryMock",
-		"libraryDetailColorTempMock",
-		"libraryDetailDoughnutChartMock",
-		"libraryDetailHeaderBarChartMock",
-		"libraryDetailShotByShotMock",
-	])
+  let {
+    libraryMock,
+    libraryDetailColorTempMock: ColorTempMock,
+    libraryDetailDoughnutChartMock: DoughnutChartMock,
+    libraryDetailHeaderBarChartMock: HeaderBarChartMock,
+    libraryDetailShotByShotMock: ShotByShotMock,
+  } = readFiles([
+    "libraryMock",
+    "libraryDetailColorTempMock",
+    "libraryDetailDoughnutChartMock",
+    "libraryDetailHeaderBarChartMock",
+    "libraryDetailShotByShotMock",
+  ])
 
-	const mock = JSON.parse(libraryMock).map(library => ({
-		...library,
-		ColorTempMock: JSON.parse(random(ColorTempMock)),
-		DoughnutChartMock: JSON.parse(random(DoughnutChartMock)),
-		HeaderBarChartMock: JSON.parse(random(HeaderBarChartMock)),
-		ShotByShotMock: JSON.parse(random(ShotByShotMock))
-	}))
+  const mock = JSON.parse(libraryMock).map(library => ({
+    ...library,
+    ColorTempMock: JSON.parse(random(ColorTempMock)),
+    DoughnutChartMock: JSON.parse(random(DoughnutChartMock)),
+    HeaderBarChartMock: JSON.parse(random(HeaderBarChartMock)),
+    ShotByShotMock: JSON.parse(random(ShotByShotMock))
+  }))
 
-	fs.writeFile(path.join(root, `${mocks}/mock.json`), JSON.stringify(mock), (err) => {
-		if (err) console.log(err)
-	});
+  fs.writeFile(path.join(root, `${mocks}/mock.json`), JSON.stringify(mock), (err) => {
+    if (err) console.log(err)
+  });
 
-	res.redirect('../')
+  res.redirect('../')
 }
 
 module.exports = createMock;
