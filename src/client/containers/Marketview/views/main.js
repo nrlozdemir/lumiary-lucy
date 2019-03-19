a350d18045fc7761ae5b26eca9dd1cfb01bd5c5d
Ali Sahin Ozcelik <alisahinozcelik@gmail.com>
2019-03-19 17:06:49 +0300

    feat(MarketView): top performing properties charts created
    and implemented to marketview platform and competitor pages
    
    Closes #LUCY-178

--- src/client/containers/Marketview/views/main.js
+++ src/client/containers/Marketview/views/main.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import { connect } from "react-redux"
-import { createStructuredSelector } from "reselect"
-import { compose, bindActionCreators } from "redux"
-import { actions, makeSelectMarketview } from "Reducers/marketview"
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import { compose, bindActionCreators } from 'redux'
+import { actions, makeSelectMarketview } from 'Reducers/marketview'
 
 import ColorCard from 'Containers/Marketview/sections/main/ColorCard'
 import PacingCard from 'Containers/Marketview/sections/main/PacingCard'
@@ -18,40 +18,61 @@ class Main extends Component {
     this.props.getTotalViewsRequest()
     this.props.getTotalCompetitorViewsRequest()
   }
-	render() {
+  render() {
     const {
-      marketview: { bubleChartData, pacingChartData, formatChartData, totalViewsData, totalCompetitorViewsData }
+      marketview: {
+        bubleChartData,
+        pacingChartData,
+        formatChartData,
+        totalViewsData,
+        totalCompetitorViewsData,
+      },
     } = this.props
-		return (
-			<React.Fragment>
-				<div className="grid-collapse">
+    return (
+      <React.Fragment>
+        <div className="grid-collapse">
           <div className="col-4 mb-48">
-            {bubleChartData && <ColorCard bubleChartData={bubleChartData}/>}
+            {bubleChartData && <ColorCard bubleChartData={bubleChartData} />}
           </div>
 
           <div className="col-4 mb-48">
-            {pacingChartData && <PacingCard pacingChartData={pacingChartData}/>}
+            {pacingChartData && (
+              <PacingCard pacingChartData={pacingChartData} />
+            )}
           </div>
 
           <div className="col-4 mb-48">
-            {formatChartData && <FormatCard formatChartData={formatChartData}/>}
+            {formatChartData && (
+              <FormatCard formatChartData={formatChartData} />
+            )}
           </div>
         </div>
 
         <div className="grid-collapse">
-          {totalViewsData && <TotalViewsCard totalViewsData={totalViewsData}/>}
-          {totalCompetitorViewsData && <TotalCompetitorViewsCard totalCompetitorViewsData={totalCompetitorViewsData}/>}
+          {totalViewsData && <TotalViewsCard totalViewsData={totalViewsData} />}
+          {totalCompetitorViewsData && (
+            <TotalCompetitorViewsCard
+              totalCompetitorViewsData={totalCompetitorViewsData}
+              titleLabels={[
+                'Barstool Sports',
+                'SB Nation',
+                'ESPN',
+                'Scout Media',
+                'Fansided',
+              ]}
+            />
+          )}
         </div>
-			</React.Fragment>
-		)
-	}
+      </React.Fragment>
+    )
+  }
 }
 
 const mapStateToProps = createStructuredSelector({
-  marketview: makeSelectMarketview()
+  marketview: makeSelectMarketview(),
 })
 
-const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch)
+const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch)
 
 const withConnect = connect(
   mapStateToProps,
--- src/client/containers/Marketview/views/platform.js
+++ src/client/containers/Marketview/views/platform.js
@@ -14,6 +14,7 @@ import { actions, makeSelectMarketview } from 'Reducers/marketview'
 import Slider from 'Containers/Marketview/sections/detail/Slider'
 import TopVideosCard from 'Containers/Marketview/sections/detail/TopVideosCard'
 import TopSimilarProperties from 'Containers/Marketview/sections/detail/TopSimilarProperties'
+import TotalCompetitorViewsCard from 'Containers/Marketview/sections/main/TotalCompetitorViewsCard'
 import RouterLoading from 'Components/RouterLoading'
 
 import style from '../style.scss'
@@ -41,12 +42,25 @@ const topVideosReferences = [
   },
 ]
 
+const chartTickOptions = {
+  stepSize: 250000,
+  callback(value) {
+    if (value < 1000) {
+      return value
+    } else if (value < 1000000) {
+      return `${Math.round(value / 1000)}k`
+    }
+    return `${Math.round((value * 100) / 1000000) / 100}m`
+  },
+}
+
 /* eslint-disable react/prefer-stateless-function */
 export class Platform extends React.Component {
   componentDidMount() {
     this.props.getCompetitorTopVideosRequest()
     this.props.getCompetitorVideosRequest()
     this.props.getSimilarPropertiesRequest()
+    this.props.getTopPerformingPropertiesRequest()
   }
 
   changeSelectedVideo = (video) => {
@@ -56,7 +70,11 @@ export class Platform extends React.Component {
   render() {
     const {
       marketview,
-      marketview: { competitorTopVideos, similarProperties },
+      marketview: {
+        competitorTopVideos,
+        similarProperties,
+        topPerformingPropertiesData,
+      },
     } = this.props
 
     if (!marketview.selectedVideo || marketview.loading) {
@@ -77,6 +95,19 @@ export class Platform extends React.Component {
           />
         )}
         {similarProperties && <TopSimilarProperties data={similarProperties} />}
+        {topPerformingPropertiesData && (
+          <div className="grid-collapse">
+            <TotalCompetitorViewsCard
+              containerClass={style.detailTopPerformingPropertyContainer}
+              totalCompetitorViewsData={topPerformingPropertiesData}
+              tickOptions={chartTickOptions}
+              title="Top Performing Property Across All Platforms"
+              height={100}
+              selects={['Resolution', 'Likes']}
+              footerLabels={['Fast', 'Medium', 'Slow', 'Slowest']}
+            />
+          </div>
+        )}
       </React.Fragment>
     )
   }
