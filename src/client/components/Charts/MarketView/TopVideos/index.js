1164c779f3ab1dc09c34ac27b4b80a94b772a2dc
Yusuf Bakır <f.yusufbakir@gmail.com>
2019-03-20 16:42:53 +0300

    feat: marketview integration mock data

--- src/client/components/Charts/MarketView/TopVideos/index.js
+++ src/client/components/Charts/MarketView/TopVideos/index.js
@@ -3,23 +3,23 @@ import { Bar } from 'react-chartjs-2'
 import { chartOptions } from './options'
 
 const TopVideosChart = ({ chartData, datasetKeyProvider }) => {
-    return (
-          <Bar
-            height={80}
-            backgroundColor="#242b49"
-						data={chartData}
-						datasetKeyProvider={datasetKeyProvider}
-            options={{
-              ...chartOptions,
-              chartArea: {
-                backgroundColor: '#fff',
-              },
-              plugins: {
-                datalabels: false,
-              },
-            }}
-          />
-    )
+  return (
+    <Bar
+      height={80}
+      backgroundColor="#242b49"
+      data={chartData}
+      datasetKeyProvider={datasetKeyProvider}
+      options={{
+        ...chartOptions,
+        chartArea: {
+          backgroundColor: '#fff',
+        },
+        plugins: {
+          datalabels: false,
+        }
+      }}
+    />
+  )
 }
 
 export default TopVideosChart
--- src/client/containers/Marketview/views/time.js
+++ src/client/containers/Marketview/views/time.js
@@ -18,101 +18,105 @@ import TopSimilarProperties from 'Containers/Marketview/sections/detail/TopSimil
 import RouterLoading from 'Components/RouterLoading'
 
 const topVideosReferences = [
-  {
-    className: 'bg-cool-blue',
-    text: 'Fast',
-  },
-  {
-    className: 'bg-lighter-purple',
-    text: 'Medium',
-  },
-  {
-    className: 'bg-coral-pink',
-    text: 'Slow',
-  },
-  {
-    className: 'bg-cool-grey',
-    text: 'Slowest',
-  },
+	{
+		className: 'bg-cool-blue',
+		text: 'Fast',
+	},
+	{
+		className: 'bg-lighter-purple',
+		text: 'Medium',
+	},
+	{
+		className: 'bg-coral-pink',
+		text: 'Slow',
+	},
+	{
+		className: 'bg-cool-grey',
+		text: 'Slowest',
+	},
 ]
 
 /* eslint-disable react/prefer-stateless-function */
 export class Time extends React.Component {
-  constructor(props) {
-    super(props)
+	constructor(props) {
+		super(props)
 
-    this.state = {
-      activeDay: 'monday',
-    }
-  }
+		this.state = {
+			activeDay: 'monday',
+		}
+	}
 
-  componentDidMount() {
-    this.props.getCompetitorTopVideosRequest()
-    this.props.getCompetitorVideosRequest()
+	componentDidMount() {
+		this.props.getCompetitorTopVideosRequest()
+		this.props.getCompetitorVideosRequest()
 		this.props.getSimilarPropertiesRequest()
- 		this.props.getMarketviewDetailTimeRequest()
-  }
+		this.props.getMarketviewDetailTimeRequest()
+	}
 
-  changeSelectedVideo = (video) => {
-    this.props.setSelectedVideo(video)
-  }
+	changeSelectedVideo = (video) => {
+		this.props.setSelectedVideo(video)
+	}
 
-  changeActiveDay(day) {
-    this.setState({ activeDay: day })
-  }
+	changeActiveDay(day) {
+		this.setState({ activeDay: day })
+	}
 
-  render() {
-    const {
-      marketview,
-      marketview: { marketviewDetailTime, competitorTopVideos },
-    } = this.props
+	render() {
+		const {
+			marketview,
+			marketview: { selectedVideo, marketviewDetailTime, competitorTopVideos },
+		} = this.props
 		const { activeDay } = this.state
 
 		const selectedDayData = marketviewDetailTime && marketviewDetailTime[activeDay]
-console.log(selectedDayData)
-    if (!marketview.selectedVideo || marketview.loading || !marketviewDetailTime) {
-      return <RouterLoading />
-    }
+		if (!selectedVideo || marketview.loading || !marketviewDetailTime) {
+			return <RouterLoading />
+		}
 
-    return (
-      <React.Fragment>
-        <DaySelection
-          onDayChange={(day) => this.changeActiveDay(day)}
-          activeDay={activeDay}
-        />
-        <Slider
-          data={marketview}
-          changeSelectedVideo={this.changeSelectedVideo}
-          className="mt-48"
-          title={`Top Performing ${activeDay
-            .charAt(0)
-            .toUpperCase()}${activeDay.slice(1)} Videos`}
-        />
-        {selectedDayData && selectedDayData.SimilarProperties && <TopSimilarProperties data={selectedDayData.SimilarProperties} />}
-        {competitorTopVideos && competitorTopVideos && (
-          <TopVideosCard
-            chartData={competitorTopVideos}
-            title="Top Performing Property Across All Days Of The Week"
-            selects={['Resolution']}
-            references={topVideosReferences}
-          />
-        )}
-      </React.Fragment>
-    )
-  }
+		return (
+			<React.Fragment>
+				<DaySelection
+					onDayChange={(day) => this.changeActiveDay(day)}
+					activeDay={activeDay}
+				/>
+				{selectedDayData && selectedDayData.CompetitorVideos && (
+					<Slider
+						data={selectedDayData.CompetitorVideos}
+						selectedVideo={selectedVideo}
+						changeSelectedVideo={this.changeSelectedVideo}
+						className="mt-48"
+						title={`Top Performing ${activeDay
+							.charAt(0)
+							.toUpperCase()}${activeDay.slice(1)} Videos`}
+					/>
+				)}
+				{selectedDayData && selectedDayData.SimilarProperties && (
+					<TopSimilarProperties data={selectedDayData.SimilarProperties} />
+				)}
+				{selectedDayData && selectedDayData.CompetitorTopVideos && (
+					<TopVideosCard
+						chartData={selectedDayData.CompetitorTopVideos}
+						title="Top Performing Property Across All Days Of The Week"
+						selects={['Resolution']}
+						references={topVideosReferences}
+					/>
+				)}
+			</React.Fragment>
+		)
+	}
 }
 
 Time.propTypes = {}
 
 const mapStateToProps = createStructuredSelector({
-  marketview: makeSelectMarketview(),
+	marketview: makeSelectMarketview(),
 })
 
 const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch)
 
 const withConnect = connect(
-  mapStateToProps,
-  mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps
 )
 
 export default compose(withConnect)(Time)
