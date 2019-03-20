ea41e10c07af13981e4cae1b129ae5421eabb78b
Yusuf Bakır <f.yusufbakir@gmail.com>
2019-03-20 16:46:18 +0300

    fix: indent

--- src/client/containers/Marketview/views/competitor.js
+++ src/client/containers/Marketview/views/competitor.js
@@ -20,88 +20,88 @@ import RouterLoading from 'Components/RouterLoading'
 import style from '../style.scss'
 
 const chartTickOptions = {
-	stepSize: 250000,
-	callback(value) {
-		if (value < 1000) {
-			return value
-		} else if (value < 1000000) {
-			return `${Math.round(value / 1000)}k`
-		}
-		return `${Math.round((value * 100) / 1000000) / 100}m`
-	},
+  stepSize: 250000,
+  callback(value) {
+    if (value < 1000) {
+      return value
+    } else if (value < 1000000) {
+      return `${Math.round(value / 1000)}k`
+    }
+    return `${Math.round((value * 100) / 1000000) / 100}m`
+  },
 }
 
 /* eslint-disable react/prefer-stateless-function */
 export class Competitor extends React.Component {
-	componentDidMount() {
-		this.props.getCompetitorTopVideosRequest()
-		this.props.getCompetitorVideosRequest()
-		this.props.getSimilarPropertiesRequest()
-		this.props.getTopPerformingPropertiesByCompetitorsRequest()
-	}
+  componentDidMount() {
+    this.props.getCompetitorTopVideosRequest()
+    this.props.getCompetitorVideosRequest()
+    this.props.getSimilarPropertiesRequest()
+    this.props.getTopPerformingPropertiesByCompetitorsRequest()
+  }
 
-	changeSelectedVideo = (video) => {
-		this.props.setSelectedVideo(video)
-	}
+  changeSelectedVideo = (video) => {
+    this.props.setSelectedVideo(video)
+  }
 
-	render() {
-		const {
-			marketview,
-			marketview: {
-				selectedVideo,
-				competitorTopVideos,
-				similarProperties,
-				topPerformingPropertiesByCompetitorsData,
-			},
-		} = this.props
+  render() {
+    const {
+      marketview,
+      marketview: {
+        selectedVideo,
+        competitorTopVideos,
+        similarProperties,
+        topPerformingPropertiesByCompetitorsData,
+      },
+    } = this.props
 
-		if (!selectedVideo || marketview.loading) {
-			return <RouterLoading />
-		}
+    if (!selectedVideo || marketview.loading) {
+      return <RouterLoading />
+    }
 
-		return (
-			<React.Fragment>
-				<Slider
-					data={marketview.videos}
-					selectedVideo={selectedVideo}
-					changeSelectedVideo={this.changeSelectedVideo}
-					title="Top Performing Competitor Videos"
-				/>
-				{competitorTopVideos && (
-					<TopVideosCard chartData={competitorTopVideos} />
-				)}
-				{similarProperties && <TopSimilarProperties data={similarProperties} />}
-				{topPerformingPropertiesByCompetitorsData && (
-					<div className="grid-collapse">
-						<TotalCompetitorViewsCard
-							containerClass={style.detailTopPerformingPropertyContainer}
-							totalCompetitorViewsData={
-								topPerformingPropertiesByCompetitorsData
-							}
-							tickOptions={chartTickOptions}
-							title="Top Performing Property Across All Competitors"
-							height={100}
-							selects={['Resolution']}
-							footerLabels={['Fast', 'Medium', 'Slow', 'Slowest']}
-						/>
-					</div>
-				)}
-			</React.Fragment>
-		)
-	}
+    return (
+      <React.Fragment>
+        <Slider
+          data={marketview.videos}
+          selectedVideo={selectedVideo}
+          changeSelectedVideo={this.changeSelectedVideo}
+          title="Top Performing Competitor Videos"
+        />
+        {competitorTopVideos && (
+          <TopVideosCard chartData={competitorTopVideos} />
+        )}
+        {similarProperties && <TopSimilarProperties data={similarProperties} />}
+        {topPerformingPropertiesByCompetitorsData && (
+          <div className="grid-collapse">
+            <TotalCompetitorViewsCard
+              containerClass={style.detailTopPerformingPropertyContainer}
+              totalCompetitorViewsData={
+                topPerformingPropertiesByCompetitorsData
+              }
+              tickOptions={chartTickOptions}
+              title="Top Performing Property Across All Competitors"
+              height={100}
+              selects={['Resolution']}
+              footerLabels={['Fast', 'Medium', 'Slow', 'Slowest']}
+            />
+          </div>
+        )}
+      </React.Fragment>
+    )
+  }
 }
 
 Competitor.propTypes = {}
 
 const mapStateToProps = createStructuredSelector({
-	marketview: makeSelectMarketview(),
+  marketview: makeSelectMarketview(),
 })
 
 const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch)
 
 const withConnect = connect(
-	mapStateToProps,
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )
 
 export default compose(withConnect)(Competitor)
