1b7b60f09eeeddf2afe5bc762821d28b8a304914
Yusuf Bakır <f.yusufbakir@gmail.com>
2019-03-26 12:49:30 +0300

    feat: Age slider

--- src/client/components/Sliders/Audience/index.js
+++ src/client/components/Sliders/Audience/index.js
@@ -1,110 +1,89 @@
 import React from 'react';
-import Slider from 'react-slick';
 import style from './style.scss';
 import { socialIconSelector } from 'Utils/';
 
-const NextArrow = (props) => {
-	const { className, style, onClick } = props;
-	if (props.currentSlide + 4 >= props.slideCount) {
-		return null;
-	}
-	return <div className={className} onClick={onClick} />;
-};
+import Swiper from 'react-id-swiper';
+import { Navigation } from 'swiper/dist/js/swiper.esm'
 
-const PrevArrow = (props) => {
-	const { className, style, onClick } = props;
-	if (props.currentSlide == 0) {
-		return null;
-	}
-	return <div className={className} onClick={onClick} />;
-};
 
 class AudienceSlider extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			refSlider: null,
-			refThumb: null
-		}
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      refThumb: null
+    }
+  }
 
-	componentDidMount() {
-		this.setState({
-			refSlider: this.refSlider,
-			refThumb: this.refThumb
-		});
-	}
+  componentDidMount() {
+    this.setState({
+      refThumb: this.refThumb,
+    });
+  }
 
-	render() {
-		const { items, changeVideo } = this.props;
-		const { refSlider, refThumb } = this.state;
+  render() {
+    const { items, changeVideo } = this.props;
+    const { refThumb } = this.state;
 
-		const settings = {
-			className: 'audienceSlickSlider',
-			infinite: false,
-			arrows: false,
-			slidesToShow: 3,
-			slidesToScroll: 1,
-			speed: 300,
-			dots: false,
-			variableWidth: true,
-			variableHeight: true,
-			swipeToSlide: false,
-			focusOnSelect: true,
-			afterChange: currentSlide => changeVideo(items[currentSlide]),
-			centerMode: true,
-			centerPadding: "0",
-		};
+    const settings = {
+      modules: [Navigation],
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+      slidesPerView: 'auto',
+      spaceBetween: 40,
+      centeredSlides: true,
+      speed: 300,
+      autoplay: false,
+      keyboard: false,
+      thumbs: {
+        swiper: refThumb
+      }
+      //afterChange: currentSlide => changeVideo(items[currentSlide]),
+    };
 
-		const thumbSettings = {
-			className: 'audienceThumbSlickSlider',
-			infinite: false,
-			arrows: true,
-			slidesToShow: 9,
-			slidesToScroll: 1,
-			speed: 300,
-			swipeToSlide: true,
-			nextArrow: <NextArrow />,
-			prevArrow: <PrevArrow />,
-			focusOnSelect: true,
-			centerMode: true,
-			centerPadding: "0",
-		};
+    const thumbSettings = {
+      slidesPerView: 9,
+      freeMode: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+    };
 
-		return (
-			<div className={style.section} >
-				<Slider
-					asNavFor={refThumb}
-					ref={slider => (this.refSlider = slider)}
-					{...settings}
-				>
-					{items.map((item, i) => (
-						<div className="item" key={i}>
-							<img src={item.image} />
-							<p>
-								<span className="icon">
-									<span className={socialIconSelector(item.socialMedia)} />
-								</span>
-								{item.title}
-								<span className="secondTitle">{item.secondTitle}</span>
-							</p>
-						</div>
-					))}
-				</Slider>
-				<Slider
-					asNavFor={refSlider}
-					ref={slider => (this.refThumb = slider)}
-					{...thumbSettings}
-				>
-					{items.map((item, i) => (
-						<div className="item" key={i}>
-							{item.age}
-						</div>
-					))}
-				</Slider>
-			</div>
-		);
-	}
+    return (
+      <div className={style.section} >
+        {refThumb && (
+          <div className="audienceSlider">
+            <Swiper {...settings}>
+              {items.map((item, i) => (
+                <div className="item" key={i}>
+                  <img src={item.image} />
+                  <p>
+                    <span className="icon">
+                      <span className={socialIconSelector(item.socialMedia)} />
+                    </span>
+                    {item.title}
+                    <span className="secondTitle">{item.secondTitle}</span>
+                  </p>
+                </div>
+              ))}
+            </Swiper>
+          </div>
+        )}
+
+        <div className="audienceThumbSlider">
+          <Swiper ref={node => node && (this.refThumb = node.swiper)} {...thumbSettings}>
+            {items.map((item, i) => (
+              <div className="item" key={i}>
+                <span>
+                  {item.age}
+                </span>
+              </div>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default AudienceSlider;
