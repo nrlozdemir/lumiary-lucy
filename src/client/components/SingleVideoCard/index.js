1a011038b30cdd835596a42054fcd405f08c9ac8
serdarbasol <serdarbasol111@gmail.com>
2019-03-28 14:30:52 +0300

    feat: video players optimization

--- src/client/components/SingleVideoCard/index.js
+++ src/client/components/SingleVideoCard/index.js
@@ -4,50 +4,99 @@
  *
  */
 
-import React from 'react'
+import React, { PureComponent } from 'react'
 import classnames from 'classnames'
 import PropTypes from 'prop-types'
 import style from './style.scss'
-import { socialIconSelector } from '../../utils'
+import { socialIconSelector, randomKey } from '../../utils'
 /* eslint-disable react/prefer-stateless-function */
-const SingleVideoCard = ({ video, options = options || {}, muted = true }) => {
-  const cardContainerClass = classnames(style.cardContainer, {
-    ['bg-dusk']: !options.barColor,
-    ['col-3']: !options.size,
-    [`col-${options.size}`]: options.size,
-    [`bg-${options.barColor}`]: options.barColor,
-    [options.customClass]: options.customClass,
-  })
 
-  const iconClass = classnames(
-    socialIconSelector(video.socialIcon),
-    style.iconClass
-  )
+let hoverInTimer
+export class SingleVideoCard extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.video = React.createRef()
+    this.state = {
+      itCanPlay: false,
+    }
+  }
 
-  const videoEl = React.createRef()
-  return (
-    <div
-      key={video.id}
-      className={cardContainerClass}
-      onMouseEnter={() => videoEl.current && videoEl.current.play()}
-      onMouseLeave={() => videoEl.current && videoEl.current.pause()}
-    >
-      <div className={style.cardImage}>
-        <video className="img-responsive" ref={videoEl} muted={muted}>
-          <source src={video.videoUrl} />
-        </video>
-        <div className={style.overlay} />
-      </div>
-      <div className={style.cardBody}>
-        <div className={style.bodyHeader}>
-          <span className={style.title}>{video.title}</span>
-          <span className={style.iconWrapper}>
-            <i className={iconClass} />
-          </span>
+  handleMouseOverPlay = () => {
+    const _this = this
+    hoverInTimer = setTimeout(() => {
+      this.video.current.play()
+      this.video.current.addEventListener(
+        'timeupdate',
+        function() {
+          if (!_this.state.itCanPlay) {
+            _this.setState({
+              itCanPlay: true,
+            })
+            this.removeEventListener('timeupdate', null)
+          }
+        },
+        false
+      )
+    }, 400)
+  }
+
+  handleMouseOutPlay = () => {
+    clearTimeout(hoverInTimer)
+    setTimeout(() => {
+      this.video.current.pause()
+      this.video.current.currentTime = 0
+      this.setState({
+        itCanPlay: false,
+      })
+    }, 100)
+  }
+
+  render() {
+    const { video, options = options || {}, muted = true } = this.props
+    const cardContainerClass = classnames(style.cardContainer, {
+      ['bg-dusk']: !options.barColor,
+      ['col-3']: !options.size,
+      [`col-${options.size}`]: options.size,
+      [`bg-${options.barColor}`]: options.barColor,
+      [options.customClass]: options.customClass,
+    })
+
+    const iconClass = classnames(
+      socialIconSelector(video.socialIcon),
+      style.iconClass
+    )
+
+    return (
+      <div
+        key={video.index}
+        className={cardContainerClass}
+        onMouseEnter={this.handleMouseOverPlay}
+        onMouseLeave={this.handleMouseOutPlay}
+      >
+        <div className={style.cardImage}>
+          <div
+            className={style.blurredImage}
+            style={{ backgroundImage: `url(${video.poster})` }}
+          />
+          {video.videoUrl && (
+            <div className={style.videoInner}>
+              <video ref={this.video} loop muted poster={video.poster}>
+                <source src={video.videoUrl} type="video/mp4" />
+              </video>
+            </div>
+          )}
+        </div>
+        <div className={style.cardBody}>
+          <div className={style.bodyHeader}>
+            <span className={style.title}>{video.title}</span>
+            <span className={style.iconWrapper}>
+              <i className={iconClass} />
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 SingleVideoCard.propTypes = {
