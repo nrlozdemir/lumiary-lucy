83c5078781f2e52ad04329dfb221b5697bce758a
serdarbasol <serdarbasol111@gmail.com>
2019-04-03 16:53:47 +0300

    fix: market view page charts fixed styles

--- src/client/components/Charts/MarketView/TotalViewsBarChart/options.js
+++ src/client/components/Charts/MarketView/TotalViewsBarChart/options.js
@@ -1,45 +1,65 @@
 export const barDataOptions = {
   legend: {
-    display: false
+    display: false,
   },
   plugins: {
-    datalabels: false
+    datalabels: false,
   },
+  hover: { mode: null },
   scales: {
     xAxes: [
       {
-        display: true,
-        gridLines: {
-          display: true,
-          color: '#5a6386',
-          zeroLineColor: '#5a6386'
-        },
+        barThickness: 56,
+        stacked: true,
         ticks: {
-          fontColor: 'white'
+          fontColor: '#fff',
+          fontSize: 12,
+          fontFamily: 'ClanOTNews',
+          padding: 16,
+          beginAtZero: true,
+          callback: function(value, index, values) {
+            return value
+          },
         },
-        stacked: true
-      }
+        gridLines: {
+          display: false,
+          color: '#5a6386',
+          lineWidth: 1,
+          drawBorder: true,
+          drawTicks: true,
+          offsetGridLines: true,
+          zeroLineColor: '#5a6386',
+        },
+      },
     ],
     yAxes: [
       {
-        display: true,
-        gridLines: {
-          display: true,
-          color: '#5a6386',
-          zeroLineColor: '#5a6386'
-        },
+        stacked: true,
         ticks: {
           stepSize: 25,
           beginAtZero: true,
-          fontColor: 'white',
+          fontColor: '#fff',
+          fontSize: 12,
+          fontFamily: 'ClanOTNews',
+          padding: 16,
+          beginAtZero: true,
           min: 0,
           max: 100, // Your absolute max value
-          callback: function (value) {
-            return (value / 100 * 100).toFixed(0) + '%'; // convert it to percentage
-          }
+          callback: function(value) {
+            return ((value / 100) * 100).toFixed(0) + '%' // convert it to percentage
+          },
         },
-        stacked: true
-      }
-    ]
-  }
-};
+        gridLines: {
+          display: true,
+          color: '#5a6386',
+          lineWidth: 1,
+          drawBorder: true,
+          drawTicks: false,
+        },
+      },
+    ],
+  },
+  chartArea: {
+    backgroundColor: '#242b49',
+  },
+}
