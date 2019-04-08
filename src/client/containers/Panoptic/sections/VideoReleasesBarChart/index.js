26103a6aa4f0731105d813e56de9185a6a18e0d7
Yusuf Bakır <f.yusufbakir@gmail.com>
2019-04-08 19:30:03 +0300

    fix: indent

--- src/client/containers/Panoptic/sections/VideoReleasesBarChart/index.js
+++ src/client/containers/Panoptic/sections/VideoReleasesBarChart/index.js
@@ -14,88 +14,88 @@ const headerTitleClass = cx('font-secondary-second', style.title)
 const referencesClass = cx('font-secondary-second', style.references)
 
 const PanopticBarChart = ({
-	data,
-	selectLikes,
-	selectPlatforms,
-	selectDate,
-	handleSelectFilters,
+  data,
+  selectLikes,
+  selectPlatforms,
+  selectDate,
+  handleSelectFilters,
 }) => {
-	return (
-		<div className={barChartContainer}>
-			<div className={barChartHeaderClass}>
-				<div className="col-6 text-bold">
-					<p className={headerTitleClass}>Video Releases vs Engagement</p>
-				</div>
-				<div className="col-6">
-					<div className={style.selects}>
-						<SelectFilters
-							handleSelectFilters={handleSelectFilters}
-							selectDate={selectDate}
-							selectDateShow={true}
-							selectLikes={selectLikes}
-							selectLikesShow={true}
-							selectPlatforms={selectPlatforms}
-							selectPlatformsShow={true}
-						/>
-					</div>
-				</div>
-			</div>
-			{/* bar charts */}
-			<div className={barContainerClass}>
-				<div className={style.wrapperBarChart}>
-					<Bar data={data} options={wrapperBarOptions} />
-				</div>
-				<div className={style.barChartBackground} />
-				<div className={style.groupChartsWrapper}>
-					<div className="col-3">
-						<div className={style.chartSection}>
-							<Bar data={data} options={options} />
-						</div>
-						<div className={style.chartSectionBadge}>
-							<span>Live Action</span>
-						</div>
-					</div>
-					<div className="col-3">
-						<div className={style.chartSection}>
-							<Bar data={data} options={options} />
-						</div>
-						<div className={style.chartSectionBadge}>
-							<span>Stop Motion</span>
-						</div>
-					</div>
-					<div className="col-3">
-						<div className={style.chartSection}>
-							<Bar data={data} options={options} />
-						</div>
-						<div className={style.chartSectionBadge}>
-							<span>Cinemagraph</span>
-						</div>
-					</div>
-					<div className="col-3">
-						<div className={style.chartSection}>
-							<Bar data={data} options={options} />
-						</div>
-						<div className={style.chartSectionBadge}>
-							<span>Animation</span>
-						</div>
-					</div>
-				</div>
-			</div>
-			{/* bar charts */}
-			<div className={barChartFooterClass}>
-				<div className={referencesClass}>
-					<div className={style.referenceItem}>
-						<span className="bg-cool-blue" />
-						Likes
+  return (
+    <div className={barChartContainer}>
+      <div className={barChartHeaderClass}>
+        <div className="col-6 text-bold">
+          <p className={headerTitleClass}>Video Releases vs Engagement</p>
+        </div>
+        <div className="col-6">
+          <div className={style.selects}>
+            <SelectFilters
+              handleSelectFilters={handleSelectFilters}
+              selectDate={selectDate}
+              selectDateShow={true}
+              selectLikes={selectLikes}
+              selectLikesShow={true}
+              selectPlatforms={selectPlatforms}
+              selectPlatformsShow={true}
+            />
           </div>
-					<div className={style.referenceItem}>
-						<span className="bg-coral-pink" />
-						Videos Released
+        </div>
+      </div>
+      {/* bar charts */}
+      <div className={barContainerClass}>
+        <div className={style.wrapperBarChart}>
+          <Bar data={data} options={wrapperBarOptions} />
+        </div>
+        <div className={style.barChartBackground} />
+        <div className={style.groupChartsWrapper}>
+          <div className="col-3">
+            <div className={style.chartSection}>
+              <Bar data={data} options={options} />
+            </div>
+            <div className={style.chartSectionBadge}>
+              <span>Live Action</span>
+            </div>
           </div>
-				</div>
-			</div>
-		</div>
-	)
+          <div className="col-3">
+            <div className={style.chartSection}>
+              <Bar data={data} options={options} />
+            </div>
+            <div className={style.chartSectionBadge}>
+              <span>Stop Motion</span>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className={style.chartSection}>
+              <Bar data={data} options={options} />
+            </div>
+            <div className={style.chartSectionBadge}>
+              <span>Cinemagraph</span>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className={style.chartSection}>
+              <Bar data={data} options={options} />
+            </div>
+            <div className={style.chartSectionBadge}>
+              <span>Animation</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* bar charts */}
+      <div className={barChartFooterClass}>
+        <div className={referencesClass}>
+          <div className={style.referenceItem}>
+            <span className="bg-cool-blue" />
+            Likes
+          </div>
+          <div className={style.referenceItem}>
+            <span className="bg-coral-pink" />
+            Videos Released
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default PanopticBarChart
--- src/client/containers/ReportGenerated/sections/VideoReleasesBarChart/index.js
+++ src/client/containers/ReportGenerated/sections/VideoReleasesBarChart/index.js
@@ -13,72 +13,72 @@ const headerTitleClass = cx('font-secondary-first text-bold', style.title)
 const referencesClass = cx('font-secondary-second', style.references)
 
 const GeneratedReportBarChart = ({
-	data,
-	selectLikes,
-	selectPlatforms,
-	selectDate,
-	handleSelectFilters,
+  data,
+  selectLikes,
+  selectPlatforms,
+  selectDate,
+  handleSelectFilters,
 }) => {
-	return (
-		<div className={barChartContainer}>
-			<div className={style.barChartHeader}>
-				<div className="text-bold">
-					<p className={headerTitleClass}>Video Releases vs Engagement</p>
-				</div>
-				<div className={style.selects}>
-					<p>
-						<span className={style.blueDot} />
-						Likes
+  return (
+    <div className={barChartContainer}>
+      <div className={style.barChartHeader}>
+        <div className="text-bold">
+          <p className={headerTitleClass}>Video Releases vs Engagement</p>
+        </div>
+        <div className={style.selects}>
+          <p>
+            <span className={style.blueDot} />
+            Likes
           </p>
-					<p>
-						<span className={style.redDot} />
-						Video Releases
+          <p>
+            <span className={style.redDot} />
+            Video Releases
           </p>
-				</div>
-			</div>
-			{/* bar charts */}
-			<div className={barContainerClass}>
-				<div className={style.wrapperBarChart}>
-					<Bar data={data} options={wrapperBarOptions} />
-				</div>
-				<div className={style.barChartBackground} />
-				<div className={style.groupChartsWrapper}>
-					<div className="col-3">
-						<div className={style.chartSection}>
-							<Bar data={data} options={options} />
-						</div>
-						<div className={style.chartSectionBadge}>
-							<span>Live Action</span>
-						</div>
-					</div>
-					<div className="col-3">
-						<div className={style.chartSection}>
-							<Bar data={data} options={options} />
-						</div>
-						<div className={style.chartSectionBadge}>
-							<span>Stop Motion</span>
-						</div>
-					</div>
-					<div className="col-3">
-						<div className={style.chartSection}>
-							<Bar data={data} options={options} />
-						</div>
-						<div className={style.chartSectionBadge}>
-							<span>Cinemagraph</span>
-						</div>
-					</div>
-					<div className="col-3">
-						<div className={style.chartSection}>
-							<Bar data={data} options={options} />
-						</div>
-						<div className={style.chartSectionBadge}>
-							<span>Animation</span>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+        </div>
+      </div>
+      {/* bar charts */}
+      <div className={barContainerClass}>
+        <div className={style.wrapperBarChart}>
+          <Bar data={data} options={wrapperBarOptions} />
+        </div>
+        <div className={style.barChartBackground} />
+        <div className={style.groupChartsWrapper}>
+          <div className="col-3">
+            <div className={style.chartSection}>
+              <Bar data={data} options={options} />
+            </div>
+            <div className={style.chartSectionBadge}>
+              <span>Live Action</span>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className={style.chartSection}>
+              <Bar data={data} options={options} />
+            </div>
+            <div className={style.chartSectionBadge}>
+              <span>Stop Motion</span>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className={style.chartSection}>
+              <Bar data={data} options={options} />
+            </div>
+            <div className={style.chartSectionBadge}>
+              <span>Cinemagraph</span>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className={style.chartSection}>
+              <Bar data={data} options={options} />
+            </div>
+            <div className={style.chartSectionBadge}>
+              <span>Animation</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default GeneratedReportBarChart
