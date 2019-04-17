364eecf90adeb16fde45d67c74de2a0b71d05b32
Nurullah Ozdemir <nrl.ozdemir@saypr.com>
2019-04-17 12:34:06 +0300

    detail.js indent fix

--- src/client/containers/Quickview/views/detail.js
+++ src/client/containers/Quickview/views/detail.js
@@ -46,44 +46,44 @@ export class Detail extends React.Component {
       this.setState({ selectedQuickviewId: match.params.id })
       getQuickviewPlatformSelectedRequest(match.params.id)
     }
-	}
-
-  replaceBoldElement(text) {
-		const regex = new RegExp(/<b>(.*?)<\/b>/g)
-
-		if( ! regex.test(text)){
-			return text
-		}
-
-		const matched = text.match(regex)
-
-		const values = matched.map(val => val.replace(/<\/?b>/g, ''))
-		if ( ! values.length){
-			return text
-		}
-
-		return (<React.Fragment>
-			{text.split(regex).reduce((prev, current, i) => {
-				if( ! i) {
-					return [current]
-				}
-				return prev.concat(
-					values.includes(current)
-					? <strong key={i + current}>{current}</strong>
-					: current
-				);
-			}, [])}
-		</React.Fragment>)
   }
 
-	textEdit(text, item) {
-		text = text.replace('{value}', item.value)
-		text = text.replace('{title}', item.title)
+  replaceBoldElement(text) {
+    const regex = new RegExp(/<b>(.*?)<\/b>/g)
+
+    if( ! regex.test(text)){
+      return text
+    }
+
+    const matched = text.match(regex)
+
+    const values = matched.map(val => val.replace(/<\/?b>/g, ''))
+    if ( ! values.length){
+      return text
+    }
+
+    return (<React.Fragment>
+      {text.split(regex).reduce((prev, current, i) => {
+        if( ! i) {
+          return [current]
+        }
+        return prev.concat(
+          values.includes(current)
+          ? <strong key={i + current}>{current}</strong>
+          : current
+        );
+      }, [])}
+    </React.Fragment>)
+  }
+
+  textEdit(text, item) {
+    text = text.replace('{value}', item.value)
+    text = text.replace('{title}', item.title)
     text = text.replace('{percentage}', item.percentage)
     text = this.replaceBoldElement(text)
 
-		return text
-	}
+    return text
+  }
 
   render() {
     const { platforms, selectedQuickviewId } = this.state
@@ -91,7 +91,7 @@ export class Detail extends React.Component {
       quickview: {
         selectedPlatform: { id, platformsValues },
       },
-		} = this.props
+    } = this.props
 
     return (
       <React.Fragment>
@@ -105,49 +105,49 @@ export class Detail extends React.Component {
                     activeClassName={style.active}
                     to={`/quickview/${selectedQuickviewId}/${toSlug(platform)}`}
                   >
-										{index === 0 ? (platform) : (<i className={socialIconSelector(platform)}></i>)}
+                    {index === 0 ? (platform) : (<i className={socialIconSelector(platform)}></i>)}
                   </NavLink>
                 ))}
               </div>
             </div>
-						<div className={style.cardWrapper}>
-							<div className={style.content}>
-								{platformsValues.map((el, i) => (
-									<div key={i} className={classnames("col-6", style.cardBlock)}>
-										<div className={style.card}>
-											<div className={style.video}>
-												<SingleVideoCard {...el.video} muted={false} />
-											</div>
-											<div className={style.items}>
-												{el.infos.map((item, index) => (
-													<div className={style.itemWrapper}>
-														<div key={index} className={style.infoItem}>
-															<p className={classnames('font-secondary-second', style.sectionBadge)}>
-																<span>{item.title}</span>
-															</p>
-															<div className={style.itemValue} data-id={i}>{item.value}</div>
-															<div className={style.progressText}>
-																<span className={style.rightTitle}>{item.percentage}%</span>
-															</div>
-															<ProgressBar
-																width={item.percentage}
-																customBarClass={style.progressBar}
-																customPercentageClass={classnames(style.percentageBlue, {
-																	[style.percentagePink]: (i == 1)
-																})}
-															/>
-															<p className={style.infoText}>
-																{this.textEdit(item.text, item)}
-															</p>
-														</div>
-													</div>
-												))}
-											</div>
-										</div>
-									</div>
-								))}
-							</div>
-						</div>
+            <div className={style.cardWrapper}>
+              <div className={style.content}>
+                {platformsValues.map((el, i) => (
+                  <div key={i} className={classnames("col-6", style.cardBlock)}>
+                    <div className={style.card}>
+                      <div className={style.video}>
+                        <SingleVideoCard {...el.video} muted={false} />
+                      </div>
+                      <div className={style.items}>
+                        {el.infos.map((item, index) => (
+                          <div className={style.itemWrapper}>
+                            <div key={index} className={style.infoItem}>
+                              <p className={classnames('font-secondary-second', style.sectionBadge)}>
+                                <span>{item.title}</span>
+                              </p>
+                              <div className={style.itemValue} data-id={i}>{item.value}</div>
+                              <div className={style.progressText}>
+                                <span className={style.rightTitle}>{item.percentage}%</span>
+                              </div>
+                              <ProgressBar
+                                width={item.percentage}
+                                customBarClass={style.progressBar}
+                                customPercentageClass={classnames(style.percentageBlue, {
+                                  [style.percentagePink]: (i == 1)
+                                })}
+                              />
+                              <p className={style.infoText}>
+                                {this.textEdit(item.text, item)}
+                              </p>
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </React.Fragment>
