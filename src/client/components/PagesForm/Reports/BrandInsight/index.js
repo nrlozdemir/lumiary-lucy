35d977b6ab5ea5c095af7c8795242720f67535db
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2019-04-17 11:20:50 +0300

    fix: input field integrated to redux form and button valid style applied

--- src/client/components/PagesForm/Reports/BrandInsight/index.js
+++ src/client/components/PagesForm/Reports/BrandInsight/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 import Select from 'Components/Form/Select'
 import Input from 'Components/Form/Input'
+import cx from 'classnames'
 import { compose } from 'redux'
+import { required } from 'Utils/validate'
 
 import style from '../style.scss'
 
@@ -13,7 +15,7 @@ import {
   selectOptionsDateRange,
 } from '../options'
 
-const ReportsForm = ({}) => {
+const ReportsForm = (props) => {
   return (
     <form onSubmit={() => console.log('object')}>
       <div className={style.formArea}>
@@ -26,6 +28,7 @@ const ReportsForm = ({}) => {
             name="brand"
             placeholder="Select One"
             label="Choose a Brand"
+            validate={required}
           />
         </div>
         <div className={style.selectionArea}>
@@ -37,6 +40,7 @@ const ReportsForm = ({}) => {
             name="social"
             placeholder="Social Platform"
             label="Social Platform"
+            validate={required}
           />
         </div>
         <div className={style.selectionArea}>
@@ -48,6 +52,7 @@ const ReportsForm = ({}) => {
             name="engagement"
             placeholder="Select One"
             label="Engagement"
+            validate={required}
           />
         </div>
         <div className={style.selectionArea}>
@@ -59,6 +64,7 @@ const ReportsForm = ({}) => {
             name="date"
             placeholder="Select Date"
             label="Date Range"
+            validate={required}
           />
         </div>
         <div className={style.selectionArea}>
@@ -69,9 +75,14 @@ const ReportsForm = ({}) => {
             id="title"
             name="title"
             placeholder="Enter title name"
+            validate={required}
           />
         </div>
-        <button className={style.selectionLink} type="submit" disabled={true}>
+        <button
+          className={cx(style.selectionLink, { [style.active]: props.valid })}
+          type="submit"
+          disabled={!props.valid}
+        >
           Generate Report
           <div className={style.icon}>
             <span className="icon-Right-Arrow-Circle">
