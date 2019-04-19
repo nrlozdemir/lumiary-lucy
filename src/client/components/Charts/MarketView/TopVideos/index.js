324d35cebc377cb4934729be98e3b5b4e27398e7
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2019-04-19 18:18:13 +0300

    feat: new routes were added and datakeyprovider bug fixed

--- src/client/components/Charts/MarketView/TopVideos/index.js
+++ src/client/components/Charts/MarketView/TopVideos/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import { Bar } from 'react-chartjs-2'
 import { chartOptions } from './options'
+import { randomKey } from '../../../../utils';
 
-const TopVideosChart = ({ chartData, datasetKeyProvider }) => {
+const TopVideosChart = ({ chartData }) => {
   return (
     <Bar
       height={80}
       backgroundColor="#242b49"
       data={chartData}
-      datasetKeyProvider={datasetKeyProvider}
+      datasetKeyProvider={() => randomKey(5)}
       options={{
         ...chartOptions,
         chartArea: {
--- src/client/containers/Reports/index.js
+++ src/client/containers/Reports/index.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import { NavLink, Route, Switch } from 'react-router-dom'
 import Main from './views/Main'
 import CompareBrand from './views/CompareBrand'
+import ReportGenerated from '../ReportGenerated';
 
 class Reports extends Component {
   render() {
     return (
       <Switch>
         <Route path="/reports" exact component={Main} />
+				<Route path="/reports/brand-insight/:id" component={ReportGenerated} />
         <Route path="/reports/:id" component={CompareBrand} />
       </Switch>
     )
