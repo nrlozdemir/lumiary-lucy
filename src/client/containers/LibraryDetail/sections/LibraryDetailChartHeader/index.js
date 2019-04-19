9725841acd24e91fc9023c466d59f02664e82a28
Nurullah Ozdemir <nrl.ozdemir@saypr.com>
2019-04-19 13:23:45 +0300

    Fix: indent

--- src/client/containers/LibraryDetail/sections/LibraryDetailChartHeader/index.js
+++ src/client/containers/LibraryDetail/sections/LibraryDetailChartHeader/index.js
@@ -5,41 +5,41 @@ import Video from '../VideoComponent'
 import ProgressBar from 'Components/ProgressBar'
 
 const libraryDetailCard = (data, index) => {
-	return (<div key={index} className={classnames(
-		"bg-dark-grey-blue",
-		"shadow-1",
-		style.videoStat)}
-	>
-		<div className={style.progressText}>
-			<span className={style.leftTitle}>{data.title}</span>
-			<span className={style.rightTitle}>{data.score}k</span>
-		</div>
-		<ProgressBar
-			width={data.percentage}
-			customBarClass={style.progressBar}
-			customPercentageClass={classnames(style.percentageBlue, {
-				[style.percentagePink]: (parseInt(data.percentage) > 50) ? false : true
-			})}
-		/>
-		<p className={style.averageText}>Avg</p>
-	</div>)
+  return (<div key={index} className={classnames(
+    "bg-dark-grey-blue",
+    "shadow-1",
+    style.videoStat)}
+  >
+    <div className={style.progressText}>
+      <span className={style.leftTitle}>{data.title}</span>
+      <span className={style.rightTitle}>{data.score}k</span>
+    </div>
+    <ProgressBar
+      width={data.percentage}
+      customBarClass={style.progressBar}
+      customPercentageClass={classnames(style.percentageBlue, {
+        [style.percentagePink]: (parseInt(data.percentage) > 50) ? false : true
+      })}
+    />
+    <p className={style.averageText}>Avg</p>
+  </div>)
 }
 
 const LibraryDetailChartHeader = ({ barChartData, videoUrl, title, socialIcon }) => (
   <div className="grid-container mr-20 ml-20 mt-72" style={{ display: 'flex' }}>
     <div className={classnames(
-			"col-6",
-			style.videoWrapper)
-		}>
+      "col-6",
+      style.videoWrapper)
+    }>
       <Video src={videoUrl} title={title} socialIcon={socialIcon} />
     </div>
-		<div className={classnames(
-			"col-6",
-			style.videoStatsWrapper)}
-		>
-			{barChartData.map(
-				(data, index) => libraryDetailCard(data, index))
-			}
+    <div className={classnames(
+      "col-6",
+      style.videoStatsWrapper)}
+    >
+      {barChartData.map(
+        (data, index) => libraryDetailCard(data, index))
+      }
     </div>
   </div>
 )
