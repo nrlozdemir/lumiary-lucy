ccc028608bf367c0bd8f54c40f206dd0e6f6db8e
Nurullah Ozdemir <nrl.ozdemir@saypr.com>
2019-04-23 19:02:01 +0300

    Chart labels global component

---
+++ src/client/components/Charts/Labels/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import style from './style.scss'
+
+const propTypes = {}
+const defaultProps = {}
+
+export default class Labels extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+
+			</React.Fragment>
+		)
+	}
+}
+
+Labels.propTypes = propTypes
+Labels.defaultProps = defaultProps
