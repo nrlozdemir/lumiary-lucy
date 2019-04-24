bd5a1aa04bf89f0e86ea925cc409e983da36446a
Nurullah Ozdemir <nrl.ozdemir@saypr.com>
2019-04-24 16:39:15 +0300

    Marketview subsection doughnut chart component

--- src/client/components/Charts/MarketView/TotalViewsDoughnutChart/index.js
+++ src/client/components/Charts/MarketView/TotalViewsDoughnutChart/index.js
@@ -1,44 +1,26 @@
 import React from 'react'
-import { Doughnut } from 'react-chartjs-2';
+import DoughnutChart from 'Components/Charts/DoughnutChart'
 import style from 'Containers/Marketview/style.scss';
 
 const TotalViewsDougnutChart = ({ doughnutData }) => {
   return (
-    <div className="d-flex justify-space-between align-items-center">
-      <div className={style.colorList}>
-        <div className={style.colorListItem}>Barstool Sports</div>
-        <div className={style.colorListItem}>SB Nation</div>
-        <div className={style.colorListItem}>ESPN</div>
-        <div className={style.colorListItem}>Scout Media</div>
-        <div className={style.colorListItem}>Fansided</div>
-      </div>
-      <div className={style.doughnutChart}>
-        <Doughnut
-          options={{
-            responsive: false,
-            legend: {
-              display: false
-            },
-            plugins: {
-              datalabels: false
-            },
-            layout: {
-              padding: 0
-            }
-          }}
-          width={300}
-          height={300}
-          data={doughnutData}
-        />
-        <div className="poa-middle text-center">
-          Past Month
-        <br />
-          Combinded
-        <br />
-          Views
-      </div>
-      </div>
-    </div>
+		<div className="d-flex justify-space-between align-items-center">
+			<DoughnutChart
+				wrapperClassName="wrapperClassName"
+				chartClassName="customChartClass"
+				width={270}
+				height={270}
+				data={doughnutData}
+				dataLabelColor="#ffffff"
+				tooltip={true}
+				dataLabelFunction="insertAfter"
+				dataLabelInsert="%"
+				labelsPosition="left"
+				labelsClassName="customLabelClass"
+				labelsData={doughnutData.labels}
+			/>
+		</div>
+
   )
 }
 
