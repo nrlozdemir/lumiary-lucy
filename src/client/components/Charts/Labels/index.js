0e34b15ee2611aa2062221c2eec78fe18ca9a531
Nurullah Ozdemir <nrl.ozdemir@saypr.com>
2019-04-24 12:30:40 +0300

    Chart label component

--- src/client/components/Charts/Labels/index.js
+++ src/client/components/Charts/Labels/index.js
@@ -1,23 +1,41 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import style from './style.scss'
+import React from "react"
+import PropTypes from "prop-types"
+import classnames from "classnames"
+import style from "./style.scss"
 
 const propTypes = {}
 const defaultProps = {}
 
 export default class Labels extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {}
-	}
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
-	render() {
-		return (
-			<React.Fragment>
+  render() {
+    const {data} = this.props
 
-			</React.Fragment>
-		)
-	}
+    return (
+      <React.Fragment>
+        {data.map((roundData, index) => (
+          <div
+            className={classnames(
+              "d-flex",
+              "align-items-center",
+              style.lables
+            )}
+            key={index}
+          >
+            <span
+              className={style.round}
+              style={{ backgroundColor: `${roundData.color}` }}
+            />
+            <span className={style.secondsText}>{roundData.data}</span>
+          </div>
+        ))}
+      </React.Fragment>
+    )
+  }
 }
 
 Labels.propTypes = propTypes
