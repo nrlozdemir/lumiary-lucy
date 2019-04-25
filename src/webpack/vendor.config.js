e03eee4451d941e9790c99520bb8897bc3a07166
Yusuf Bakır <f.yusufbakir@gmail.com>
2019-04-25 20:10:02 +0300

    feat: removed to never used packages

--- src/webpack/vendor.config.js
+++ src/webpack/vendor.config.js
@@ -2,84 +2,65 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  context: process.cwd(),
+	context: process.cwd(),
 
-  resolve: {
-    extensions: ['.js', '.jsx', '.json', '.less', '.css'],
-    modules: [__dirname, 'node_modules'],
-  },
+	resolve: {
+		extensions: ['.js', '.jsx', '.json', '.less', '.css'],
+		modules: [__dirname, 'node_modules'],
+	},
 
-  entry: {
-    library: [
-      'react',
-      'react-dom',
-      'react-loadable',
-      'react-masonry-component',
-      'react-slick',
-      'react-table',
-      'react-tabs',
-      'react-router',
-      'react-router-dom',
-      'react-modal',
-      'react-dropzone-s3-uploader',
-      'react-jw-player',
-      'react-rangeslider',
-      'react-social-login',
-      'react-stay-scrolled',
-      'react-twitter-auth',
-      'react-dnd',
-      'react-dnd-html5-backend',
-      'react-dnd-test-backend',
-      'react-date-range',
-      'react-id-swiper',
+	entry: {
+		library: [
+			'react',
+			'react-dom',
+			'react-slick',
+			'react-table',
+			'react-tabs',
+			'react-router',
+			'react-router-dom',
+			'react-modal',
+			'react-date-range',
+			'react-id-swiper',
 
-      'redux',
-      'redux-form',
-      'redux-saga',
-      'redux-persist',
+			'redux',
+			'redux-form',
+			'redux-saga',
 
-      'reselect',
+			'reselect',
 
-      'qs',
-      'prop-types',
-      'bourbon',
-      'moment',
-      'node-neat',
-      'classnames',
+			'qs',
+			'prop-types',
+			'bourbon',
+			'moment',
+			'node-neat',
+			'classnames',
 
-      'slate',
-      'slate-react',
-      'history',
-      'jwt-decode',
-      'immutability-helper',
-      'immutable',
-      'isomorphic-fetch',
-      'lodash',
-      'swiper',
+			'history',
+			'immutability-helper',
+			'immutable',
+			'lodash',
+			'swiper',
 
-      'react-circular-progressbar',
-      'react-icons',
-      '@saypr/bubble-chart',
+			'@saypr/bubble-chart',
 
-      'chart.js',
-      'react-chartjs-2',
-      'chartjs-plugin-annotation',
-      'chartjs-plugin-datalabels',
-      'chartjs-plugin-style',
-    ],
-  },
+			'chart.js',
+			'react-chartjs-2',
+			'chartjs-plugin-datalabels',
+			'chartjs-plugin-style',
+		],
+	},
 
-  output: {
-    filename: '[name].dll.js',
-    path: path.resolve(__dirname, '../build/library'),
-    library: '[name]',
-  },
+	output: {
+		filename: '[name].dll.js',
+		path: path.resolve(__dirname, '../build/library'),
+		library: '[name]',
+	},
 
-  plugins: [
-    new webpack.DllPlugin({
-      context: __dirname,
-      name: '[name]',
-      path: path.join(__dirname, '..', 'build/library/[name].json'),
-    }),
-  ],
+	plugins: [
+		new webpack.DllPlugin({
+			context: __dirname,
+			name: '[name]',
+			path: path.join(__dirname, '..', 'build/library/[name].json'),
+		}),
+	],
 }
