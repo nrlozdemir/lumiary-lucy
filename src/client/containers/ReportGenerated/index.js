bd886158c90f279380d2d01014d726e761f50848
serdarbasol <serdarbasol111@gmail.com>
2019-04-25 21:40:10 +0300

    fix: removed generaled page switch component and added container

--- src/client/containers/ReportGenerated/index.js
+++ src/client/containers/ReportGenerated/index.js
@@ -65,7 +65,7 @@ class ReportGenerated extends Component {
     return (
       <React.Fragment>
         {/*<ReportsHeader />*/}
-        <div className={style.ReportsGeneratedContainer}>
+        <div className="grid-container col-12 mr-40 ml-40 mt-72 mb-72">
           <CreatedFilters />
           {topPerformingVideos && (
             <Slider
