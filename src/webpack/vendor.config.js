49096305466b454b77e6542ff338c89eabd292de
Yusuf Bakır <f.yusufbakir@gmail.com>
2019-04-25 20:13:51 +0300

    fix: indent

--- src/webpack/vendor.config.js
+++ src/webpack/vendor.config.js
@@ -2,65 +2,65 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-	context: process.cwd(),
+  context: process.cwd(),
 
-	resolve: {
-		extensions: ['.js', '.jsx', '.json', '.less', '.css'],
-		modules: [__dirname, 'node_modules'],
-	},
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.less', '.css'],
+    modules: [__dirname, 'node_modules'],
+  },
 
-	entry: {
-		library: [
-			'react',
-			'react-dom',
-			'react-slick',
-			'react-table',
-			'react-tabs',
-			'react-router',
-			'react-router-dom',
-			'react-modal',
-			'react-date-range',
-			'react-id-swiper',
+  entry: {
+    library: [
+      'react',
+      'react-dom',
+      'react-slick',
+      'react-table',
+      'react-tabs',
+      'react-router',
+      'react-router-dom',
+      'react-modal',
+      'react-date-range',
+      'react-id-swiper',
 
-			'redux',
-			'redux-form',
-			'redux-saga',
+      'redux',
+      'redux-form',
+      'redux-saga',
 
-			'reselect',
+      'reselect',
 
-			'qs',
-			'prop-types',
-			'bourbon',
-			'moment',
-			'node-neat',
-			'classnames',
+      'qs',
+      'prop-types',
+      'bourbon',
+      'moment',
+      'node-neat',
+      'classnames',
 
-			'history',
-			'immutability-helper',
-			'immutable',
-			'lodash',
-			'swiper',
+      'history',
+      'immutability-helper',
+      'immutable',
+      'lodash',
+      'swiper',
 
-			'@saypr/bubble-chart',
+      '@saypr/bubble-chart',
 
-			'chart.js',
-			'react-chartjs-2',
-			'chartjs-plugin-datalabels',
-			'chartjs-plugin-style',
-		],
-	},
+      'chart.js',
+      'react-chartjs-2',
+      'chartjs-plugin-datalabels',
+      'chartjs-plugin-style',
+    ],
+  },
 
-	output: {
-		filename: '[name].dll.js',
-		path: path.resolve(__dirname, '../build/library'),
-		library: '[name]',
-	},
+  output: {
+    filename: '[name].dll.js',
+    path: path.resolve(__dirname, '../build/library'),
+    library: '[name]',
+  },
 
-	plugins: [
-		new webpack.DllPlugin({
-			context: __dirname,
-			name: '[name]',
-			path: path.join(__dirname, '..', 'build/library/[name].json'),
-		}),
-	],
+  plugins: [
+    new webpack.DllPlugin({
+      context: __dirname,
+      name: '[name]',
+      path: path.join(__dirname, '..', 'build/library/[name].json'),
+    }),
+  ],
 }
