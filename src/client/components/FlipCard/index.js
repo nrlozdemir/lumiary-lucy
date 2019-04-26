a9743dc62b82cb864bbe523e164abb72ad99430f
Nurullah Ozdemir <nrl.ozdemir@saypr.com>
2019-04-26 23:23:04 +0300

    Fix: indent

--- src/client/components/FlipCard/index.js
+++ src/client/components/FlipCard/index.js
@@ -6,8 +6,8 @@ import styles from './style.scss'
 /*
 Example Usage:
 <FlipCard width={300} height={250}>
-	<h1>This is the front</h1>
-	<h2>This is the back</h2>
+  <h1>This is the front</h1>
+  <h2>This is the back</h2>
 </FlipCard>
 
 Also take the following props:
@@ -15,46 +15,46 @@ containerClassName, flipperClassName, frontClassName, backClassName
 */
 
 const defaultProps = {
-	containerClassName: classnames(styles.flipContainer, 'col-3 ml-0'),
-	flipperClassName: styles.flipper,
-	frontClassName: styles.front,
-	backClassName: styles.back,
-	width: 260,
-	height: 134
+  containerClassName: classnames(styles.flipContainer, 'col-3 ml-0'),
+  flipperClassName: styles.flipper,
+  frontClassName: styles.front,
+  backClassName: styles.back,
+  width: 260,
+  height: 134
 }
 
 export default class FlipCard extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-	render() {
-		const {
-			containerClassName,
-			flipperClassName,
-			frontClassName,
-			backClassName,
-			children,
-			width,
-			height
-		} = this.props
+  render() {
+    const {
+      containerClassName,
+      flipperClassName,
+      frontClassName,
+      backClassName,
+      children,
+      width,
+      height
+    } = this.props
 
-		return (
-			<React.Fragment>
-				<div className={containerClassName} style={{width: width + 'px', height: height + 'px'}}>
-					<div className={flipperClassName}>
-						<div className={frontClassName}>
-							{children && children[0]}
-						</div>
-						<div className={backClassName}>
-							{children && children[1]}
-						</div>
-					</div>
-				</div>
-			</React.Fragment>
-		);
-	}
+    return (
+      <React.Fragment>
+        <div className={containerClassName} style={{width: width + 'px', height: height + 'px'}}>
+          <div className={flipperClassName}>
+            <div className={frontClassName}>
+              {children && children[0]}
+            </div>
+            <div className={backClassName}>
+              {children && children[1]}
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 FlipCard.defaultProps = defaultProps;
--- src/client/containers/Panoptic/sections/Cards/index.js
+++ src/client/containers/Panoptic/sections/Cards/index.js
@@ -9,97 +9,97 @@ import PureBarChart from 'Components/Charts/Panoptic/PureBarChart'
 import styles from './style.scss'
 
 function parseData(vars){
-	const days = [
-		'Sunday',
-		'Monday',
-		'Tuesday',
-		'Wednesday',
-		'Thursday',
-		'Friday',
-		'Saturday',
-	]
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ]
 
-	const { stat } = vars.data
-	const { title } = vars
+  const { stat } = vars.data
+  const { title } = vars
   const selected = days[new Date().getDay()]
   const selectedPrev = days[
-		(day => new Date(day.setDate(day.getDate()-1)).getDay())
-		(new Date)
-	]
-	const statSelected = Object.values(stat).filter(
+    (day => new Date(day.setDate(day.getDate()-1)).getDay())
+    (new Date)
+  ]
+  const statSelected = Object.values(stat).filter(
     (word) => word.label === selected
-	)
-	const statSelectedPrev = Object.values(stat).filter(
+  )
+  const statSelectedPrev = Object.values(stat).filter(
     (word) => word.label === selectedPrev
-	)
-	const todayScore = statSelected[0].score
-	const previousDayScore = statSelectedPrev[0].score
+  )
+  const todayScore = statSelected[0].score
+  const previousDayScore = statSelectedPrev[0].score
   const statDifference = todayScore - previousDayScore
-	const statDifferenceValue = (statDifference < 0
-		? statDifference * -1
-		: statDifference) + '%'
+  const statDifferenceValue = (statDifference < 0
+    ? statDifference * -1
+    : statDifference) + '%'
 
-	let statArrowClassName, statClassName, backText
+  let statArrowClassName, statClassName, backText
 
-	if (statDifference === 0) {
+  if (statDifference === 0) {
     statArrowClassName = classnames(styles.arrow, styles.arrowRight)
     statClassName = classnames(styles.stats, styles.noChange)
     backText = `${title} didn't changed today from yesterday`
-	} else if (statDifference > 0) {
+  } else if (statDifference > 0) {
     statArrowClassName = classnames(styles.arrow, styles.arrowUp)
     statClassName = classnames(styles.stats, styles.increase)
     backText = `${title} increased today from yesterday, from ${
-			previousDayScore
-		}k to ${todayScore}k`
-	} else if (statDifference < 0) {
+      previousDayScore
+    }k to ${todayScore}k`
+  } else if (statDifference < 0) {
     statArrowClassName = classnames(styles.arrow, styles.arrowDown)
     statClassName = classnames(styles.stats, styles.decrease)
     backText = `${title} decreased today from yesterday, from ${
-			previousDayScore
-		}k to ${todayScore}k`
-	}
+      previousDayScore
+    }k to ${todayScore}k`
+  }
 
-	return {
-		statClassName,
-		statArrowClassName,
-		statDifferenceValue,
-		selected,
-		statDifference,
-		backText
-	}
+  return {
+    statClassName,
+    statArrowClassName,
+    statDifferenceValue,
+    selected,
+    statDifference,
+    backText
+  }
 }
 
 const Front = (props) => {
-	const { title } = props
-	const {
-		statClassName,
-		statArrowClassName,
-		statDifferenceValue,
-		selected,
-		statDifference
-	} = parseData(props)
+  const { title } = props
+  const {
+    statClassName,
+    statArrowClassName,
+    statDifferenceValue,
+    selected,
+    statDifference
+  } = parseData(props)
 
-	return (<div className={statClassName}>
-		<div className={styles.content}>
-			<p className={styles.headline}>{title}</p>
-			<div className={styles.changes}>
-				<div className={styles.circle}>
-					<i className={statArrowClassName} />
-				</div>
-				<p className={styles.label}>{statDifferenceValue}</p>
-			</div>
-		</div>
-		<PureBarChart
-			data={props.data}
-			selected={selected}
-			difference={statDifference}
-		/>
-	</div>)
+  return (<div className={statClassName}>
+    <div className={styles.content}>
+      <p className={styles.headline}>{title}</p>
+      <div className={styles.changes}>
+        <div className={styles.circle}>
+          <i className={statArrowClassName} />
+        </div>
+        <p className={styles.label}>{statDifferenceValue}</p>
+      </div>
+    </div>
+    <PureBarChart
+      data={props.data}
+      selected={selected}
+      difference={statDifference}
+    />
+  </div>)
 }
 
 const Back = (props) => {
-	const { backText } = parseData(props)
-	return (<p className={styles.backText}>{backText}</p>)
+  const { backText } = parseData(props)
+  return (<p className={styles.backText}>{backText}</p>)
 }
 
 class Cards extends React.Component {
@@ -112,30 +112,30 @@ class Cards extends React.Component {
       data: { views, likes, comments, shares },
       loading,
       error,
-		} = this.props.flipCardsData
+    } = this.props.flipCardsData
 
     return (
       <div className="grid-container col-12">
         <div className={styles.flipWrapper}>
           <FlipCard>
-						{views && <Front data={views} title="Views" />}
-						{views && <Back data={views} title="Views" />}
-					</FlipCard>
+            {views && <Front data={views} title="Views" />}
+            {views && <Back data={views} title="Views" />}
+          </FlipCard>
 
           <FlipCard>
-						{likes && <Front data={likes} title="Likes" />}
-						{likes && <Back data={likes} title="Likes" />}
-					</FlipCard>
+            {likes && <Front data={likes} title="Likes" />}
+            {likes && <Back data={likes} title="Likes" />}
+          </FlipCard>
 
           <FlipCard>
-						{comments && <Front data={comments} title="Comments" />}
-						{comments && <Back data={comments} title="Comments" />}
-					</FlipCard>
+            {comments && <Front data={comments} title="Comments" />}
+            {comments && <Back data={comments} title="Comments" />}
+          </FlipCard>
 
-					<FlipCard>
-						{shares && <Front data={shares} title="Shares" />}
-						{shares && <Back data={shares} title="Shares" />}
-					</FlipCard>
+          <FlipCard>
+            {shares && <Front data={shares} title="Shares" />}
+            {shares && <Back data={shares} title="Shares" />}
+          </FlipCard>
         </div>
       </div>
     )
