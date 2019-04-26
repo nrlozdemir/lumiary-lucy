59211b1ffda6e87660e3363ec570bf06893d6759
Nurullah Ozdemir <nrl.ozdemir@saypr.com>
2019-04-27 02:01:00 +0300

    Library detail flipcard with updated mock integration

--- src/client/containers/LibraryDetail/sections/LibraryDetailChartHeader/index.js
+++ src/client/containers/LibraryDetail/sections/LibraryDetailChartHeader/index.js
@@ -2,27 +2,83 @@ import React from 'react'
 import classnames from 'classnames'
 import style from './style.scss'
 import Video from '../VideoComponent'
+import FlipCard from 'Components/FlipCard'
 import ProgressBar from 'Components/ProgressBar'
 
-const libraryDetailCard = (data, index) => {
-  return (<div key={index} className={classnames(
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function replaceBoldElement(text) {
+	const regex = new RegExp(/<b>(.*?)<\/b>/g)
+
+	if( ! regex.test(text)){
+		return text
+	}
+
+	const matched = text.match(regex)
+
+	const values = matched.map(val => val.replace(/<\/?b>/g, ''))
+	if ( ! values.length){
+		return text
+	}
+
+	return (<React.Fragment>
+		{text.split(regex).reduce((prev, current, i) => {
+			if( ! i) {
+				return [current]
+			}
+			return prev.concat(
+				values.includes(current)
+				? <strong key={i + current}>{current}</strong>
+				: current
+			);
+		}, [])}
+	</React.Fragment>)
+}
+
+function textEdit(text, item) {
+	text = text.replace('{score}', item.score)
+	text = text.replace('{title}', item.title)
+	text = text.replace('{percentage}', item.percentage)
+	text = replaceBoldElement(text)
+
+	return text
+}
+
+const Front = (props) => {
+	const { data } = props
+  return (<div className={classnames(
     "bg-dark-grey-blue",
     "shadow-1",
     style.videoStat)}
   >
     <div className={style.progressText}>
-      <span className={style.leftTitle}>{data.title}</span>
+      <span className={style.leftTitle}>{capitalizeFirstLetter(data.title)}</span>
       <span className={style.rightTitle}>{data.score}k</span>
     </div>
     <ProgressBar
       width={data.percentage}
       customBarClass={style.progressBar}
-      customPercentageClass={classnames(style.percentageBlue, {
-        [style.percentagePink]: !(parseInt(data.percentage) > 50)
+      customPercentageClass={classnames(style.percentageIncrease, {
+        [style.percentageDecrease]: (parseInt(data.percentage) < 50)
       })}
     />
     <p className={style.averageText}>Avg</p>
   </div>)
+}
+
+const Back = (props) => {
+	const {
+		data,
+		data: { text }
+	} = props
+
+	return (
+		<p className={style.backText}>
+			{textEdit(text, data)}
+		</p>
+	)
 }
 
 const LibraryDetailChartHeader = ({ barChartData, videoUrl, title, socialIcon }) => (
@@ -39,8 +95,15 @@ const LibraryDetailChartHeader = ({ barChartData, videoUrl, title, socialIcon })
 				style.videoStatsWrapper)}
 			>
 				{barChartData.map(
-					(data, index) => libraryDetailCard(data, index))
-				}
+					(element, i) => {
+						return (
+							<FlipCard key={i} width={320} height={100}>
+								<Front data={element} />
+								<Back data={element} />
+							</FlipCard>
+						)
+					}
+				)}
 			</div>
 		</div>
 	</div>
