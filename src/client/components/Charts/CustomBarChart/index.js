319098666589783715a14c61f1695d20f408d453
Nurullah Ozdemir <nrl.ozdemir@saypr.com>
2019-04-29 16:53:41 +0300

    data will be the same like zeplin design: 100k-106k

--- src/client/components/Charts/CustomBarChart/index.js
+++ src/client/components/Charts/CustomBarChart/index.js
@@ -49,7 +49,10 @@ export default class CustomBarChart extends React.Component {
       difference
     } = this.props
     const zeroFill = parseInt(options.zeroFill)
-    const aspectRatio = (options.maxHeight / 100).toFixed(2)
+		const statMax = Object.values(data).reduce((prev, next) => {
+			return (prev.score < next.score) ? next : prev
+		})
+		const aspectRatio = statMax && (options.maxHeight / statMax.score).toFixed(2)
 
     return (
       <React.Fragment>
