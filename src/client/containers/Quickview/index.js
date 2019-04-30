4e689eb6fe24341e395b5ec716a2e05f72a84260
Nurullah Ozdemir <nrl.ozdemir@saypr.com>
2019-04-30 17:12:15 +0300

    AssetLayer integrated to quickview, SingleVideo options updated, mock cleared

--- src/client/containers/Quickview/index.js
+++ src/client/containers/Quickview/index.js
@@ -13,6 +13,8 @@ import { createStructuredSelector } from 'reselect'
 import { compose, bindActionCreators } from 'redux'
 import { actions, makeSelectQuickview } from 'Reducers/quickview'
 import { toSlug, socialIconSelector } from 'Utils/index'
+import AssetLayer from 'Components/AssetLayer'
+import PercentageBarGraph from 'Components/Charts/PercentageBarGraph'
 import SingleVideoCard from 'Components/SingleVideoCard'
 import ProgressBar from 'Components/ProgressBar'
 import { textEdit } from 'Utils/text'
@@ -82,48 +84,81 @@ export class Main extends React.Component {
             </div>
             <div className={style.cardWrapper}>
               <div className={style.content}>
-                {platformsValues.map((el, i) => (
-                  <div key={i} className={classnames("col-6", style.cardBlock)}>
-                    <div className={style.card}>
-                      <h1 className={classnames({[style.rightVideoTitle]: i === 1})}>
-                        {i == 0
-                          ? "Best Performing Videos"
-                          : "Underperforming Videos"
-                        }
-                        <i className="icon icon-Information"></i>
-                      </h1>
+                {platformsValues && platformsValues.map((el, i) => {
 
-                      <div className={style.video}>
-                        <SingleVideoCard {...el.video} muted={false} />
-                      </div>
-                      <div className={style.items}>
-                        {el.infos.map((item, index) => (
-                          <div key={index} className={style.itemWrapper}>
-                            <div className={style.infoItem}>
-                              <p className={classnames('font-secondary-second', style.sectionBadge)}>
-                                <span>{item.title}</span>
-                              </p>
-                              <div className={style.itemValue} data-id={i}>{item.value}</div>
-                              <div className={style.progressText}>
-                                <span className={style.rightTitle}>{item.percentage}%</span>
-                              </div>
-                              <ProgressBar
-                                width={item.percentage}
-                                customBarClass={style.progressBar}
-                                customPercentageClass={classnames(style.percentageBlue, {
-                                  [style.percentagePink]: (i == 1)
-                                })}
-                              />
-                              <p className={style.infoText}>
-                                {textEdit(item.text, item)}
-                              </p>
-                            </div>
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  </div>
-                ))}
+									const { cvScore, socialIcon, title, videoUrl, poster } = el.video
+
+									return(
+										<div key={i} className={classnames("col-6", style.cardBlock)}>
+											<div className={style.card}>
+												<h1 className={classnames(
+													{ [style.rightVideoTitle]: i === 1 }
+													)}>
+													{i == 0
+														? "Best Performing Videos"
+														: "Underperforming Videos"
+													}
+													<i className="icon icon-Information"></i>
+												</h1>
+												<div className={classnames(
+													style.assetContainer,
+													{ [style.right]: i === 1 }
+													)}>
+													<AssetLayer
+														leftSocialIcon={socialIcon}
+														title={title}
+														rightValue={cvScore}
+														width={510}
+														height={286}
+													>
+														<div className={style.video}>
+															<SingleVideoCard {...el} muted={false} options={{size: "auto"}} />
+														</div>
+														<div className={style.percentageWrapper} style={{right: '80px'}}>
+															<PercentageBarGraph
+																backgroundColor='#303a5d'
+																customClass={style.libraryPercentageGraph}
+																id={`videolist-${i}`}
+																percentage={cvScore}
+																disableLabels={true}
+																color='#2fd7c4'
+																lineCount={30}
+																height={19}
+																width={67}
+																xSmall
+															/>
+														</div>
+													</AssetLayer>
+												</div>
+												<div className={style.items}>
+													{el.infos.map((item, index) => (
+														<div key={index} className={style.itemWrapper}>
+															<div className={style.infoItem}>
+																<p className={classnames('font-secondary-second', style.sectionBadge)}>
+																	<span>{item.title}</span>
+																</p>
+																<div className={style.itemValue} data-id={i}>{item.value}</div>
+																<div className={style.progressText}>
+																	<span className={style.rightTitle}>{item.percentage}%</span>
+																</div>
+																<ProgressBar
+																	width={item.percentage}
+																	customBarClass={style.progressBar}
+																	customPercentageClass={classnames(style.percentageBlue, {
+																		[style.percentagePink]: (i == 1)
+																	})}
+																/>
+																<p className={style.infoText}>
+																	{textEdit(item.text, item)}
+																</p>
+															</div>
+														</div>
+													))}
+												</div>
+											</div>
+										</div>
+									)
+								})}
               </div>
             </div>
           </div>
