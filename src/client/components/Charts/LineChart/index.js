bb3f1b921aa6b8db89b25c9d9b76afe2fcfb7bae
Nurullah Ozdemir <nrl.ozdemir@saypr.com>
2019-05-03 11:31:58 +0300

    linechart initial commit

---
+++ src/client/components/Charts/LineChart/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import style from 'style.scss'
+
+const defaultProps = {}
+
+export default class LineChart extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+
+			</React.Fragment>
+		)
+	}
+}
+
+LineChart.propTypes = propTypes
+LineChart.defaultProps = defaultProps
