ded4e937344bd109f614edec6a897a302c64f23b
Nurullah Ozdemir <nrl.ozdemir@saypr.com>
2019-05-06 11:45:12 +0300

    chart types default options

---
+++ src/client/components/Charts/LineChart/defaultOptions.js
@@ -0,0 +1,56 @@
+export const lineStackedAreaOptions = [
+  {
+		fill: true,
+		borderColor: "transparent",
+		backgroundColor: "rgba(220, 118, 149, 0.6)"
+  },
+  {
+		fill: true,
+		borderColor: "transparent",
+		backgroundColor: "rgba(163, 137, 255, 0.6)"
+  },
+  {
+		fill: true,
+		borderColor: "transparent",
+		backgroundColor: "rgba(126, 206, 218, 0.6)"
+  }
+]
+
+export const lineOptions = [
+  {
+    fill: false,
+    lineTension: 0.1,
+    borderColor: "#2FD7C4",
+    borderCapStyle: "butt",
+    borderDash: [],
+    borderDashOffset: 0.0,
+    borderJoinStyle: "miter",
+    pointRadius: 5,
+    pointBackgroundColor: "#2FD7C4",
+    pointBorderColor: "#fff",
+    pointBorderWidth: 1,
+    pointHoverRadius: 5,
+    pointHoverBackgroundColor: "rgba(75,192,192,1)",
+    pointHoverBorderColor: "rgba(220,220,220,1)",
+    pointHoverBorderWidth: 2,
+    pointHitRadius: 10
+  },
+  {
+    fill: false,
+    lineTension: 0.1,
+    borderColor: "#8562F3",
+    borderCapStyle: "butt",
+    borderDash: [],
+    borderDashOffset: 0.0,
+    borderJoinStyle: "miter",
+    pointRadius: 5,
+    pointBackgroundColor: "#8562F3",
+    pointBorderColor: "#fff",
+    pointBorderWidth: 1,
+    pointHoverRadius: 5,
+    pointHoverBackgroundColor: "rgba(75,192,192,1)",
+    pointHoverBorderColor: "rgba(220,220,220,1)",
+    pointHoverBorderWidth: 2,
+    pointHitRadius: 10
+  }
+];
