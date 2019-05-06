f20e80015ba540da34d52b08aef3558a1ff14909
Yusuf Bakır <f.yusufbakir@gmail.com>
2019-05-06 12:56:54 +0300

    fix: indent

--- src/client/containers/Panoptic/sections/VideoReleasesBarChart/index.js
+++ src/client/containers/Panoptic/sections/VideoReleasesBarChart/index.js
@@ -7,50 +7,50 @@ import { actions, makeSelectPanopticVideoReleases } from 'Reducers/panoptic'
 import VideoReleasesBarChartModule from 'Components/Modules/VideoReleasesBarChartModule'
 
 class VideoReleasesBarChart extends Component {
-	callBack = (data, moduleKey) => {
-		this.props.getVideoReleasesData(data)
-	}
+  callBack = (data, moduleKey) => {
+    this.props.getVideoReleasesData(data)
+  }
 
-	render() {
-		const {
-			videoReleasesData: { data, loading, error }
-		} = this.props
+  render() {
+    const {
+      videoReleasesData: { data, loading, error }
+    } = this.props
 
-		return (
-			<VideoReleasesBarChartModule
-				data={data}
-				moduleKey={'Panoptic/VideoReleasesBarChartModule'}
-				title="Video Releases vs Engagement"
-				action={this.callBack}
-				filters={[
-					{
-						type: 'platform',
-						selectKey: 'PVR-asd',
-						placeHolder: 'Platform',
-					},
-					{
-						type: 'timeRange',
-						selectKey: 'PVR-wds',
-						placeHolder: 'Date',
-					}]}
-				legend={[
-					{ label: 'Videos', color: 'cool-blue' },
-					{ label: 'Engagement', color: 'coral-pink' },
-				]}
-			/>
-		)
-	}
+    return (
+      <VideoReleasesBarChartModule
+        data={data}
+        moduleKey={'Panoptic/VideoReleasesBarChartModule'}
+        title="Video Releases vs Engagement"
+        action={this.callBack}
+        filters={[
+          {
+            type: 'platform',
+            selectKey: 'PVR-asd',
+            placeHolder: 'Platform',
+          },
+          {
+            type: 'timeRange',
+            selectKey: 'PVR-wds',
+            placeHolder: 'Date',
+          }]}
+        legend={[
+          { label: 'Videos', color: 'cool-blue' },
+          { label: 'Engagement', color: 'coral-pink' },
+        ]}
+      />
+    )
+  }
 }
 
 const mapStateToProps = createStructuredSelector({
-	videoReleasesData: makeSelectPanopticVideoReleases(),
+  videoReleasesData: makeSelectPanopticVideoReleases(),
 })
 
 const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch)
 
 const withConnect = connect(
-	mapStateToProps,
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )
 
 export default compose(withConnect)(VideoReleasesBarChart)
--- src/client/containers/ReportGenerated/sections/VideoReleasesBarChart/index.js
+++ src/client/containers/ReportGenerated/sections/VideoReleasesBarChart/index.js
@@ -3,29 +3,29 @@ import React, { Component } from 'react'
 import VideoReleasesBarChartModule from 'Components/Modules/VideoReleasesBarChartModule'
 
 class VideoReleasesBarChart extends Component {
-	callBack = (data, moduleKey) => {
-		console.log(data)
-	}
+  callBack = (data, moduleKey) => {
+    console.log(data)
+  }
 
-	render() {
-		const {
-			data
-		} = this.props
+  render() {
+    const {
+      data
+    } = this.props
 
-		return (
-			<VideoReleasesBarChartModule
-				data={data}
-				moduleKey={'ReportGenerated/VideoReleasesBarChartModule'}
-				title="Video Releases vs Engagement"
-				action={this.callBack}
-				legend={[
-					{ label: 'Videos', color: 'cool-blue' },
-					{ label: 'Engagement', color: 'coral-pink' },
-				]}
-				legendEnd={true}
-			/>
-		)
-	}
+    return (
+      <VideoReleasesBarChartModule
+        data={data}
+        moduleKey={'ReportGenerated/VideoReleasesBarChartModule'}
+        title="Video Releases vs Engagement"
+        action={this.callBack}
+        legend={[
+          { label: 'Videos', color: 'cool-blue' },
+          { label: 'Engagement', color: 'coral-pink' },
+        ]}
+        legendEnd={true}
+      />
+    )
+  }
 }
 
 export default VideoReleasesBarChart
