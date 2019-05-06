b76d18e4ae1d3eb0ddc05951494fead53b7dea12
Nurullah Ozdemir <nrl.ozdemir@saypr.com>
2019-05-06 18:04:36 +0300

    merged from dev

--- src/client/containers/Panoptic/index.js
+++ src/client/containers/Panoptic/index.js
@@ -5,25 +5,28 @@ import FilteringSection from './sections/FilteringSection'
 import PacingCard from './sections/PacingCard'
 import CompareShares from './sections/CompareShares'
 import Cards from './sections/Cards'
+import TopPerformingFormat from './sections/TopPerformingFormat'
 import style from './style.scss'
 
 /* eslint-disable react/prefer-stateless-function */
-const Panoptic = () => {
-  return (
-    <React.Fragment>
-      <div className="grid-container col-12">
-        <div className={style.alignTabs}>
-
+class Panoptic extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="grid-container col-12">
+          <div className={style.alignTabs} />
         </div>
-      </div>
-      <Cards />
-      <VideoReleasesBarChart />
-      <ColorTemperature />
-      <FilteringSection />
-      <PacingCard />
-      <CompareShares />
-    </React.Fragment>
-  )
+
+        <Cards />
+        <TopPerformingFormat />
+        <VideoReleasesBarChart />
+        <ColorTemperature />
+        <FilteringSection />
+        <PacingCard />
+        <CompareShares />
+      </React.Fragment>
+    )
+  }
 }
 
 export default Panoptic
