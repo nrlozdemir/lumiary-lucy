793e668d12e7acd9e98d0b6940edc466b2498396
Nurullah Ozdemir <nrl.ozdemir@saypr.com>
2019-05-07 16:10:02 +0300

    Audience sections integrated with audience mock

--- src/client/containers/Audience/sections/DominantColor/index.js
+++ src/client/containers/Audience/sections/DominantColor/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import { compose, bindActionCreators } from 'redux'
-import { actions, makeSelectAudienceDominantColor } from 'Reducers/panoptic'
+import { actions, makeSelectAudienceDominantColor } from 'Reducers/audience'
 import RadarChartModule from 'Components/Modules/RadarChartModule'
 
 class DominantColor extends React.Component {
