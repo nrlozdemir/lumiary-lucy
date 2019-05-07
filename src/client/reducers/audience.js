9cf1bf55284acc58c7baae81d2be1672256a7e50
Nurullah Ozdemir <nrl.ozdemir@saypr.com>
2019-05-07 15:58:23 +0300

    create reducer and saga for Audience

---
+++ src/client/reducers/audience.js
@@ -0,0 +1,357 @@
+/*
+ *
+ * Panoptic reducer
+ *
+ */
+
+import { fromJS } from 'immutable'
+import { createSelector } from 'reselect'
+
+export const types = {
+
+  GET_AUDIENCE_CONTENT_VITALITY_SCORE_DATA: 'Audience/GET_AUDIENCE_CONTENT_VITALITY_SCORE_DATA',
+  GET_AUDIENCE_CONTENT_VITALITY_SCORE_DATA_SUCCESS: 'Audience/GET_AUDIENCE_CONTENT_VITALITY_SCORE_DATA_SUCCESS',
+  GET_AUDIENCE_CONTENT_VITALITY_SCORE_DATA_ERROR: 'Audience/GET_AUDIENCE_CONTENT_VITALITY_SCORE_DATA_ERROR',
+
+	GET_AUDIENCE_PERFORMANCE_DATA: 'Audience/GET_AUDIENCE_PERFORMANCE_DATA',
+	GET_AUDIENCE_PERFORMANCE_DATA_SUCCESS: 'Audience/GET_AUDIENCE_PERFORMANCE_DATA_SUCCESS',
+	GET_AUDIENCE_PERFORMANCE_DATA_ERROR: 'Audience/GET_AUDIENCE_PERFORMANCE_DATA_ERROR',
+
+  UPDATE_AUDIENCE_PERFORMANCE: 'Audience/UPDATE_AUDIENCE_PERFORMANCE',
+  UPDATE_AUDIENCE_PERFORMANCE_SUCCESS: 'Audience/UPDATE_AUDIENCE_PERFORMANCE_SUCCESS',
+  UPDATE_AUDIENCE_PERFORMANCE_ERROR: 'Audience/UPDATE_AUDIENCE_PERFORMANCE_ERROR',
+
+  GET_AUDIENCE_AGE_SLIDER_DATA: 'Audience/GET_AUDIENCE_AGE_SLIDER_DATA',
+  GET_AUDIENCE_AGE_SLIDER_DATA_SUCCESS: 'Audience/GET_AUDIENCE_AGE_SLIDER_DATA_SUCCESS',
+  GET_AUDIENCE_AGE_SLIDER_DATA_ERROR: 'Audience/GET_AUDIENCE_AGE_SLIDER_DATA_ERROR',
+
+  GET_AUDIENCE_GENDER_DATA: 'Audience/GET_AUDIENCE_GENDER_DATA',
+  GET_AUDIENCE_GENDER_DATA_SUCCESS: 'Audience/GET_AUDIENCE_GENDER_DATA_SUCCESS',
+  GET_AUDIENCE_GENDER_DATA_ERROR: 'Audience/GET_AUDIENCE_GENDER_DATA_ERROR',
+
+  GET_AUDIENCE_COLOR_TEMPERATURE_DATA: 'Audience/GET_AUDIENCE_COLOR_TEMPERATURE_DATA',
+  GET_AUDIENCE_COLOR_TEMPERATURE_DATA_SUCCESS: 'Audience/GET_AUDIENCE_COLOR_TEMPERATURE_DATA_SUCCESS',
+  GET_AUDIENCE_COLOR_TEMPERATURE_DATA_ERROR: 'Audience/GET_AUDIENCE_COLOR_TEMPERATURE_DATA_ERROR',
+}
+
+export const actions = {
+  getAudienceContentVitalityScoreData: () => ({
+    type: types.GET_AUDIENCE_CONTENT_VITALITY_SCORE_DATA,
+  }),
+  getAudienceContentVitalityScoreDataSuccess: (payload) => ({
+    type: types.GET_AUDIENCE_CONTENT_VITALITY_SCORE_DATA_SUCCESS,
+    payload,
+  }),
+  getAudienceContentVitalityScoreDataError: (error) => ({
+    type: types.GET_AUDIENCE_CONTENT_VITALITY_SCORE_DATA_ERROR,
+    error,
+  }),
+
+  getAudiencePerformanceData: () => ({
+    type: types.GET_AUDIENCE_PERFORMANCE_DATA,
+  }),
+  getAudiencePerformanceDataSuccess: (payload) => ({
+    type: types.GET_AUDIENCE_PERFORMANCE_DATA_SUCCESS,
+    payload,
+  }),
+  getAudiencePerformanceDataError: (error) => ({
+    type: types.GET_AUDIENCE_PERFORMANCE_DATA_ERROR,
+    error,
+	}),
+
+  updateAudiencePerformance: (payload) => ({
+    type: types.UPDATE_AUDIENCE_PERFORMANCE,
+    payload,
+  }),
+  updateAudiencePerformanceSuccess: (payload) => ({
+    type: types.UPDATE_AUDIENCE_PERFORMANCE_SUCCESS,
+    payload,
+  }),
+  updateAudiencePerformanceError: (error) => ({
+    type: types.UPDATE_AUDIENCE_PERFORMANCE_ERROR,
+    error,
+  }),
+
+  getAudienceAgeSliderData: () => ({
+    type: types.GET_AUDIENCE_AGE_SLIDER_DATA,
+  }),
+  getAudienceAgeSliderDataSuccess: (payload) => ({
+    type: types.GET_AUDIENCE_AGE_SLIDER_DATA_SUCCESS,
+    payload,
+  }),
+  getAudienceAgeSliderDataError: (error) => ({
+    type: types.GET_AUDIENCE_AGE_SLIDER_DATA_ERROR,
+    error,
+  }),
+
+  getAudienceGenderData: () => ({
+    type: types.GET_AUDIENCE_GENDER_DATA,
+  }),
+  getAudienceGenderDataSuccess: (payload) => ({
+    type: types.GET_AUDIENCE_GENDER_DATA_SUCCESS,
+    payload,
+  }),
+  getAudienceGenderDataError: (error) => ({
+    type: types.GET_AUDIENCE_GENDER_DATA_ERROR,
+    error,
+  }),
+
+  getAudienceColorTemperatureData: () => ({
+    type: types.GET_AUDIENCE_COLOR_TEMPERATURE_DATA,
+  }),
+  getAudienceColorTemperatureDataSuccess: (payload) => ({
+    type: types.GET_AUDIENCE_COLOR_TEMPERATURE_DATA_SUCCESS,
+    payload,
+  }),
+  getAudienceColorTemperatureDataError: (error) => ({
+    type: types.GET_AUDIENCE_COLOR_TEMPERATURE_DATA_ERROR,
+    error,
+  }),
+}
+
+export const initialState = fromJS({
+  data: {},
+  audienceContentVitalityScoreData: {
+    data: [],
+    loading: false,
+    error: null,
+  },
+  audiencePerformanceData: {
+    data: {},
+    loading: false,
+    error: null,
+  },
+  audienceAgeSliderData: {
+    data: [],
+    loading: false,
+    error: null,
+  },
+  audienceGenderData: {
+    data: [],
+    loading: false,
+    error: null,
+  },
+  audienceColorTemperatureData: {
+    data: [],
+    loading: false,
+    error: null,
+  },
+  loading: false,
+  error: false,
+})
+
+const audienceReducer = (state = initialState, action) => {
+  switch (action.type) {
+  	case types.GET_AUDIENCE_CONTENT_VITALITY_SCORE_DATA:
+			return state
+				.setIn(
+					['audienceContentVitalityScoreData', 'loading'],
+					fromJS(true)
+				)
+
+  	case types.GET_AUDIENCE_CONTENT_VITALITY_SCORE_DATA_SUCCESS:
+      return state
+        .setIn(
+          ['audienceContentVitalityScoreData', 'data'],
+          fromJS(action.payload)
+        )
+        .setIn(
+					['audienceContentVitalityScoreData', 'loading'],
+					fromJS(false)
+				)
+
+    case types.GET_AUDIENCE_CONTENT_VITALITY_SCORE_DATA_ERROR:
+      return state
+        .setIn(
+          ['audienceContentVitalityScoreData', 'error'],
+          fromJS(action.error)
+        )
+        .setIn(
+					['audienceContentVitalityScoreData', 'loading'],
+					fromJS(false)
+				)
+
+		case types.GET_AUDIENCE_PERFORMANCE_DATA:
+			return state
+				.setIn(
+					['audiencePerformanceData', 'loading'],
+					fromJS(true)
+				)
+
+    case types.GET_AUDIENCE_PERFORMANCE_DATA_SUCCESS:
+      return state
+        .setIn(
+					['audiencePerformanceData', 'data'],
+					fromJS(action.payload)
+				)
+        .setIn(
+					['audiencePerformanceData', 'loading'],
+					fromJS(false)
+				)
+
+    case types.GET_AUDIENCE_PERFORMANCE_DATA_ERROR:
+      return state
+        .setIn(
+					['audiencePerformanceData', 'error'],
+					fromJS(action.error)
+				)
+				.setIn(
+					['audiencePerformanceData', 'loading'],
+					fromJS(false)
+				)
+
+    case types.UPDATE_AUDIENCE_PERFORMANCE_SUCCESS:
+			return state
+				.mergeIn(
+        	['audiencePerformanceData', 'data'],
+        	fromJS(action.payload)
+      	)
+
+    case types.UPDATE_AUDIENCE_PERFORMANCE_ERROR:
+			return state
+				.setIn(
+					['audiencePerformanceData', 'error'],
+					fromJS(action.error)
+			)
+
+		case types.GET_AUDIENCE_AGE_SLIDER_DATA:
+			return state
+				.setIn(
+					['audienceAgeSliderData', 'loading'],
+					fromJS(true)
+				)
+
+    case types.GET_AUDIENCE_AGE_SLIDER_DATA_SUCCESS:
+      return state
+        .setIn(
+					['audienceAgeSliderData', 'data'],
+					fromJS(action.payload)
+				)
+        .setIn(
+					['audienceAgeSliderData', 'loading'],
+					fromJS(false)
+				)
+
+    case types.GET_AUDIENCE_AGE_SLIDER_DATA_ERROR:
+      return state
+        .setIn(
+					['audienceAgeSliderData', 'error'],
+					fromJS(action.error)
+				)
+        .setIn(
+					['audienceAgeSliderData', 'loading'],
+					fromJS(false)
+				)
+
+		case types.GET_AUDIENCE_GENDER_DATA:
+			return state
+				.setIn(
+					['audienceGenderData', 'loading'],
+					fromJS(true)
+				)
+
+		case types.GET_AUDIENCE_GENDER_DATA_SUCCESS:
+			return state
+				.setIn(
+					['audienceGenderData', 'data'],
+					fromJS(action.payload)
+				)
+				.setIn(
+					['audienceGenderData', 'loading'],
+					fromJS(false)
+				)
+
+		case types.GET_AUDIENCE_GENDER_DATA_ERROR:
+			return state
+				.setIn(
+					['audienceGenderData', 'error'],
+					fromJS(action.error)
+				)
+				.setIn(
+					['audienceGenderData', 'loading'],
+					fromJS(false)
+				)
+
+		case types.GET_AUDIENCE_COLOR_TEMPERATURE_DATA:
+			return state
+				.setIn(
+					['audienceColorTemperatureData', 'loading'],
+					fromJS(true)
+				)
+
+		case types.GET_AUDIENCE_COLOR_TEMPERATURE_DATA_SUCCESS:
+			return state
+				.setIn(
+					['audienceColorTemperatureData', 'data'],
+					fromJS(action.payload)
+				)
+				.setIn(
+					['audienceColorTemperatureData', 'loading'],
+					fromJS(false)
+				)
+
+		case types.GET_AUDIENCE_COLOR_TEMPERATURE_DATA_ERROR:
+			return state
+				.setIn(
+					['audienceColorTemperatureData', 'error'],
+					fromJS(action.error)
+				)
+				.setIn(
+					['audienceColorTemperatureData', 'loading'],
+					fromJS(false)
+				)
+
+    default:
+      return state
+  }
+}
+
+export const selectAudienceDomain = (state) => state.Audience
+
+const selectAudienceContentVitalityScoreDomain = (state) =>
+  state.Audience.get('audienceContentVitalityScoreData')
+
+export const makeSelectAudienceContentVitalityScore = () =>
+  createSelector(
+    selectAudienceContentVitalityScoreDomain,
+    (substate) => substate.toJS()
+	)
+
+const selectAudiencePerformanceDomain = (state) =>
+	state.Audience.get('audiencePerformanceData')
+
+export const makeSelectAudiencePerformance = () =>
+  createSelector(
+    selectAudiencePerformanceDomain,
+    (substate) => substate.toJS()
+  )
+
+const selectAudienceAgeSliderDomain = (state) =>
+	state.Audience.get('audienceAgeSliderData')
+
+export const makeSelectAudienceAgeSlider = () =>
+	createSelector(
+		selectAudienceAgeSliderDomain,
+		(substate) => substate.toJS()
+	)
+
+const selectAudienceGenderDomain = (state) =>
+	state.Audience.get('audienceGenderData')
+
+export const makeSelectAudienceGender = () =>
+	createSelector(
+		selectAudienceGenderDomain,
+		(substate) => substate.toJS()
+	)
+
+const selectAudienceColorTemperatureDomain = (state) =>
+	state.Audience.get('audienceColorTemperatureData')
+
+export const makeSelectAudienceColorTemperature = () =>
+	createSelector(
+		selectAudienceColorTemperatureDomain,
+		(substate) => substate.toJS()
+	)
+
+
+export default audienceReducer
