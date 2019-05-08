d3e8ec59608054559b98ddef78c53ffeb4c00434
Nurullah Ozdemir <nrl.ozdemir@saypr.com>
2019-05-09 01:56:31 +0300

    Font is ClanOTnews for x-y axes

--- src/client/containers/Reports/section/PerformanceComparison/options.js
+++ src/client/containers/Reports/section/PerformanceComparison/options.js
@@ -19,7 +19,8 @@ export const stackedChartOptions = {
         barThickness: 56,
         stacked: true,
         ticks: {
-          fontColor: '#fff',
+					fontColor: '#fff',
+					fontFamily: 'ClanOTNews',
           fontSize: 12,
           stepSize: 1,
           beginAtZero: true,
@@ -42,6 +43,7 @@ export const stackedChartOptions = {
         stacked: true,
         ticks: {
           fontColor: '#fff',
+					fontFamily: 'ClanOTNews',
           fontSize: 12,
           stepSize: 25,
           beginAtZero: true,
