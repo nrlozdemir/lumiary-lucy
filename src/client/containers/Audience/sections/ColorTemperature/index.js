7bf45c9c3702a003136116fe36c30256f3ebd8a5
Nurullah Ozdemir <nrl.ozdemir@saypr.com>
2019-05-08 11:38:52 +0300

    fix: indent

--- src/client/containers/Audience/sections/ColorTemperature/index.js
+++ src/client/containers/Audience/sections/ColorTemperature/index.js
@@ -7,47 +7,47 @@ import ColorTemperatureModule from 'Components/Modules/ColorTemperatureModule'
 import chartStyle from './style.scss'
 
 class ColorTemperature extends React.Component {
-	callBack = (data) => {
-		this.props.getAudienceColorTemperatureData(data)
-	}
+  callBack = (data) => {
+    this.props.getAudienceColorTemperatureData(data)
+  }
 
-	render() {
-		const {
-			audienceColorTemperatureData: { data, loading, error },
-		} = this.props
+  render() {
+    const {
+      audienceColorTemperatureData: { data, loading, error },
+    } = this.props
 
-		return (
-			<ColorTemperatureModule
-				moduleKey="Audience/ColorTemperature"
-				extraClasses={chartStyle.colorChartWrapper}
-				data={data}
-				title="Color Temperature / Sentiment Comparison"
-				action={this.callBack}
-				legend={[
-					{ label: 'Male', color: 'redRound' },
-					{ label: 'Female', color: 'purpleRound' },
-				]}
-				filters={[
-					{
-						type: 'timeRange',
-						selectKey: 'ACT-wds',
-						placeHolder: 'Date',
-					},
-				]}
-			/>
-		)
-	}
+    return (
+      <ColorTemperatureModule
+        moduleKey="Audience/ColorTemperature"
+        extraClasses={chartStyle.colorChartWrapper}
+        data={data}
+        title="Color Temperature / Sentiment Comparison"
+        action={this.callBack}
+        legend={[
+          { label: 'Male', color: 'redRound' },
+          { label: 'Female', color: 'purpleRound' },
+        ]}
+        filters={[
+          {
+            type: 'timeRange',
+            selectKey: 'ACT-wds',
+            placeHolder: 'Date',
+          },
+        ]}
+      />
+    )
+  }
 }
 
 const mapStateToProps = createStructuredSelector({
-	audienceColorTemperatureData: makeSelectAudienceColorTemperature(),
+  audienceColorTemperatureData: makeSelectAudienceColorTemperature(),
 })
 
 const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch)
 
 const withConnect = connect(
-	mapStateToProps,
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )
 
 export default compose(withConnect)(ColorTemperature)
