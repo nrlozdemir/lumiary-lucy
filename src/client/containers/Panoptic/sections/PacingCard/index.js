a64f1ebd0d3de62422c6584cd7c19e60e423cf7e
Jazzy Guo <jazzy@quickframe.com>
2019-05-09 17:26:59 -0400

    fix: formatting

--- src/client/containers/Panoptic/sections/PacingCard/index.js
+++ src/client/containers/Panoptic/sections/PacingCard/index.js
@@ -10,76 +10,76 @@ import StadiumChart from 'Components/Charts/Panoptic/StadiumChart'
 import style from './style.scss'
 
 const pacingCardContainer = classnames(
-	'shadow-1 col-12 mt-72',
-	style.pacingCardContainer
+  'shadow-1 col-12 mt-72',
+  style.pacingCardContainer
 )
 
 class PacingCard extends React.Component {
-	callBack = (data, moduleKey) => {
-		const { getPacingCardData } = this.props
-		getPacingCardData(data)
-	}
+  callBack = (data, moduleKey) => {
+    const { getPacingCardData } = this.props
+    getPacingCardData(data)
+  }
 
-	render() {
-		const {
-			pacingChartData: {
-				data: { horizontalStackedBarData, stadiumData },
-				loading,
-				error,
-			},
-		} = this.props
+  render() {
+    const {
+      pacingChartData: {
+        data: { horizontalStackedBarData, stadiumData },
+        loading,
+        error,
+      },
+    } = this.props
 
-		const isEmpty =
-			!!horizontalStackedBarData &&
-			!!stadiumData &&
-			horizontalStackedBarData.datasets.every((dataset) =>
-				dataset.data.every((data) => data === 0)
-			) &&
-			stadiumData.every((data) => data.value === 0)
+    const isEmpty =
+      !!horizontalStackedBarData &&
+      !!stadiumData &&
+      horizontalStackedBarData.datasets.every((dataset) =>
+        dataset.data.every((data) => data === 0)
+      ) &&
+      stadiumData.every((data) => data.value === 0)
 
-		return (
-			<Module
-				moduleKey={'Panoptic/PacingCard'}
-				title="Pacing For Each Format by Performance"
-				action={this.callBack}
-				filters={[
-					{
-						type: 'engagement',
-						selectKey: 'PCT-asd',
-						placeHolder: 'Engagement',
-					},
-					{
-						type: 'timeRange',
-						selectKey: 'PCT-wds',
-						placeHolder: 'Date',
-					},
-				]}
-				isEmpty={isEmpty}
-			>
-				<div className={style.pacingCardInner}>
-					<div className={style.pacingCardInnerItem}>
-						{horizontalStackedBarData && (
-							<HorizontalStackedBarChart barData={horizontalStackedBarData} />
-						)}
-					</div>
-					<div className={style.pacingCardInnerItem}>
-						{stadiumData && <StadiumChart data={stadiumData} />}
-					</div>
-				</div>
-			</Module>
-		)
-	}
+    return (
+      <Module
+        moduleKey={'Panoptic/PacingCard'}
+        title="Pacing For Each Format by Performance"
+        action={this.callBack}
+        filters={[
+          {
+            type: 'engagement',
+            selectKey: 'PCT-asd',
+            placeHolder: 'Engagement',
+          },
+          {
+            type: 'timeRange',
+            selectKey: 'PCT-wds',
+            placeHolder: 'Date',
+          },
+        ]}
+        isEmpty={isEmpty}
+      >
+        <div className={style.pacingCardInner}>
+          <div className={style.pacingCardInnerItem}>
+            {horizontalStackedBarData && (
+              <HorizontalStackedBarChart barData={horizontalStackedBarData} />
+            )}
+          </div>
+          <div className={style.pacingCardInnerItem}>
+            {stadiumData && <StadiumChart data={stadiumData} />}
+          </div>
+        </div>
+      </Module>
+    )
+  }
 }
 
 const mapStateToProps = createStructuredSelector({
-	pacingChartData: makeSelectPanopticPacingCard(),
+  pacingChartData: makeSelectPanopticPacingCard(),
 })
 
 const mapDispatchToProps = (dispatch) => bindActionCreators(actions, dispatch)
 
 const withConnect = connect(
-	mapStateToProps,
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )
 
 export default compose(withConnect)(PacingCard)
--- src/client/sagas/panoptic.js
+++ src/client/sagas/panoptic.js
@@ -13,335 +13,335 @@ import _ from 'lodash'
 const RESOURCE = '/report'
 
 function getPanopticDataApi() {
-	//this will use ajax function in utils/api when real data is provided
-	return axios.get('/').then((res) => panopticMockData)
+  //this will use ajax function in utils/api when real data is provided
+  return axios.get('/').then((res) => panopticMockData)
 }
 
 function getAudienceDataApi() {
-	//this will use ajax function in utils/api when real data is provided
-	return axios.get('/').then((res) => audienceMockData)
+  //this will use ajax function in utils/api when real data is provided
+  return axios.get('/').then((res) => audienceMockData)
 }
 
 function updateAudiencePerformanceApi({ min, max }) {
-	//this will use ajax function in utils/api when real data is provided
-	return axios.get('/').then((res) => updateAudiencePer(min, max))
+  //this will use ajax function in utils/api when real data is provided
+  return axios.get('/').then((res) => updateAudiencePer(min, max))
 }
 
 function* getVideoReleasesData() {
-	try {
-		const payload = yield call(getPanopticDataApi)
+  try {
+    const payload = yield call(getPanopticDataApi)
 
-		const shuffleData = payload.videoReleasesData
-		shuffleData.datasets[0].data = _.shuffle(shuffleData.datasets[0].data)
-		shuffleData.datasets[1].data = _.shuffle(shuffleData.datasets[1].data)
-		yield put(actions.getVideoReleasesDataSuccess(shuffleData))
-	} catch (err) {
-		yield put(actions.getVideoReleasesDataError(err))
-	}
+    const shuffleData = payload.videoReleasesData
+    shuffleData.datasets[0].data = _.shuffle(shuffleData.datasets[0].data)
+    shuffleData.datasets[1].data = _.shuffle(shuffleData.datasets[1].data)
+    yield put(actions.getVideoReleasesDataSuccess(shuffleData))
+  } catch (err) {
+    yield put(actions.getVideoReleasesDataError(err))
+  }
 }
 
 function* getColorTemperatureData() {
-	try {
-		const payload = yield call(getPanopticDataApi)
+  try {
+    const payload = yield call(getPanopticDataApi)
 
-		const shuffleData = _.shuffle(payload.colorTempData)
-		yield put(actions.getColorTemperatureDataSuccess(shuffleData))
-	} catch (err) {
-		yield put(actions.getColorTemperatureDataError(err))
-	}
+    const shuffleData = _.shuffle(payload.colorTempData)
+    yield put(actions.getColorTemperatureDataSuccess(shuffleData))
+  } catch (err) {
+    yield put(actions.getColorTemperatureDataError(err))
+  }
 }
 
 function* getFilteringSectionData() {
-	try {
-		const payload = yield call(getPanopticDataApi)
-		const shuffleData = {
-			doughnutData: {
-				labels: payload.verticalStackedChartData.doughnutData.labels,
-				datasets: [
-					{
-						backgroundColor:
-							payload.verticalStackedChartData.doughnutData.datasets[0]
-								.backgroundColor,
-						data: _.shuffle(
-							payload.verticalStackedChartData.doughnutData.datasets[0].data
-						),
-						hoverBackgroundColor:
-							payload.verticalStackedChartData.doughnutData.datasets[0]
-								.hoverBackgroundColor,
-					},
-				],
-			},
-			stackedChartData: {
-				...payload.verticalStackedChartData.stackedChartData,
-				datasets: _.shuffle(
-					payload.verticalStackedChartData.stackedChartData.datasets
-				),
-			},
-		}
-		yield put(actions.getFilteringSectionDataSuccess(shuffleData))
-	} catch (err) {
-		yield put(actions.getFilteringSectionDataError(err))
-	}
+  try {
+    const payload = yield call(getPanopticDataApi)
+    const shuffleData = {
+      doughnutData: {
+        labels: payload.verticalStackedChartData.doughnutData.labels,
+        datasets: [
+          {
+            backgroundColor:
+              payload.verticalStackedChartData.doughnutData.datasets[0]
+                .backgroundColor,
+            data: _.shuffle(
+              payload.verticalStackedChartData.doughnutData.datasets[0].data
+            ),
+            hoverBackgroundColor:
+              payload.verticalStackedChartData.doughnutData.datasets[0]
+                .hoverBackgroundColor,
+          },
+        ],
+      },
+      stackedChartData: {
+        ...payload.verticalStackedChartData.stackedChartData,
+        datasets: _.shuffle(
+          payload.verticalStackedChartData.stackedChartData.datasets
+        ),
+      },
+    }
+    yield put(actions.getFilteringSectionDataSuccess(shuffleData))
+  } catch (err) {
+    yield put(actions.getFilteringSectionDataError(err))
+  }
 }
 
 function getPacingCardDataApi(vals) {
-	return ajax({
-		url: RESOURCE,
-		method: 'POST',
-		params: qs.stringify(vals),
-	}).then((response) => {
-		if (response.error) {
-			throw response.error
-		}
-		return response.data
-	})
+  return ajax({
+    url: RESOURCE,
+    method: 'POST',
+    params: qs.stringify(vals),
+  }).then((response) => {
+    if (response.error) {
+      throw response.error
+    }
+    return response.data
+  })
 }
 
 function* getPacingCardData({ data }) {
-	try {
-		const { 'PCT-asd': metricOption = {}, 'PCT-wds': dateOption = {} } = data
-		const { value: dateOptionValue } = dateOption
+  try {
+    const { 'PCT-asd': metricOption = {}, 'PCT-wds': dateOption = {} } = data
+    const { value: dateOptionValue } = dateOption
 
-		const dateRange =
-			(!!dateOptionValue &&
-				dateOptionValue.value &&
-				(!!dateOptionValue.value.startDate
-					? [dateOptionValue.value.startDate, dateOptionValue.value.endDate]
-					: dateOptionValue.value)) ||
-			'24hours'
+    const dateRange =
+      (!!dateOptionValue &&
+        dateOptionValue.value &&
+        (!!dateOptionValue.value.startDate
+          ? [dateOptionValue.value.startDate, dateOptionValue.value.endDate]
+          : dateOptionValue.value)) ||
+      '24hours'
 
-		const metric =
-			(!!metricOption && !!metricOption.value && metricOption.value.value) ||
-			'views'
+    const metric =
+      (!!metricOption && !!metricOption.value && metricOption.value.value) ||
+      'views'
 
-		const options = {
-			metric,
-			dateRange,
-			platform: 'all',
-			property: ['pacing'],
-			dateBucket: 'none',
-			display: 'percentage',
-		}
+    const options = {
+      metric,
+      dateRange,
+      platform: 'all',
+      property: ['pacing'],
+      dateBucket: 'none',
+      display: 'percentage',
+    }
 
-		const stadiumData = yield call(getPacingCardDataApi, options)
+    const stadiumData = yield call(getPacingCardDataApi, options)
 
-		const horizontalStackedBarData = yield call(getPacingCardDataApi, {
-			...options,
-			proportionOf: 'format',
-		})
+    const horizontalStackedBarData = yield call(getPacingCardDataApi, {
+      ...options,
+      proportionOf: 'format',
+    })
 
-		if (
-			!!stadiumData.data &&
-			!!stadiumData.data.pacing &&
-			!!horizontalStackedBarData.data &&
-			!!horizontalStackedBarData.data.pacing
-		) {
-			const {
-				data: { pacing: stadiumPacing },
-			} = stadiumData
+    if (
+      !!stadiumData.data &&
+      !!stadiumData.data.pacing &&
+      !!horizontalStackedBarData.data &&
+      !!horizontalStackedBarData.data.pacing
+    ) {
+      const {
+        data: { pacing: stadiumPacing },
+      } = stadiumData
 
-			const {
-				data: { pacing: barChartPacing },
-			} = horizontalStackedBarData
+      const {
+        data: { pacing: barChartPacing },
+      } = horizontalStackedBarData
 
-			yield put(
-				actions.getPacingCardDataSuccess({
-					stadiumData: stadiumPacing,
-					horizontalStackedBarData: barChartPacing,
-				})
-			)
-		} else {
-			yield put(
-				actions.getPacingCardDataError('Error fetching pacing card data')
-			)
-		}
-	} catch (err) {
-		console.log(err)
-		yield put(actions.getPacingCardDataError(err))
-	}
+      yield put(
+        actions.getPacingCardDataSuccess({
+          stadiumData: stadiumPacing,
+          horizontalStackedBarData: barChartPacing,
+        })
+      )
+    } else {
+      yield put(
+        actions.getPacingCardDataError('Error fetching pacing card data')
+      )
+    }
+  } catch (err) {
+    console.log(err)
+    yield put(actions.getPacingCardDataError(err))
+  }
 }
 
 function* getCompareSharesData() {
-	try {
-		const payload = yield call(getPanopticDataApi)
-		const shuffleData = payload.compareSharesData
-		shuffleData[0].datas.datasets[0].data = _.shuffle(
-			shuffleData[0].datas.datasets[0].data
-		)
-		shuffleData[1].datas.datasets[0].data = _.shuffle(
-			shuffleData[1].datas.datasets[0].data
-		)
+  try {
+    const payload = yield call(getPanopticDataApi)
+    const shuffleData = payload.compareSharesData
+    shuffleData[0].datas.datasets[0].data = _.shuffle(
+      shuffleData[0].datas.datasets[0].data
+    )
+    shuffleData[1].datas.datasets[0].data = _.shuffle(
+      shuffleData[1].datas.datasets[0].data
+    )
 
-		yield put(actions.getCompareSharesDataSuccess(shuffleData))
-	} catch (err) {
-		yield put(actions.getCompareSharesDataError(err))
-	}
+    yield put(actions.getCompareSharesDataSuccess(shuffleData))
+  } catch (err) {
+    yield put(actions.getCompareSharesDataError(err))
+  }
 }
 
 function* getAudiencePerformanceData() {
-	try {
-		const payload = yield call(getAudienceDataApi)
-		const shuffleData = payload.performance
-		shuffleData.bubblesBoth = _.shuffle(shuffleData.bubblesBoth)
-		shuffleData.bubblesFemales = _.shuffle(shuffleData.bubblesFemales)
-		shuffleData.bubblesMales = _.shuffle(shuffleData.bubblesMales)
-		yield put(actions.getAudiencePerformanceDataSuccess(shuffleData))
-	} catch (err) {
-		yield put(actions.getAudiencePerformanceDataError(err))
-	}
+  try {
+    const payload = yield call(getAudienceDataApi)
+    const shuffleData = payload.performance
+    shuffleData.bubblesBoth = _.shuffle(shuffleData.bubblesBoth)
+    shuffleData.bubblesFemales = _.shuffle(shuffleData.bubblesFemales)
+    shuffleData.bubblesMales = _.shuffle(shuffleData.bubblesMales)
+    yield put(actions.getAudiencePerformanceDataSuccess(shuffleData))
+  } catch (err) {
+    yield put(actions.getAudiencePerformanceDataError(err))
+  }
 }
 
 function* getAudienceAgeSliderData() {
-	try {
-		const payload = yield call(getAudienceDataApi)
-		const randomImage = (image) => {
-			return image.replace(
-				/image=(\d+)/g,
-				'image=' + Math.floor(Math.random(1) * Math.floor(30))
-			)
-		}
-		const data = payload.ageSlider
-		data.map((element) => (element.image = randomImage(element.image)))
-		yield put(actions.getAudienceAgeSliderDataSuccess(data))
-	} catch (err) {
-		yield put(actions.getAudienceAgeSliderDataError(err))
-	}
+  try {
+    const payload = yield call(getAudienceDataApi)
+    const randomImage = (image) => {
+      return image.replace(
+        /image=(\d+)/g,
+        'image=' + Math.floor(Math.random(1) * Math.floor(30))
+      )
+    }
+    const data = payload.ageSlider
+    data.map((element) => (element.image = randomImage(element.image)))
+    yield put(actions.getAudienceAgeSliderDataSuccess(data))
+  } catch (err) {
+    yield put(actions.getAudienceAgeSliderDataError(err))
+  }
 }
 
 function* getAudienceGenderData() {
-	try {
-		const payload = yield call(getAudienceDataApi)
-		const shuffleData = payload.genderData
-		shuffleData.datasets[0].data = _.shuffle(shuffleData.datasets[0].data)
-		shuffleData.datasets[1].data = _.shuffle(shuffleData.datasets[1].data)
-		yield put(actions.getAudienceGenderDataSuccess(shuffleData))
-	} catch (err) {
-		yield put(actions.getAudienceGenderDataError(err))
-	}
+  try {
+    const payload = yield call(getAudienceDataApi)
+    const shuffleData = payload.genderData
+    shuffleData.datasets[0].data = _.shuffle(shuffleData.datasets[0].data)
+    shuffleData.datasets[1].data = _.shuffle(shuffleData.datasets[1].data)
+    yield put(actions.getAudienceGenderDataSuccess(shuffleData))
+  } catch (err) {
+    yield put(actions.getAudienceGenderDataError(err))
+  }
 }
 
 function* getAudienceColorTemperatureData() {
-	try {
-		const payload = yield call(getAudienceDataApi)
-		let shuffleData = payload.colorTempData
-		shuffleData = shuffleData.map((data) => {
-			data.data.map((item) => {
-				item.x = _.random(-50, 50)
-				item.y = _.random(-50, 50)
-			})
-			return data
-		})
-		yield put(actions.getAudienceColorTemperatureDataSuccess(shuffleData))
-	} catch (err) {
-		yield put(actions.getAudienceColorTemperatureDataError(err))
-	}
+  try {
+    const payload = yield call(getAudienceDataApi)
+    let shuffleData = payload.colorTempData
+    shuffleData = shuffleData.map((data) => {
+      data.data.map((item) => {
+        item.x = _.random(-50, 50)
+        item.y = _.random(-50, 50)
+      })
+      return data
+    })
+    yield put(actions.getAudienceColorTemperatureDataSuccess(shuffleData))
+  } catch (err) {
+    yield put(actions.getAudienceColorTemperatureDataError(err))
+  }
 }
 
 function* getAudienceChangeOverTimeData() {
-	try {
-		const payload = yield call(getAudienceDataApi)
-		let shuffleData = payload.lineChartData
-		shuffleData.datasets[0].data = _.shuffle(shuffleData.datasets[0].data)
-		shuffleData.datasets[1].data = _.shuffle(shuffleData.datasets[1].data)
-		yield put(actions.getAudienceChangeOverTimeDataSuccess(shuffleData))
-	} catch (err) {
-		yield put(actions.getAudienceChangeOverTimeDataError(err))
-	}
+  try {
+    const payload = yield call(getAudienceDataApi)
+    let shuffleData = payload.lineChartData
+    shuffleData.datasets[0].data = _.shuffle(shuffleData.datasets[0].data)
+    shuffleData.datasets[1].data = _.shuffle(shuffleData.datasets[1].data)
+    yield put(actions.getAudienceChangeOverTimeDataSuccess(shuffleData))
+  } catch (err) {
+    yield put(actions.getAudienceChangeOverTimeDataError(err))
+  }
 }
 
 function* getAudienceContentVitalityScoreData() {
-	try {
-		const payload = yield call(getAudienceDataApi)
-		let shuffleData = payload.lineStackedChartData
-		shuffleData.datasets[0].data = _.shuffle(shuffleData.datasets[0].data)
-		shuffleData.datasets[1].data = _.shuffle(shuffleData.datasets[1].data)
-		yield put(actions.getAudienceContentVitalityScoreDataSuccess(shuffleData))
-	} catch (err) {
-		yield put(actions.getAudienceContentVitalityScoreDataError(err))
-	}
+  try {
+    const payload = yield call(getAudienceDataApi)
+    let shuffleData = payload.lineStackedChartData
+    shuffleData.datasets[0].data = _.shuffle(shuffleData.datasets[0].data)
+    shuffleData.datasets[1].data = _.shuffle(shuffleData.datasets[1].data)
+    yield put(actions.getAudienceContentVitalityScoreDataSuccess(shuffleData))
+  } catch (err) {
+    yield put(actions.getAudienceContentVitalityScoreDataError(err))
+  }
 }
 
 function* getAudienceDominantColorData() {
-	try {
-		const payload = yield call(getAudienceDataApi)
-		let shuffleData = payload.chartData
-		shuffleData[0].datas.datasets[0].data = _.shuffle(
-			shuffleData[0].datas.datasets[0].data
-		)
-		shuffleData[1].datas.datasets[0].data = _.shuffle(
-			shuffleData[1].datas.datasets[0].data
-		)
-		yield put(actions.getAudienceDominantColorDataSuccess(shuffleData))
-	} catch (err) {
-		yield put(actions.getAudienceDominantColorDataError(err))
-	}
+  try {
+    const payload = yield call(getAudienceDataApi)
+    let shuffleData = payload.chartData
+    shuffleData[0].datas.datasets[0].data = _.shuffle(
+      shuffleData[0].datas.datasets[0].data
+    )
+    shuffleData[1].datas.datasets[0].data = _.shuffle(
+      shuffleData[1].datas.datasets[0].data
+    )
+    yield put(actions.getAudienceDominantColorDataSuccess(shuffleData))
+  } catch (err) {
+    yield put(actions.getAudienceDominantColorDataError(err))
+  }
 }
 
 function* getData() {
-	try {
-		const payload = yield call(getPanopticDataApi)
-		yield put(actions.getDataSuccess(payload))
-	} catch (err) {
-		yield put(actions.getDataError(err))
-	}
+  try {
+    const payload = yield call(getPanopticDataApi)
+    yield put(actions.getDataSuccess(payload))
+  } catch (err) {
+    yield put(actions.getDataError(err))
+  }
 }
 
 function* getAudienceData() {
-	try {
-		const payload = yield call(getAudienceDataApi)
-		yield put(actions.getAudienceDataSuccess(payload))
-	} catch (err) {
-		yield put(actions.getAudienceDataError(err))
-	}
+  try {
+    const payload = yield call(getAudienceDataApi)
+    yield put(actions.getAudienceDataSuccess(payload))
+  } catch (err) {
+    yield put(actions.getAudienceDataError(err))
+  }
 }
 
 function* updateAudiencePerformance({ payload: { min, max } }) {
-	try {
-		const payload = yield call(updateAudiencePerformanceApi, { min, max })
-		yield put(actions.updateAudiencePerformanceSuccess(payload))
-	} catch (err) {
-		yield put(actions.updateAudiencePerformanceError(err))
-	}
+  try {
+    const payload = yield call(updateAudiencePerformanceApi, { min, max })
+    yield put(actions.updateAudiencePerformanceSuccess(payload))
+  } catch (err) {
+    yield put(actions.updateAudiencePerformanceError(err))
+  }
 }
 
 function* getFlipCardsData() {
-	try {
-		const payload = yield call(getPanopticDataApi)
-		yield put(actions.getFlipCardsDataSuccess(payload.flipCardsData))
-	} catch (err) {
-		yield put(actions.getFlipCardsDataError(err))
-	}
+  try {
+    const payload = yield call(getPanopticDataApi)
+    yield put(actions.getFlipCardsDataSuccess(payload.flipCardsData))
+  } catch (err) {
+    yield put(actions.getFlipCardsDataError(err))
+  }
 }
 
 export default [
-	takeLatest(types.GET_DATA, getData),
-	takeLatest(types.GET_VIDEO_RELEASES_DATA, getVideoReleasesData),
-	takeLatest(types.GET_COLOR_TEMPERATURE_DATA, getColorTemperatureData),
-	takeLatest(types.GET_FILTERING_SECTION_DATA, getFilteringSectionData),
-	takeLatest(types.GET_PACING_CARD_DATA, getPacingCardData),
-	takeLatest(types.GET_COMPARE_SHARES_DATA, getCompareSharesData),
+  takeLatest(types.GET_DATA, getData),
+  takeLatest(types.GET_VIDEO_RELEASES_DATA, getVideoReleasesData),
+  takeLatest(types.GET_COLOR_TEMPERATURE_DATA, getColorTemperatureData),
+  takeLatest(types.GET_FILTERING_SECTION_DATA, getFilteringSectionData),
+  takeLatest(types.GET_PACING_CARD_DATA, getPacingCardData),
+  takeLatest(types.GET_COMPARE_SHARES_DATA, getCompareSharesData),
 
-	takeLatest(types.GET_AUDIENCE_PERFORMANCE_DATA, getAudiencePerformanceData),
-	takeLatest(types.GET_AUDIENCE_AGE_SLIDER_DATA, getAudienceAgeSliderData),
-	takeLatest(types.GET_AUDIENCE_GENDER_DATA, getAudienceGenderData),
-	takeLatest(
-		types.GET_AUDIENCE_COLOR_TEMPERATURE_DATA,
-		getAudienceColorTemperatureData
-	),
-	takeLatest(
-		types.GET_AUDIENCE_CHANGE_OVER_TIME_DATA,
-		getAudienceChangeOverTimeData
-	),
-	takeLatest(
-		types.GET_AUDIENCE_CONTENT_VITALITY_SCORE_DATA,
-		getAudienceContentVitalityScoreData
-	),
-	takeLatest(
-		types.GET_AUDIENCE_DOMINANT_COLOR_DATA,
-		getAudienceDominantColorData
-	),
+  takeLatest(types.GET_AUDIENCE_PERFORMANCE_DATA, getAudiencePerformanceData),
+  takeLatest(types.GET_AUDIENCE_AGE_SLIDER_DATA, getAudienceAgeSliderData),
+  takeLatest(types.GET_AUDIENCE_GENDER_DATA, getAudienceGenderData),
+  takeLatest(
+    types.GET_AUDIENCE_COLOR_TEMPERATURE_DATA,
+    getAudienceColorTemperatureData
+  ),
+  takeLatest(
+    types.GET_AUDIENCE_CHANGE_OVER_TIME_DATA,
+    getAudienceChangeOverTimeData
+  ),
+  takeLatest(
+    types.GET_AUDIENCE_CONTENT_VITALITY_SCORE_DATA,
+    getAudienceContentVitalityScoreData
+  ),
+  takeLatest(
+    types.GET_AUDIENCE_DOMINANT_COLOR_DATA,
+    getAudienceDominantColorData
+  ),
 
-	takeLatest(types.GET_AUDIENCE_DATA, getAudienceData),
-	takeLatest(types.UPDATE_AUDIENCE_PERFORMANCE, updateAudiencePerformance),
-	takeLatest(types.GET_FLIPCARDS_DATA, getFlipCardsData),
+  takeLatest(types.GET_AUDIENCE_DATA, getAudienceData),
+  takeLatest(types.UPDATE_AUDIENCE_PERFORMANCE, updateAudiencePerformance),
+  takeLatest(types.GET_FLIPCARDS_DATA, getFlipCardsData),
 ]
