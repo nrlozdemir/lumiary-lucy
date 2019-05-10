2e7db8f532ed67b15f8c10fa56b048360411b8c7
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2019-05-10 11:43:36 +0300

    feat: library detail theme applied

--- src/client/containers/Marketview/sections/detail/Slider/index.js
+++ src/client/containers/Marketview/sections/detail/Slider/index.js
@@ -4,6 +4,7 @@ import MarketViewSlider from 'Components/Sliders/Marketview'
 
 import style from 'Containers/Marketview/style.scss'
 import { Module } from 'Components/Module'
+import { ThemeContext } from 'ThemeContext/themeContext'
 
 export default class Slider extends React.Component {
   constructor(props) {
@@ -25,55 +26,72 @@ export default class Slider extends React.Component {
     } = this.props
 
     return (
-      <Module
-        containerClass={style.sliderModuleContainer}
-        bodyClass={style.sliderModuleContainerBody}
-        moduleKey={moduleKey}
-        title={title}
-        filters={[
-          {
-            type: 'engagement',
-            selectKey: 'Mwplt-engagement',
-            placeHolder: 'Engagement',
-          },
-          {
-            type: 'timeRange',
-            selectKey: 'Mwplt-date',
-            placeHolder: 'Date',
-          },
-        ]}
-      >
-        <div className="col-12-no-gutters">
-          <MarketViewSlider items={data} changeVideo={changeSelectedVideo} />
-        </div>
-        <div className="col-12-no-gutters mt-56">
-          {selectedVideo &&
-            selectedVideo.options.map((card, index) => (
-              <div className={style.card} key={index}>
-                <p className={style.marketCardHeader}>{card.name}</p>
-                {card.compareValues.map((value, i) => (
-                  <div className={style.progressArea} key={i}>
-                    <p className={style.title}>{value.title}</p>
-                    <p className={style.progressText}>
-                      <span className={style.leftTitle}>{value.leftTitle}</span>
-                      <span className={style.rightTitle}>
-                        {value.rightTitle}
-                      </span>
-                    </p>
-                    <ProgressBar
-                      width={value.value}
-                      customBarClass={style.progressBar}
-                      customPercentageClass={
-                        i % 2 ? style.percentageRed : style.percentageBlue
-                      }
-                    />
+      <ThemeContext.Consumer>
+        {({ themeContext: { colors } }) => (
+          <Module
+            containerClass={style.sliderModuleContainer}
+            bodyClass={style.sliderModuleContainerBody}
+            moduleKey={moduleKey}
+            title={title}
+            filters={[
+              {
+                type: 'engagement',
+                selectKey: 'Mwplt-engagement',
+                placeHolder: 'Engagement',
+              },
+              {
+                type: 'timeRange',
+                selectKey: 'Mwplt-date',
+                placeHolder: 'Date',
+              },
+            ]}
+          >
+            <div className="col-12-no-gutters">
+              <MarketViewSlider
+                items={data}
+                changeVideo={changeSelectedVideo}
+              />
+            </div>
+            <div className="col-12-no-gutters mt-56">
+              {selectedVideo &&
+                selectedVideo.options.map((card, index) => (
+                  <div
+                    className={style.card}
+                    key={index}
+                    style={{
+                      backgroundColor: colors.bodyBackground,
+                      color: colors.textColor,
+                      border: ` 1px solid ${colors.moduleBorder}`,
+                    }}
+                  >
+                    <p className={style.marketCardHeader}>{card.name}</p>
+                    {card.compareValues.map((value, i) => (
+                      <div className={style.progressArea} key={i}>
+                        <p className={style.title}>{value.title}</p>
+                        <p className={style.progressText}>
+                          <span className={style.leftTitle}>
+                            {value.leftTitle}
+                          </span>
+                          <span className={style.rightTitle}>
+                            {value.rightTitle}
+                          </span>
+                        </p>
+                        <ProgressBar
+                          width={value.value}
+                          customBarClass={style.progressBar}
+                          customPercentageClass={
+                            i % 2 ? style.percentageRed : style.percentageBlue
+                          }
+                        />
+                      </div>
+                    ))}
+                    <p className={style.cardDescription}>{card.description} </p>
                   </div>
                 ))}
-                <p className={style.cardDescription}>{card.description} </p>
-              </div>
-            ))}
-        </div>
-      </Module>
+            </div>
+          </Module>
+        )}
+      </ThemeContext.Consumer>
     )
   }
 }
