a7f03b870b41cbee6af7bbaf568e6fccff07c032
serdarbasol <serdarbasol111@gmail.com>
2019-05-13 20:46:36 +0300

    feat: missing shotbyshot theme

--- src/client/components/Sliders/SingleItemSlider/index.js
+++ src/client/components/Sliders/SingleItemSlider/index.js
@@ -1,43 +1,44 @@
-import React from "react";
-import cn from "classnames";
-import Slider from "react-slick";
-import ScrollSlider from "rc-slider";
-import style from "./style.scss";
+import React from 'react'
+import cn from 'classnames'
+import Slider from 'react-slick'
+import ScrollSlider from 'rc-slider'
+import style from './style.scss'
 
 /* eslint-disable react/prefer-stateless-function */
 class SingleItemSlider extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       nav1: null,
       nav2: null,
-      slideIndex: 0
-    };
+      slideIndex: 0,
+    }
   }
   componentDidMount() {
-    const { selectedImage } = this.props;
+    const { selectedImage } = this.props
 
     if (selectedImage) {
-      this.slider1.slickGoTo(selectedImage);
-      this.slider2.slickGoTo(selectedImage);
+      this.slider1.slickGoTo(selectedImage)
+      this.slider2.slickGoTo(selectedImage)
     }
 
     this.setState({
       nav1: this.slider1,
-      nav2: this.slider2
-    });
+      nav2: this.slider2,
+    })
   }
   onScrollChange(val) {
-    this.state.nav1.slickGoTo(val);
-    this.state.nav2.slickGoTo(val);
-    this.setState({ slideIndex: val });
+    this.state.nav1.slickGoTo(val)
+    this.state.nav2.slickGoTo(val)
+    this.setState({ slideIndex: val })
   }
   render() {
+    console.log('##', this.props)
     return (
-      <div className={cn(style.sliderClass, "libraryDetailSingleItemSlider")}>
+      <div className={cn(style.sliderClass, 'libraryDetailSingleItemSlider')}>
         <Slider
           asNavFor={this.state.nav2}
-          ref={slider => (this.slider1 = slider)}
+          ref={(slider) => (this.slider1 = slider)}
           swipeToSlide={false}
           arrows={false}
           beforeChange={(current, next) => this.setState({ slideIndex: next })}
@@ -48,10 +49,10 @@ class SingleItemSlider extends React.Component {
             </div>
           ))}
         </Slider>
-        <div className={cn(style.thumbnailContainer, "mt-32")}>
+        <div className={cn(style.thumbnailContainer, 'mt-32')}>
           <Slider
             asNavFor={this.state.nav1}
-            ref={slider => (this.slider2 = slider)}
+            ref={(slider) => (this.slider2 = slider)}
             swipeToSlide={false}
             focusOnSelect={true}
             variableWidth={true}
@@ -65,7 +66,7 @@ class SingleItemSlider extends React.Component {
             {this.props.slideImages.map((image, i) => (
               <div
                 className={cn(style.thumbnail, {
-                  [style["activeThumb"]]: i === this.state.slideIndex
+                  [style['activeThumb']]: i === this.state.slideIndex,
                 })}
                 key={`thumbnailImage-${i}`}
               >
@@ -79,44 +80,44 @@ class SingleItemSlider extends React.Component {
             step={null}
             defaultValue={0}
             value={this.state.slideIndex}
-            onChange={val => this.onScrollChange(val)}
+            onChange={(val) => this.onScrollChange(val)}
             max={this.props.slideImages.length}
             handleStyle={{
-              backgroundColor: "white",
+              ...this.props.customHandleStyle,
               borderRadius: '50%',
-              width: "16px",
-              height: "16px",
-              marginTop: "-6px",
-              position: 'absolute'
+              width: '16px',
+              height: '16px',
+              marginTop: '-6px',
+              position: 'absolute',
             }}
             trackStyle={{
-              height: "16px",
-              backgroundColor: "transparent"
+              height: '16px',
+              backgroundColor: 'transparent',
             }}
             railStyle={{
-              height: "5px",
-              borderRadius: "10px",
-              backgroundColor: "#21243B"
+              height: '5px',
+              borderRadius: '10px',
+              backgroundColor: '#21243B',
             }}
             dotStyle={{
-              width: "0px",
-              height: "0px",
+              width: '0px',
+              height: '0px',
               border: 0,
-              top: "0px"
+              top: '0px',
             }}
             marks={this.props.slideImages.reduce(
               (prevData, currenData, currentIndex) => {
-                return { ...prevData, [currentIndex]: "" };
+                return { ...prevData, [currentIndex]: '' }
               },
               {}
             )}
           />
         </div>
       </div>
-    );
+    )
   }
 }
 
-SingleItemSlider.propTypes = {};
+SingleItemSlider.propTypes = {}
 
-export default SingleItemSlider;
+export default SingleItemSlider
