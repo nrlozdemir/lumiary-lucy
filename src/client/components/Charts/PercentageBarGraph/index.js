c13bcf5a7c805924c7186a5dc41d68e12eef0530
Nurullah Ozdemir <nrlozdemir@gmail.com>
2019-05-21 19:06:17 +0300

    percentage graph component styling

--- src/client/components/Charts/PercentageBarGraph/index.js
+++ src/client/components/Charts/PercentageBarGraph/index.js
@@ -7,10 +7,9 @@ const pdata = {"datasets": [{"data": [10, 12, 14, 16, 18, 20, 22, 24, 26,
 	28, 30, 32, 34, 36, 40, 48, 58, 70, 80,
 	88, 94, 98,
 	100,
-	98, 90, 80,
+	96, 90, 80,
 	70, 60, 54, 48, 42, 36, 34, 32, 30, 28,
 	26, 24, 22, 20, 18]}]}
-
 
 const PercentageBarGraph = ({
   percentage,
@@ -19,92 +18,81 @@ const PercentageBarGraph = ({
   id,
   width = 238,
   height = 44,
-  backgroundColor,
+  backgroundColor
 }) => {
   const active = Math.round((60 / 100) * percentage)
   return (
     <div className={id}>
-      <div
-        className={style.percentageContainer}
-        style={{ backgroundColor: backgroundColor }}
-      >
+      <div className={style.percentageContainer}>
         {!disableLabels && <div className={style.percentage}>{percentage}</div>}
-        <div
-          className={classnames(style.percentageGraph, {
-            [style.noLabel]: disableLabels,
-          })}
-        >
-
+        <div className={classnames(style.percentageGraph)}>
           <StackedPercentageChart
-                    key={Math.random()}
-                    backgroundColor="transparent"
-                    borderColor="transparent"
-                    width={width}
-                    height={height}
-                    chartType= 'percentageGraph'
-                    dataSet = {
-                      {
-                        labels: pdata.datasets[0].data,
-                        datasets: pdata.datasets,
-                      }
+            key={Math.random()}
+            width={width}
+            height={height}
+            chartType= 'percentageGraph'
+            dataSet={
+              {
+                labels: pdata.datasets[0].data,
+                datasets: pdata.datasets
+              }
+            }
+            removeTooltip={true}
+            removePointRadius={true}
+            xAxesFlatten={false}
+            flattenFirstSpace={0}
+            flattenLastSpace={0}
+            options={{
+              responsive: false,
+							color: color,
+							tickColor: 'black',
+              layout: {
+                padding: {
+                  bottom: -10
+                }
+              },
+              scales: {
+                xAxes: [
+                  {
+                    gridLines: {
+                      display: false,
+                      lineWidth: 1,
+                      drawBorder: false,
+                      drawTicks: false,
+                      drawOnChartArea: false
+                    },
+                    ticks: {
+                      display: false,
+                      beginAtZero: true,
+                      padding: 0,
+                      stepSize: 1
                     }
-                    removeTooltip={true}
-                    removePointRadius={true}
-                    xAxesFlatten={false}
-                    flattenFirstSpace={0}
-                    flattenLastSpace={0}
-                    options={{
-                      responsive: false,
-                      color: color,
-                      chartType: 'percentageGraph',
-                      layout: {
-                        padding: {
-                          bottom: -10
-                        }
-                      },
-                      scales: {
-                        xAxes: [
-                          {
-                            gridLines: {
-                              display: false,
-                              lineWidth: 1,
-                              drawBorder: false,
-                              drawTicks: false,
-                              drawOnChartArea: false
-                            },
-                            ticks: {
-                              display: false,
-                              beginAtZero: true,
-                              padding: 0,
-                              stepSize: 1,
-                            },
-                          },
-                        ],
-                        yAxes: [
-                          {
-                            gridLines: {
-                              display: false,
-                              drawBorder: false,
-                              drawTicks: false,
-                              drawOnChartArea: false
-                            },
-                            ticks: {
-                              display: false,
-                              beginAtZero: true,
-                              padding: 0,
-                              max: 100,
-                              stepSize: 10,
-                            },
-                          },
-                        ],
-                      },
-                    }}
-                  />
-
-
-          </div>
+                  }
+                ],
+                yAxes: [
+                  {
+                    gridLines: {
+                      display: false,
+                      drawBorder: false,
+                      drawTicks: false,
+                      drawOnChartArea: false
+                    },
+                    ticks: {
+                      display: false,
+                      beginAtZero: true,
+                      padding: 0,
+                      max: 100,
+                      stepSize: 10,
+                    }
+                  }
+                ]
+              }
+            }}
+          />
         </div>
+				{!disableLabels && <div className={style.cvScoreLabel}>CV Score</div>}
       </div>
+    </div>
   )
 }
 
