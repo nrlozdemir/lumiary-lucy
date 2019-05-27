0c5edd92c1f787ae132aaa88390beeb480e7b056
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2019-05-27 18:11:29 +0300

    feat: total competitors added to marketview

--- src/client/containers/Marketview/sections/main/TotalCompetitorCard/index.js
+++ src/client/containers/Marketview/sections/main/TotalCompetitorCard/index.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import BarChartModule from 'Components/Modules/BarChartModule'
 
-import { chartCombineDataset, randomKey } from 'Utils'
+import { chartCombineDataset, randomKey, isDataSetEmpty } from 'Utils'
 import { TotalCompetitor_DatasetOptions } from './options'
 
 const TotalCompetitorCard = ({ data }) => {
   const combineData = chartCombineDataset(
     {
       labels: ['0-15s', '16-30s', '31-60s', '61s+'],
-      datasets: data,
+      datasets: data.datasets,
     },
     TotalCompetitor_DatasetOptions
   )
-
+  const isEmpty = isDataSetEmpty(combineData)
+  console.log(isEmpty)
   return (
     <BarChartModule
       moduleKey={randomKey(10)}
       barData={combineData}
       title="Total Competitor Views By Duration"
       height={55}
+      isEmpty={isEmpty}
       titleLabels={[
         'Barstool Sports',
         'SB Nation',
