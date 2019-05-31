3238309d614ef68f11b3fe2cd24dcaf9fd40f76b
serdarbasol <serdarbasol111@gmail.com>
2019-05-31 15:20:05 +0300

    fix: webpack bundle size fixed, removed misc and font scss on all imported styles

--- src/client/containers/DynamicImport/index.js
+++ src/client/containers/DynamicImport/index.js
@@ -1,31 +1,34 @@
-import React, { Component } from "react"
-import Navbar from "Components/Navbar/index";
+import React, { Component } from 'react'
+import Navbar from 'Components/Navbar/index'
+import 'Scss/fonts.scss'
+import 'Scss/_misc/_icons.scss'
 
 class DynamicImport extends Component {
   _isMounted = false
   state = {
-    component: null
+    component: null,
   }
   componentDidMount() {
     this._isMounted = true
-    this.props.load()
-      .then((component) => {
-        if (this._isMounted) {
-          this.setState(() => ({
-            component: component.default ? component.default : component
-          }))
-        }
-      })
+    this.props.load().then((component) => {
+      if (this._isMounted) {
+        this.setState(() => ({
+          component: component.default ? component.default : component,
+        }))
+      }
+    })
   }
   componentWillUnmount() {
     this._isMounted = false
   }
   render() {
-    const { removeNavbar, children, match, routeConfig } = this.props;
-    const { component } = this.state;
+    const { removeNavbar, children, match, routeConfig } = this.props
+    const { component } = this.state
     return (
       <React.Fragment>
-        {!removeNavbar ? <Navbar match={match} routeConfig={routeConfig} /> : null}
+        {!removeNavbar ? (
+          <Navbar match={match} routeConfig={routeConfig} />
+        ) : null}
         {children(component)}
       </React.Fragment>
     )
