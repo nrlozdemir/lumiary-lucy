4c2a5afe8e83a70565b74b42ee339dad89ec5616
Shlomo Mounitz <smounitz@gmail.com>
2019-06-06 13:48:10 -0400

    feat: add QAPI

--- src/client/utils/globals.js
+++ src/client/utils/globals.js
@@ -1,5 +1,7 @@
 export const API_ROOT = process.env.API_ROOT
 export const API_VERSION = process.env.API_VERSION
+export const QAPI_ROOT = process.env.QAPI_ROOT
+export const QAPI_VERSION = process.env.QAPI_VERSION
 export const staticUrl = process.env.STATIC_URL
 export const mediaUrl = process.env.MEDIA_URL
 export const breakpoints = process.env.BREAKPOINTS
