192af95612734ac9e81c94872f0afeafba9f56cc
Nurullah Ozdemir <nrlozdemir@gmail.com>
2019-06-12 11:56:23 +0300

    dummy data for radar chart

--- src/client/containers/LibraryDetail/sections/LibraryDetailShotByShot/index.js
+++ src/client/containers/LibraryDetail/sections/LibraryDetailShotByShot/index.js
@@ -263,8 +263,58 @@ class LibraryDetailShotByShot extends React.Component {
   }
 
   render() {
-    const { radarData, shotInfo, radarChartData } = this.props
+    const { radarData, shotInfo } = this.props
     const { selectedImage, viewportShots, sliderMarks, shotsTotalWidth } = this.state
+
+    const radarChartData = {
+      "labels":[
+         "#fff20d",
+         "#f8b90b",
+         "#eb7919",
+         "#dd501d",
+         "#cc2226",
+         "#b83057",
+         "#923683",
+         "#79609b",
+         "#3178b0",
+         "#229a78",
+         "#13862b",
+         "#aac923"
+      ],
+      "datasets":[
+         {
+            "data":[
+               65,
+               59,
+               34,
+               81,
+               56,
+               40,
+               65,
+               59,
+               34,
+               81,
+               56
+            ]
+         },
+         {
+            "data":[
+               28,
+               48,
+               40,
+               19,
+               96,
+               74,
+               65,
+               59,
+               34,
+               81,
+               56
+            ]
+         }
+      ]
+   }
+
 
     return (
       <ThemeContext.Consumer>
