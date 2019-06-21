2be3fb83224a4e4433d680f2c0f8cef9d9b10921
Wally Faye <wally@quickframe.com>
2019-06-21 13:46:40 -0400

    feat: pull remote data

--- src/client/sagas/quickview.js
+++ src/client/sagas/quickview.js
@@ -1,61 +1,64 @@
-import { takeLatest, call, put } from 'redux-saga/effects'
+import { takeLatest, call, put, select } from 'redux-saga/effects'
 import axios from 'axios'
+import querystring from 'querystring'
 
 import { types, actions } from 'Reducers/quickview'
+import { selectAuthProfile } from 'Reducers/auth'
+import { getDataFromApi } from 'Utils/api'
 
 //mocks
 import quickviewItemsData from 'Api/mocks/quickviewItemsMock.json'
 
-function getQuickviewItemsApi() {
+function* getQuickviewItemsApi({ platform, metric, daterange, uuid, }) {
   //this will use ajax function in utils/api when real data is provided
 
-  const platform = 'facebook'
-  const serverData = {
-    "maxVideo": {
-      "id": 1130,
-      "uuid": "13c710aa-1c0b-450a-9fe1-0b4274a83fd4",
-      "title": "The levels of disrespect...\n\n(via _kennedychandler1/IG)",
-      "fileName": "d65aa957-d094-4cf3-8d37-dafe50e752ea/facebook/13c710aa-1c0b-450a-9fe1-0b4274a83fd4/442696289887625.mp4",
-      "date": "2019-06-04T15:55:13.000Z",
-      "brand_uuid": "d65aa957-d094-4cf3-8d37-dafe50e752ea",
-      "platform": null,
-      "createdAt": "2019-06-04T17:14:42.914Z",
-      "updatedAt": "2019-06-04T19:41:00.899Z",
-      "cvScores.value": 15.376738536833,
-      "aspectRatios.value": "4:5",
-      "frameRates.value": 30,
-      "durations.value": 5.933984,
-      "durations.bucket_name": '0-15',
-      "pacings.value": 5.9,
-      "pacings.bucket_name": 'Medium',
-      "formats.value": "None",
-      "resolutions.value": "480p"
-    },
-    "minVideo": {
-      "id": 1125,
-      "uuid": "a9be4618-0296-48c6-849c-de7ac547f992",
-      "title": "Steph wishes he would've handled things differently",
-      "fileName": "d65aa957-d094-4cf3-8d37-dafe50e752ea/facebook/a9be4618-0296-48c6-849c-de7ac547f992/2435667196490605.mp4",
-      "date": "2019-06-04T16:51:46.000Z",
-      "brand_uuid": "d65aa957-d094-4cf3-8d37-dafe50e752ea",
-      "platform": null,
-      "createdAt": "2019-06-04T17:12:22.854Z",
-      "updatedAt": "2019-06-04T19:40:58.746Z",
-      "cvScores.value": 1,
-      "aspectRatios.value": "1:1",
-      "frameRates.value": 29.97,
-      "durations.value": 9.34301,
-      "durations.bucket_name": '31-60',
-      "pacings.value": 2.0353685,
-      "pacings.bucket_name": 'Slowest',
-      "formats.value": "None",
-      "resolutions.value": "480p"
-    },
-    "differences": {
-      "duration": 36.48744890565246,
-      "pacings": 189.87379926534186
-    }
-  }
+  // // const platform = 'facebook'
+  // const serverData = {
+  //   "maxVideo": {
+  //     "id": 1130,
+  //     "uuid": "13c710aa-1c0b-450a-9fe1-0b4274a83fd4",
+  //     "title": "The levels of disrespect...\n\n(via _kennedychandler1/IG)",
+  //     "fileName": "d65aa957-d094-4cf3-8d37-dafe50e752ea/facebook/13c710aa-1c0b-450a-9fe1-0b4274a83fd4/442696289887625.mp4",
+  //     "date": "2019-06-04T15:55:13.000Z",
+  //     "brand_uuid": "d65aa957-d094-4cf3-8d37-dafe50e752ea",
+  //     "platform": null,
+  //     "createdAt": "2019-06-04T17:14:42.914Z",
+  //     "updatedAt": "2019-06-04T19:41:00.899Z",
+  //     "cvScores.value": 15.376738536833,
+  //     "aspectRatios.value": "4:5",
+  //     "frameRates.value": 30,
+  //     "durations.value": 5.933984,
+  //     "durations.bucket_name": '0-15',
+  //     "pacings.value": 5.9,
+  //     "pacings.bucket_name": 'Medium',
+  //     "formats.value": "None",
+  //     "resolutions.value": "480p"
+  //   },
+  //   "minVideo": {
+  //     "id": 1125,
+  //     "uuid": "a9be4618-0296-48c6-849c-de7ac547f992",
+  //     "title": "Steph wishes he would've handled things differently",
+  //     "fileName": "d65aa957-d094-4cf3-8d37-dafe50e752ea/facebook/a9be4618-0296-48c6-849c-de7ac547f992/2435667196490605.mp4",
+  //     "date": "2019-06-04T16:51:46.000Z",
+  //     "brand_uuid": "d65aa957-d094-4cf3-8d37-dafe50e752ea",
+  //     "platform": null,
+  //     "createdAt": "2019-06-04T17:12:22.854Z",
+  //     "updatedAt": "2019-06-04T19:40:58.746Z",
+  //     "cvScores.value": 1,
+  //     "aspectRatios.value": "1:1",
+  //     "frameRates.value": 29.97,
+  //     "durations.value": 9.34301,
+  //     "durations.bucket_name": '31-60',
+  //     "pacings.value": 2.0353685,
+  //     "pacings.bucket_name": 'Slowest',
+  //     "formats.value": "None",
+  //     "resolutions.value": "480p"
+  //   },
+  //   "differences": {
+  //     "duration": 36.48744890565246,
+  //     "pacings": 189.87379926534186
+  //   }
+  // }
 
   const parseVideoResponse = (videoData) => {
     const { 
@@ -142,6 +145,17 @@ function getQuickviewItemsApi() {
     return maxVideo
   }
 
+  const serverData = yield call(
+    getDataFromApi,
+    {},
+    `/quickview?${querystring.stringify({
+      metric,
+      daterange,
+      platform,
+    })}`,
+    'GET'
+  )
+
   const serverObject = Object.keys(serverData).reduce((accumulator, compareKey) => {
     if(!accumulator[platform]){
       accumulator[platform] = [null, null]
@@ -164,19 +178,31 @@ function getQuickviewItemsApi() {
     return accumulator
   }, {})
 
-  console.log(serverObject)
-  console.log(quickviewItemsData)
+  console.log('serverObject', serverObject)
+  console.log('quickviewItemsData', quickviewItemsData)
 
-  return axios.get('/').then((res) => serverObject)
+  return serverObject
 }
 
 function* getQuickviewItemsSaga({ payload }) {
   try {
-    const { platform, data } = payload
+    const { platform = 'facebook', data = {} } = payload
+    const { metric = 'views', daterange = 'week' } = data
 
-    console.log('get quickview', data, platform)
+    const profile = yield select(selectAuthProfile)
+    const { brand = {} } = profile
+    const { uuid } = brand
 
-    const response = yield call(getQuickviewItemsApi)
+    if(!uuid){
+      throw new Error('brand uuid must be provided')
+    }
+
+    const response = yield call(getQuickviewItemsApi, {
+      platform,
+      metric,
+      daterange,
+      uuid,
+    })
 
     yield put({
       type: types.GET_QUICKVIEW_ITEMS_SUCCESS,
