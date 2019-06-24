4a2b533e19a8ffe8eb957d5a93f99993ba51e98b
Wally Faye <wally@quickframe.com>
2019-06-24 09:50:41 -0400

    feat: pull industry data

--- src/client/sagas/quickview.js
+++ src/client/sagas/quickview.js
@@ -60,21 +60,36 @@ function* getQuickviewItemsApi({ platform, metric, daterange, uuid, }) {
   //   }
   // }
 
-  const parseVideoResponse = (videoData) => {
+  const parseVideoResponse = ({ videoInfo, serverData }) => {
     const { 
       title = '', 
       brand_uuid = '', 
       uuid = '', 
-      'cvScores.value':cvScoreValue = '',
+      'cvScores.value':cvScoresValue = '',
+      'cvScores.bucket_name':cvScoresBucketName = '',
+      'cvScores.industry_percent':cvScoresIndustryPercent = '',
       'durations.value':durationsValue = '',
-      'pacings.value':pacingsValue = '',
       'durations.bucket_name':durationsBucketName = '',
+      'durations.industry_percent':durationsIndustryPercent = '',
+      'pacings.value':pacingsValue = '',
       'pacings.bucket_name':pacingsBucketName = '',
+      'pacings.industry_percent':pacingsIndustryPercent = '',
       'formats.value':formatsValue = '',
+      'formats.bucket_name':formatsBucketName = '',
+      'formats.industry_percent':formatsIndustryPercent = '',
+      'colors.value':colorsValue = '',
+      'colors.bucket_name':colorsBucketName = '',
+      'colors.industry_percent':colorsIndustryPercent = '',
       'aspectRatios.value':aspectRatiosValue = '',
+      'aspectRatios.bucket_name':aspectRatiosBucketName = '',
+      'aspectRatios.industry_percent':aspectRatiosIndustryPercent = '',
       'resolutions.value':resolutionsValue = '',
+      'resolutions.bucket_name':resolutionsBucketName = '',
+      'resolutions.industry_percent':resolutionsIndustryPercent = '',
       'frameRates.value':frameRatesValue = '',
-    } = videoData
+      'frameRates.bucket_name':frameRatesBucketName = '',
+      'frameRates.industry_percent':frameRatesIndustryPercent = '',
+    } = videoInfo
 
     const maxVideo = {
       video: {
@@ -82,62 +97,62 @@ function* getQuickviewItemsApi({ platform, metric, daterange, uuid, }) {
         videoUrl: `https://s3.amazonaws.com/quickframe-media-staging/lumiere/${brand_uuid}/${uuid}.mp4`,
         poster: `https://s3.amazonaws.com/quickframe-media-staging/lumiere/${brand_uuid}/${uuid}.mp4`,
         socialIcon: platform,
-        cvScore: cvScoreValue,
+        cvScore: cvScoresValue,
       },
       infos: [
         {
           "title": "Duration",
           "value": durationsBucketName,
+          "percentage": durationsIndustryPercent,
+          "text": "{percentage}% of industry videos are {value} sec in length"
+        },
+        {
+          "title": "Pacing",
+          "value": pacingsBucketName,
+          "percentage": pacingsIndustryPercent,
+          "text": "{percentage}% of industry videos have <b>{value} {title}</b>"
+        },
+        {
+          "title": "Format",
+          "value": formatsBucketName,
+          "percentage": formatsIndustryPercent,
+          "text": "{percentage}% of industry videos have a <b>{title}</b> of <b>{value}</b>"
+        },
+        {
+          "title": "Dominant Color",
+          "value": "N/A",
           "percentage": 0,
-          "text": "{value} of best performing industry videos are {percentage} sec in length"
+          "text": "{percentage}% of industry videos have a <b>{title}</b> of <b>{value}</b>"
         },
         {
-            "title": "Pacing",
-            "value": pacingsBucketName,
-            "percentage": 0,
-            "text": "{percentage} of best performing industry videos have <b>{value} {title}</b>"
+          "title": "Predominant Talent Age",
+          "value": "N/A",
+          "percentage": 0,
+          "text": "{percentage}% of industry videos are <b>{title}</b> of <b>{value}</b>"
         },
         {
-            "title": "Format",
-            "value": formatsValue,
-            "percentage": 0,
-            "text": "{percentage} of best performing industry videos have a <b>{title}</b> of <b>{value}</b>"
+          "title": "Predominant Talent Gender",
+          "value": "N/A",
+          "percentage": 0,
+          "text": "{percentage}% of industry videos are <b>{title}</b> of <b>{value}</b>"
         },
         {
-            "title": "Dominant Color",
-            "value": "N/A",
-            "percentage": 0,
-            "text": "{percentage} of best performing industry videos have a <b>{title}</b> of <b>{value}</b>"
+          "title": "Aspect Ratio",
+          "value": aspectRatiosBucketName,
+          "percentage": aspectRatiosIndustryPercent,
+          "text": "{percentage}% of industry videos have an <b>{title}</b> of <b>{value}</b>"
         },
         {
-            "title": "Predominant Talent Age",
-            "value": "N/A",
-            "percentage": 0,
-            "text": "{percentage} of best performing industry videos are <b>{title}</b> of <b>{value}</b>"
+          "title": "Resolution",
+          "value": resolutionsBucketName,
+          "percentage": resolutionsIndustryPercent,
+          "text": "{percentage}% of industry videos have a <b>{title}</b> of <b>{value}</b>"
         },
         {
-            "title": "Predominant Talent Gender",
-            "value": "N/A",
-            "percentage": 0,
-            "text": "{percentage} of best performing industry videos are <b>{title}</b> of <b>{value}</b>"
-        },
-        {
-            "title": "Aspect Ratio",
-            "value": aspectRatiosValue,
-            "percentage": 0,
-            "text": "{percentage} of best performing industry videos have an <b>{title}</b> of <b>{value}</b>"
-        },
-        {
-            "title": "Resolution",
-            "value": resolutionsValue,
-            "percentage": 0,
-            "text": "{percentage} of best performing industry videos have a <b>{title}</b> of <b>{value}</b>"
-        },
-        {
-            "title": "Frame Rate",
-            "value": `${frameRatesValue}fps`,
-            "percentage": 0,
-            "text": "{percentage} of best performing industry videos have a <b>{title}</b> of <b>{value}</b>"
+          "title": "Frame Rate",
+          "value": `${frameRatesBucketName}fps`,
+          "percentage": frameRatesIndustryPercent,
+          "text": "{percentage}% of industry videos have a <b>{title}</b> of <b>{value}</b>"
         }
       ]
     }
@@ -164,11 +179,11 @@ function* getQuickviewItemsApi({ platform, metric, daterange, uuid, }) {
 
     switch (compareKey) {
       case 'maxVideo':
-        accumulator[platform][1] = parseVideoResponse(videoInfo)
+        accumulator[platform][1] = parseVideoResponse({videoInfo, serverData})
         break
 
       case 'minVideo':
-        accumulator[platform][0] = parseVideoResponse(videoInfo)
+        accumulator[platform][0] = parseVideoResponse({videoInfo, serverData})
         break
 
       case 'differences':
