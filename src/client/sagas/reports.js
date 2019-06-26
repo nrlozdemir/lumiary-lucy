068b214e9f5ca34b05bab711bb711da1e78bbd1b
Yusuf Bakır <f.yusufbakir@gmail.com>
2019-06-26 13:36:34 +0300

    removed console.log

--- src/client/sagas/reports.js
+++ src/client/sagas/reports.js
@@ -74,8 +74,6 @@ function* brandInsightSubmit({ payload }) {
       title,
     }
 
-    console.log(parameters)
-
     yield put(actions.brandInsightFormSubmitSuccess(parameters))
     yield put(
       push(
