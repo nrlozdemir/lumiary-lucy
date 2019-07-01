1d89dcb0dd0d5e531a62c1f97526d9a68928b760
Jazzy Guo <jazzy@quickframe.com>
2019-07-01 13:52:41 -0400

    feat: added loading to shotbyshot people/objects/color

---
+++ src/client/containers/LibraryDetail/sections/LibraryDetailShotByShot/ConfidenceImage.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+import style from './style.scss'
+import ProgressBar from 'Components/ProgressBar'
+import PropTypes from 'prop-types'
+
+import cx from 'classnames'
+
+// If an image 404s, this will return null
+class ConfidenceImage extends Component {
+  constructor() {
+    super()
+    this.state = {
+      urlError: false,
+    }
+  }
+
+  onError = () => {
+    this.setState({ urlError: true })
+  }
+
+  render() {
+    const { urlError } = this.state
+    const { confidence, imgUrl, minAge, colors, label } = this.props
+
+    return urlError ? null : (
+      <div
+        className={cx(style.tabPanelItem, 'grid-container')}
+        style={{
+          background: colors.shotByShotBackground,
+          borderColor: colors.shotByShotBorder,
+          marginRight: '16px !important',
+        }}
+      >
+        <div className="col-5-no-gutters">
+          <img
+            src={imgUrl}
+            className={cx(style.imageItem, 'grid-container')}
+            onError={this.onError}
+          />
+        </div>
+        <div className="col-7-no-gutters">
+          <div className="pt-20">
+            <div className={style.progressbarContainer}>
+              <div className={style.barOptions}>
+                <p>{label}</p>
+                <p>{confidence}% Accurate</p>
+              </div>
+              <ProgressBar
+                width={confidence}
+                customBarClass={style.progressBar}
+                customPercentageClass={style.percentage}
+              />
+            </div>
+          </div>
+
+          {minAge && (
+            <div className="pt-20">
+              <div className={style.progressbarContainer}>
+                <div className={style.barOptions}>
+                  <p>{minAge} Y/O</p>
+                  <p>{minAge.toFixed(0)}% Accurate</p>
+                </div>
+                <ProgressBar
+                  width={confidence}
+                  customBarClass={style.progressBar}
+                  customPercentageClass={style.percentage}
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+ProgressBar.propTypes = {
+  imgUrl: PropTypes.string,
+  isPeople: PropTypes.bool,
+  label: PropTypes.string,
+  confidence: PropTypes.string,
+  minAge: PropTypes.string,
+}
+
+ProgressBar.defaultProps = {
+  imgUrl: '',
+  minAge: null,
+}
+
+export default ConfidenceImage
