646bbdf951723f292a860f24b070e9f390943a91
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2019-07-01 16:47:22 +0300

    fix: report page brand insight color

--- src/client/components/PageForms/Reports/BrandInsight/index.js
+++ src/client/components/PageForms/Reports/BrandInsight/index.js
@@ -38,6 +38,7 @@ const ReportsForm = (props) => {
                   placeholder="Select One"
                   label="Choose a Brand"
                   validate={required}
+                  isReportDropdown
                 />
               </div>
               <div className={style.selectionArea}>
@@ -50,6 +51,7 @@ const ReportsForm = (props) => {
                   placeholder="Social Platform"
                   label="Social Platform"
                   validate={required}
+                  isReportDropdown
                 />
               </div>
               <div className={style.selectionArea}>
@@ -62,6 +64,7 @@ const ReportsForm = (props) => {
                   placeholder="Select One"
                   label="Engagement"
                   validate={required}
+                  isReportDropdown
                 />
               </div>
               <div className={style.selectionArea}>
@@ -74,6 +77,7 @@ const ReportsForm = (props) => {
                   placeholder="Select Date"
                   label="Date Range"
                   validate={required}
+                  isReportDropdown
                 />
               </div>
               <div className={style.selectionArea}>
