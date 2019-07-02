385df46ddba850fd7881f38a9ddb2586db1cf53e
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2019-07-02 18:06:06 +0300

    fix: text replacement

--- src/client/utils/text.js
+++ src/client/utils/text.js
@@ -3,29 +3,41 @@ import React from 'react'
 const replaceBoldElement = (text) => {
   const regex = new RegExp(/<b>(.*?)<\/b>/g)
 
-  if( ! regex.test(text)){
+  if (!regex.test(text)) {
     return text
   }
 
   const matched = text.match(regex)
 
-  const values = matched.map(val => val.replace(/<\/?b>/g, ''))
-  if ( ! values.length){
+  const values = matched.map((val) => val.replace(/<\/?b>/g, ''))
+  if (!values.length) {
     return text
   }
 
-  return (<React.Fragment>
-    {text.split(regex).reduce((prev, current, i) => {
-      if( ! i) {
-        return [current]
-      }
-      return prev.concat(
-        values.includes(current)
-        ? <strong key={i + current}>{current}</strong>
-        : current
-      );
-    }, [])}
-  </React.Fragment>)
+  return (
+    <React.Fragment>
+      {text.split(regex).reduce((prev, current, i) => {
+        if (!i) {
+          return [current]
+        }
+        return prev.concat(
+          values.includes(current) ? (
+            <strong key={i + current}>{current}</strong>
+          ) : (
+            current
+          )
+        )
+      }, [])}
+    </React.Fragment>
+  )
+}
+const replaceBoldString = (str) => {
+  return str.replace(
+    /(\d+\%)|(\d+\.\d+\%)|Fast|Meduim|Slow|Slowest|0-15s|16-30s|31-60s|61+|Live Action|Cinemagraph|Stop Motion|Animation/gi,
+    function(x) {
+      return '<b>' + x + '</b>'
+    }
+  )
 }
 
 const textEdit = (text, item) => {
@@ -37,6 +49,4 @@ const textEdit = (text, item) => {
   return text
 }
 
-export {
-  textEdit
-}
+export { textEdit, replaceBoldString }
