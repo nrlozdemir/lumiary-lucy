8b72d7acc7df0ac1e88dfd8973cd8ba0e1443635
Nurullah Ozdemir <nrlozdemir@gmail.com>
2019-07-04 19:45:34 +0300

    total views card bar spacing

--- src/client/containers/Marketview/sections/main/TotalViewsCard/index.js
+++ src/client/containers/Marketview/sections/main/TotalViewsCard/index.js
@@ -82,7 +82,7 @@ class TotalViewsChart extends React.Component {
       >
         <div className="grid-collapse">
           <div className="col-6 mt-24">
-            <StackedBarChart barData={loading ? {} : barData} />
+            <StackedBarChart barData={loading ? {} : barData} barSpacing={2} />
           </div>
           <div className="col-6">
             <DoughnutChart
