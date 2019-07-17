61b07d8dfb128c1164c5ef930471e57a2630f78d
Nurullah Ozdemir <nrlozdemir@gmail.com>
2019-07-17 19:51:20 +0300

    tooltip component for global usage

--- src/client/components/ToolTip/index.js
+++ src/client/components/ToolTip/index.js
@@ -2,31 +2,45 @@ import React from 'react'
 import classnames from 'classnames'
 import style from './style.scss'
 import { ThemeContext } from 'ThemeContext/themeContext'
+import ReactTooltip from 'react-tooltip'
 
-export default ({ show = false, children, width = 480 }) => {
+export default (props) => {
   return (
     <ThemeContext.Consumer>
-      {({ themeContext: { colors } }) => (
-        <div
-          className={classnames(style.toolTip, show && style.show)}
-          style={{
-            color: colors.tooltipTextColor,
-            background: colors.tooltipBackground,
-            width: `${width}px`
-          }}
-        >
-          <span
-            className={style.toolTipTick}
-            style={{
-              border: `10px solid ${colors.tooltipBackground}`,
-              borderColor: `transparent transparent ${
-                colors.tooltipBackground
-              } ${colors.tooltipBackground}`,
-            }}
-          />
-          {children}
-        </div>
-      )}
+      {({ themeContext: { colors } }) => {
+        const theme = colors.themeType
+        
+        if (!!props.default && props.default === true) {
+
+          return (
+            <ReactTooltip
+              {...props}
+              type={theme === 'dark' ? 'dark' : 'light'}
+            />
+          )
+        } else {
+          return (
+            <div
+              className={classnames(style.toolTip, true && style.display)}
+              style={{
+                color: colors.tooltipTextColor,
+                background: colors.tooltipBackground,
+              }}
+            >
+              <span
+                className={style.toolTipTick}
+                style={{
+                  border: `10px solid ${colors.tooltipBackground}`,
+                  borderColor: `transparent transparent ${
+                    colors.tooltipBackground
+                  } ${colors.tooltipBackground}`,
+                }}
+              />
+              {!!props.children && props.children}
+            </div>
+          )
+        }
+      }}
     </ThemeContext.Consumer>
   )
 }
