b6ebc0fd4de3a2cf130b5341fa3322577bac6ab5
Jazzy Guo <jazzy@quickframe.com>
2019-07-22 10:00:32 -0400

    Revert "fix: indent"
    This reverts commit 8bab75ed309959491a45943aa201ed2dee71c89b.

--- src/client/components/Charts/MarketView/PacingPieChart/index.js
+++ src/client/components/Charts/MarketView/PacingPieChart/index.js
@@ -19,13 +19,7 @@ const PacingPieChart = ({ data = {} }) => {
                 callbacks: {
                   title: () => '',
                   label: function(tooltipItem, data) {
-                    return (
-                      (data &&
-                        data.datasets &&
-                        data.datasets[0] &&
-                        data.datasets[0].data[tooltipItem['index']]) ||
-                      ''
-                    )
+                    return data && data.datasets && data.datasets[0] && data.datasets[0].data[tooltipItem['index']] || ''
                   },
                 },
               }),
