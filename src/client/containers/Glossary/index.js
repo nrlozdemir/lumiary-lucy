a9bc52802e6874ce3cacf503fd31d0f0af634f91
serdarbasol <serdarbasol111@gmail.com>
2019-07-24 17:23:28 +0300

    feat: grid container for side and main

--- src/client/containers/Glossary/index.js
+++ src/client/containers/Glossary/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
+import style from './style.scss'
 import LetterBar from './sections/letterBar'
 import SideBar from './sections/sideBar'
 import MainContentArea from './sections/mainContentArea'
@@ -12,8 +13,10 @@ class Glossary extends Component {
     return (
       <div className="grid-container col-12">
         <LetterBar />
-        <SideBar />
-        <MainContentArea />
+        <div className={style.glossaryBodyContainer}>
+          <SideBar />
+          <MainContentArea />
+        </div>
       </div>
     )
   }
--- src/client/containers/Glossary/sections/sideBar.js
+++ src/client/containers/Glossary/sections/sideBar.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import { withTheme } from 'ThemeContext/withTheme'
+import style from '../style.scss'
 
 class SidaBar extends Component {
   render() {
-    return <div>SidaBar</div>
+    const { colors } = this.props.themeContext
+    return <div className={style.glossarySidebar}>SidaBar</div>
   }
 }
 
-export default SidaBar
+export default withTheme(SidaBar)
