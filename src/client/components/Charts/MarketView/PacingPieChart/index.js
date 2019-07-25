141f75783594ab6b4039cccbbf403fdf994fa6ef
Nurullah Ozdemir <nrlozdemir@gmail.com>
2019-07-25 15:13:39 +0300

    marketview pacing chart staging bug

--- src/client/components/Charts/MarketView/PacingPieChart/index.js
+++ src/client/components/Charts/MarketView/PacingPieChart/index.js
@@ -25,7 +25,7 @@ const PacingPieChart = ({ data = {} }) => {
             height={240}
             width={240}
             options={{
-              tooltips: customChartToolTip(colors, {
+              tooltips: {
                 callbacks: {
                   title: function(tooltipItem, data) {
                     const name =
@@ -44,12 +44,24 @@ const PacingPieChart = ({ data = {} }) => {
                     return `${metricSuffix(parseInt(count) || 0)} Shares`
                   },
                 },
-                titleAlign: 'center',
-                footerAlign: 'center',
-                bodyAlign: 'center',
+                backgroundColor: colors.tooltipBackground,
+                cornerRadius: 6,
+                titleFontColor: colors.chartTooltipColor,
+                titleFontStyle: 'normal',
+                mode: 'point',
+                bodyFontFamily: 'ClanOT',
+                titleFontFamily: 'ClanOT',
+                bodyFontColor: colors.chartTooltipColor,
+                xPadding: 8,
+                yPadding: 12,
+                bodyFontStyle: 'normal',
+                displayColors: false,
+                //titleAlign: 'center',
+                //footerAlign: 'center',
+                //bodyAlign: 'center',
                 xPadding: 12,
                 yPadding: 12,
-              }),
+              },
               responsive: false,
               legend: {
                 display: false,
