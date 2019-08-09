f635a5c032b270201777069647d8e670e0a04a8c
Jazzy Guo <jazzy@quickframe.com>
2019-08-09 12:20:14 -0400

    feat: sentry 2 prod

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/browser'
 
-import App from "./App";
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById("app"));
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://71e88d90e3b4440e87da3550dec33e52@sentry.io/1355450',
+    environment: process.env.ENVIRONMENT,
+  })
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
