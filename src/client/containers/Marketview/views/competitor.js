4ddf53f878b8704f11472c11f4eab9d8b2fdbeb9
Jazzy Guo <jazzy@quickframe.com>
2019-08-14 16:02:22 -0400

    feat: day filter

--- src/client/containers/Marketview/views/competitor.js
+++ src/client/containers/Marketview/views/competitor.js
@@ -13,6 +13,89 @@ import TopPerformingProperty from '../sections/detail/TopPerformingProperty'
 /* eslint-disable react/prefer-stateless-function */
 export class Competitor extends React.Component {
   render() {
+    // return (
+    //   <React.Fragment>
+    //     <Slider
+    //       moduleKey="MarketView/Competitor/Slider"
+    //       title="Top Performing Competitor Videos"
+    //       filters={[
+    //         {
+    //           type: 'metric',
+    //           selectKey: 'Mwplt-engagement',
+    //           placeHolder: 'Engagement',
+    //         },
+    //         {
+    //           type: 'dateRange',
+    //           selectKey: 'Mwplt-date',
+    //           placeHolder: 'Date',
+    //         },
+    //       ]}
+    //       container="competitor"
+    //     />
+    //     <TopVideosOverTime
+    //       moduleKey="MarketView/Competitor/TopVideosOverTime"
+    //       title="Top Videos Over Time By Competitor"
+    //       filters={[
+    //         {
+    //           type: 'propertyEngagement',
+    //           selectKey: 'propertyEngagement',
+    //           placeHolder: 'Engagement by property',
+    //         },
+    //         {
+    //           type: 'dateRange',
+    //           selectKey: 'dateRange',
+    //           placeHolder: 'dateRange',
+    //         },
+    //       ]}
+    //       references={[
+    //         {
+    //           className: 'bg-cool-blue',
+    //           text: 'Barstool Sports',
+    //         },
+    //         {
+    //           className: 'bg-lighter-purple',
+    //           text: 'SB Nation',
+    //         },
+    //         {
+    //           className: 'bg-coral-pink',
+    //           text: 'ESPN',
+    //         },
+    //         {
+    //           className: 'bg-cool-grey',
+    //           text: 'Scout Media',
+    //         },
+    //         {
+    //           className: 'bg-dusk"',
+    //           text: 'Fanside',
+    //         },
+    //       ]}
+    //       container="competitor"
+    //     />
+    //     <TopSimilarProperties
+    //       moduleKey="MarketView/Competitor/TopSimilarProperties"
+    //       title="Top Similar Properties Of Top Videos"
+    //       filters={[
+    //         {
+    //           type: 'dateRange',
+    //           selectKey: 'dateRange',
+    //           placeHolder: 'dateRange',
+    //         },
+    //       ]}
+    //       container="competitor"
+    //     />
+    //     <TopPerformingProperty
+    //       moduleKey="MarketView/Competitor/TopPerformingProperty"
+    //       filters={[
+    //         {
+    //           type: 'property',
+    //           selectKey: 'sakdlfjlasdjf-property',
+    //           placeHolder: 'Property',
+    //         },
+    //       ]}
+    //       container="competitor"
+    //     />
+    //   </React.Fragment>
+
     return (
       <React.Fragment>
         <Slider
@@ -29,68 +112,12 @@ export class Competitor extends React.Component {
               selectKey: 'Mwplt-date',
               placeHolder: 'Date',
             },
-          ]}
-          container="competitor"
-        />
-        <TopVideosOverTime
-          moduleKey="MarketView/Competitor/TopVideosOverTime"
-          title="Top Videos Over Time By Competitor"
-          filters={[
             {
-              type: 'propertyEngagement',
-              selectKey: 'propertyEngagement',
-              placeHolder: 'Engagement by property',
+              type: 'onDay',
+              selectKey: 'Mwplt-onDay',
+              placeHolder: 'Day',
             },
-            {
-              type: 'dateRange',
-              selectKey: 'dateRange',
-              placeHolder: 'dateRange',
-            },
-          ]}
-          references={[
-            {
-              className: 'bg-cool-blue',
-              text: 'Barstool Sports',
-            },
-            {
-              className: 'bg-lighter-purple',
-              text: 'SB Nation',
-            },
-            {
-              className: 'bg-coral-pink',
-              text: 'ESPN',
-            },
-            {
-              className: 'bg-cool-grey',
-              text: 'Scout Media',
-            },
-            {
-              className: 'bg-dusk"',
-              text: 'Fanside',
-            },
-          ]}
-          container="competitor"
-        />
-        <TopSimilarProperties
-          moduleKey="MarketView/Competitor/TopSimilarProperties"
-          title="Top Similar Properties Of Top Videos"
-          filters={[
-            {
-              type: 'dateRange',
-              selectKey: 'dateRange',
-              placeHolder: 'dateRange',
-            },
-          ]}
-          container="competitor"
-        />
-        <TopPerformingProperty
-          moduleKey="MarketView/Competitor/TopPerformingProperty"
-          filters={[
-            {
-              type: 'property',
-              selectKey: 'sakdlfjlasdjf-property',
-              placeHolder: 'Property',
-            },
+
           ]}
           container="competitor"
         />
