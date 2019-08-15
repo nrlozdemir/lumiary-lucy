41fb9acb64c648496209c36958ca0bfa12d7d18b
Jazzy Guo <jazzy@quickframe.com>
2019-08-15 14:34:51 -0400

    feat: set up cv charts

--- src/client/containers/Marketview/views/platform.js
+++ src/client/containers/Marketview/views/platform.js
@@ -9,98 +9,127 @@ import Slider from '../sections/detail/Slider'
 import TopVideosOverTime from '../sections/detail/TopVideosOverTime'
 import TopSimilarProperties from '../sections/detail/TopSimilarProperties'
 import TopPerformingProperty from '../sections/detail/TopPerformingProperty'
+import CVScoresComparison from '../sections/detail/CVScoresComparison'
+
 /* eslint-disable react/prefer-stateless-function */
 export class Platform extends React.Component {
   render() {
+    // return (
+    //   <React.Fragment>
+    //     <Slider
+    //       title="Top Performing Videos By Platform"
+    //       moduleKey="MarketView/Platform/Slider"
+    //       filters={[
+    //         {
+    //           type: 'platformEngagement',
+    //           selectKey: 'mwplttpaap-plateng',
+    //           placeHolder: 'Engagement by Platform',
+    //         },
+    //         {
+    //           type: 'dateRange',
+    //           selectKey: 'dateRange',
+    //           placeHolder: 'dateRange',
+    //         },
+    //       ]}
+    //       container="platform"
+    //     />
+    //     <TopVideosOverTime
+    //       moduleKey="MarketView/Platform/TopVideosOverTime"
+    //       title="Top Videos Over Time By Platform"
+    //       filters={[
+    //         {
+    //           type: 'property',
+    //           selectKey: 'mwplttpaap-property',
+    //           placeHolder: 'Property',
+    //         },
+    //         {
+    //           type: 'metric',
+    //           selectKey: 'mwplttpaap-engagement',
+    //           placeHolder: 'Engagement',
+    //         },
+    //         {
+    //           type: 'dateRange',
+    //           selectKey: 'dateRange',
+    //           placeHolder: 'dateRange',
+    //         },
+    //       ]}
+    //       container="platform"
+    //       references={[
+    //         {
+    //           className: 'bg-cool-blue',
+    //           text: 'Facebook',
+    //         },
+    //         {
+    //           className: 'bg-lighter-purple',
+    //           text: 'Instagram',
+    //         },
+    //         {
+    //           className: 'bg-coral-pink',
+    //           text: 'Twitter',
+    //         },
+    //         {
+    //           className: 'bg-cool-grey',
+    //           text: 'Youtube',
+    //         },
+    //       ]}
+    //       container="platform"
+    //     />
+    //     <TopSimilarProperties
+    //       moduleKey="MarketView/Platform/TopSimilarProperties"
+    //       title="Top Similar Properties Of Top Videos"
+    //       action={this.getSimilarProperties}
+    //       filters={[
+    //         {
+    //           type: 'dateRange',
+    //           selectKey: 'dateRange',
+    //           placeHolder: 'dateRange',
+    //         },
+    //       ]}
+    //       container="platform"
+    //     />
+    //     <TopPerformingProperty
+    //       moduleKey="MarketView/Platform/TopPerformingProperty"
+    //       title="Top Performing Property Across All Platforms"
+    //       filters={[
+    //         {
+    //           type: 'metric',
+    //           selectKey: 'mwplttpaap-engagement',
+    //           placeHolder: 'Engagement',
+    //         },
+    //         {
+    //           type: 'property',
+    //           selectKey: 'mwplttpaap-property',
+    //           placeHolder: 'Property',
+    //         },
+    //       ]}
+    //       container="platform"
+    //     />
+    //   </React.Fragment>
+    // )
+
     return (
       <React.Fragment>
-        <Slider
-          title="Top Performing Videos By Platform"
-          moduleKey="MarketView/Platform/Slider"
-          filters={[
-            {
-              type: 'platformEngagement',
-              selectKey: 'mwplttpaap-plateng',
-              placeHolder: 'Engagement by Platform',
-            },
-            {
-              type: 'dateRange',
-              selectKey: 'dateRange',
-              placeHolder: 'dateRange',
-            },
-          ]}
-          container="platform"
-        />
-        <TopVideosOverTime
-          moduleKey="MarketView/Platform/TopVideosOverTime"
-          title="Top Videos Over Time By Platform"
+        <CVScoresComparison
+          moduleKey="MarketView/Time/CVScoreComparison"
+          title="CV Score By Videos Produced Comparison"
           filters={[
             {
               type: 'property',
-              selectKey: 'mwplttpaap-property',
-              placeHolder: 'Property',
+              selectKey: 'MVPFCV-property',
+              placeHolder: 'property',
             },
             {
-              type: 'metric',
-              selectKey: 'mwplttpaap-engagement',
-              placeHolder: 'Engagement',
+              type: 'platform',
+              selectKey: 'MVPFCV-platform',
+              placeHolder: 'Platform',
             },
-            {
-              type: 'dateRange',
-              selectKey: 'dateRange',
-              placeHolder: 'dateRange',
-            },
-          ]}
-          container="platform"
-          references={[
-            {
-              className: 'bg-cool-blue',
-              text: 'Facebook',
-            },
-            {
-              className: 'bg-lighter-purple',
-              text: 'Instagram',
-            },
-            {
-              className: 'bg-coral-pink',
-              text: 'Twitter',
-            },
-            {
-              className: 'bg-cool-grey',
-              text: 'Youtube',
-            },
-          ]}
-          container="platform"
-        />
-        <TopSimilarProperties
-          moduleKey="MarketView/Platform/TopSimilarProperties"
-          title="Top Similar Properties Of Top Videos"
-          action={this.getSimilarProperties}
-          filters={[
             {
               type: 'dateRange',
-              selectKey: 'dateRange',
-              placeHolder: 'dateRange',
+              selectKey: 'MVPFCV-date',
+              placeHolder: 'Date',
             },
           ]}
-          container="platform"
-        />
-        <TopPerformingProperty
-          moduleKey="MarketView/Platform/TopPerformingProperty"
-          title="Top Performing Property Across All Platforms"
-          filters={[
-            {
-              type: 'metric',
-              selectKey: 'mwplttpaap-engagement',
-              placeHolder: 'Engagement',
-            },
-            {
-              type: 'property',
-              selectKey: 'mwplttpaap-property',
-              placeHolder: 'Property',
-            },
-          ]}
-          container="platform"
+          container="competitor"
         />
       </React.Fragment>
     )
