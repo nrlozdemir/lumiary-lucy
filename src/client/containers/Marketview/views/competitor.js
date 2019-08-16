978f19080d36b9d455f70efe0fa05cc2a2fb97f4
Jazzy Guo <jazzy@quickframe.com>
2019-08-16 10:56:55 -0400

    feat: fk these cv charts on god

--- src/client/containers/Marketview/views/competitor.js
+++ src/client/containers/Marketview/views/competitor.js
@@ -14,116 +14,116 @@ import CVScoresComparison from '../sections/detail/CVScoresComparison'
 /* eslint-disable react/prefer-stateless-function */
 export class Competitor extends React.Component {
   render() {
-    // return (
-    //   <React.Fragment>
-    //     <Slider
-    //       moduleKey="MarketView/Competitor/Slider"
-    //       title="Top Performing Competitor Videos"
-    //       filters={[
-    //         {
-    //           type: 'metric',
-    //           selectKey: 'Mwplt-engagement',
-    //           placeHolder: 'Engagement',
-    //         },
-    //         {
-    //           type: 'dateRange',
-    //           selectKey: 'Mwplt-date',
-    //           placeHolder: 'Date',
-    //         },
-    //       ]}
-    //       container="competitor"
-    //     />
-    //     <TopVideosOverTime
-    //       moduleKey="MarketView/Competitor/TopVideosOverTime"
-    //       title="Top Videos Over Time By Competitor"
-    //       filters={[
-    //         {
-    //           type: 'propertyEngagement',
-    //           selectKey: 'propertyEngagement',
-    //           placeHolder: 'Engagement by property',
-    //         },
-    //         {
-    //           type: 'dateRange',
-    //           selectKey: 'dateRange',
-    //           placeHolder: 'dateRange',
-    //         },
-    //       ]}
-    //       references={[
-    //         {
-    //           className: 'bg-cool-blue',
-    //           text: 'Barstool Sports',
-    //         },
-    //         {
-    //           className: 'bg-lighter-purple',
-    //           text: 'SB Nation',
-    //         },
-    //         {
-    //           className: 'bg-coral-pink',
-    //           text: 'ESPN',
-    //         },
-    //         {
-    //           className: 'bg-cool-grey',
-    //           text: 'Scout Media',
-    //         },
-    //         {
-    //           className: 'bg-dusk"',
-    //           text: 'Fanside',
-    //         },
-    //       ]}
-    //       container="competitor"
-    //     />
-    //     <TopSimilarProperties
-    //       moduleKey="MarketView/Competitor/TopSimilarProperties"
-    //       title="Top Similar Properties Of Top Videos"
-    //       filters={[
-    //         {
-    //           type: 'dateRange',
-    //           selectKey: 'dateRange',
-    //           placeHolder: 'dateRange',
-    //         },
-    //       ]}
-    //       container="competitor"
-    //     />
-    //     <TopPerformingProperty
-    //       moduleKey="MarketView/Competitor/TopPerformingProperty"
-    //       filters={[
-    //         {
-    //           type: 'property',
-    //           selectKey: 'sakdlfjlasdjf-property',
-    //           placeHolder: 'Property',
-    //         },
-    //       ]}
-    //       container="competitor"
-    //     />
-    //   </React.Fragment>
-    // )
-
     return (
       <React.Fragment>
-        <CVScoresComparison
-          moduleKey="MarketView/Competitor/CVScoreComparison"
-          title="CV Score By Videos Produced Comparison"
+        <Slider
+          moduleKey="MarketView/Competitor/Slider"
+          title="Top Performing Competitor Videos"
           filters={[
             {
-              type: 'brand',
-              selectKey: 'MVCompetitor-brand',
-              placeHolder: 'Brand',
-            },
-            {
-              type: 'platform',
-              selectKey: 'MVCompetitor-platform',
-              placeHolder: 'Platform',
+              type: 'metric',
+              selectKey: 'Mwplt-engagement',
+              placeHolder: 'Engagement',
             },
             {
               type: 'dateRange',
-              selectKey: 'MVCompetitor-dateRange',
+              selectKey: 'Mwplt-date',
               placeHolder: 'Date',
+            },
+          ]}
+          container="competitor"
+        />
+        <TopVideosOverTime
+          moduleKey="MarketView/Competitor/TopVideosOverTime"
+          title="Top Videos Over Time By Competitor"
+          filters={[
+            {
+              type: 'propertyEngagement',
+              selectKey: 'propertyEngagement',
+              placeHolder: 'Engagement by property',
+            },
+            {
+              type: 'dateRange',
+              selectKey: 'dateRange',
+              placeHolder: 'dateRange',
+            },
+          ]}
+          references={[
+            {
+              className: 'bg-cool-blue',
+              text: 'Barstool Sports',
+            },
+            {
+              className: 'bg-lighter-purple',
+              text: 'SB Nation',
+            },
+            {
+              className: 'bg-coral-pink',
+              text: 'ESPN',
+            },
+            {
+              className: 'bg-cool-grey',
+              text: 'Scout Media',
+            },
+            {
+              className: 'bg-dusk"',
+              text: 'Fanside',
+            },
+          ]}
+          container="competitor"
+        />
+        <TopSimilarProperties
+          moduleKey="MarketView/Competitor/TopSimilarProperties"
+          title="Top Similar Properties Of Top Videos"
+          filters={[
+            {
+              type: 'dateRange',
+              selectKey: 'dateRange',
+              placeHolder: 'dateRange',
+            },
+          ]}
+          container="competitor"
+        />
+        <TopPerformingProperty
+          moduleKey="MarketView/Competitor/TopPerformingProperty"
+          filters={[
+            {
+              type: 'property',
+              selectKey: 'sakdlfjlasdjf-property',
+              placeHolder: 'Property',
             },
           ]}
           container="competitor"
         />
       </React.Fragment>
     )
+
+    // return (
+    //   <React.Fragment>
+    //     <CVScoresComparison
+    //       moduleKey="MarketView/Competitor/CVScoreComparison"
+    //       title="CV Score By Videos Produced Comparison"
+    //       filters={[
+    //         {
+    //           type: 'brand',
+    //           selectKey: 'MVCompetitor-brand',
+    //           placeHolder: 'Brand',
+    //         },
+    //         {
+    //           type: 'platform',
+    //           selectKey: 'MVCompetitor-platform',
+    //           placeHolder: 'Platform',
+    //         },
+    //         {
+    //           type: 'dateRange',
+    //           selectKey: 'MVCompetitor-dateRange',
+    //           placeHolder: 'Date',
+    //         },
+    //       ]}
+    //       container="competitor"
+    //     />
+    //   </React.Fragment>
+    // )
   }
 }
 
--- src/client/containers/Marketview/views/platform.js
+++ src/client/containers/Marketview/views/platform.js
@@ -14,125 +14,124 @@ import CVScoresComparison from '../sections/detail/CVScoresComparison'
 /* eslint-disable react/prefer-stateless-function */
 export class Platform extends React.Component {
   render() {
-    // return (
-    //   <React.Fragment>
-    //     <Slider
-    //       title="Top Performing Videos By Platform"
-    //       moduleKey="MarketView/Platform/Slider"
-    //       filters={[
-    //         {
-    //           type: 'platformEngagement',
-    //           selectKey: 'mwplttpaap-plateng',
-    //           placeHolder: 'Engagement by Platform',
-    //         },
-    //         {
-    //           type: 'dateRange',
-    //           selectKey: 'dateRange',
-    //           placeHolder: 'dateRange',
-    //         },
-    //       ]}
-    //       container="platform"
-    //     />
-    //     <TopVideosOverTime
-    //       moduleKey="MarketView/Platform/TopVideosOverTime"
-    //       title="Top Videos Over Time By Platform"
-    //       filters={[
-    //         {
-    //           type: 'property',
-    //           selectKey: 'mwplttpaap-property',
-    //           placeHolder: 'Property',
-    //         },
-    //         {
-    //           type: 'metric',
-    //           selectKey: 'mwplttpaap-engagement',
-    //           placeHolder: 'Engagement',
-    //         },
-    //         {
-    //           type: 'dateRange',
-    //           selectKey: 'dateRange',
-    //           placeHolder: 'dateRange',
-    //         },
-    //       ]}
-    //       container="platform"
-    //       references={[
-    //         {
-    //           className: 'bg-cool-blue',
-    //           text: 'Facebook',
-    //         },
-    //         {
-    //           className: 'bg-lighter-purple',
-    //           text: 'Instagram',
-    //         },
-    //         {
-    //           className: 'bg-coral-pink',
-    //           text: 'Twitter',
-    //         },
-    //         {
-    //           className: 'bg-cool-grey',
-    //           text: 'Youtube',
-    //         },
-    //       ]}
-    //       container="platform"
-    //     />
-    //     <TopSimilarProperties
-    //       moduleKey="MarketView/Platform/TopSimilarProperties"
-    //       title="Top Similar Properties Of Top Videos"
-    //       action={this.getSimilarProperties}
-    //       filters={[
-    //         {
-    //           type: 'dateRange',
-    //           selectKey: 'dateRange',
-    //           placeHolder: 'dateRange',
-    //         },
-    //       ]}
-    //       container="platform"
-    //     />
-    //     <TopPerformingProperty
-    //       moduleKey="MarketView/Platform/TopPerformingProperty"
-    //       title="Top Performing Property Across All Platforms"
-    //       filters={[
-    //         {
-    //           type: 'metric',
-    //           selectKey: 'mwplttpaap-engagement',
-    //           placeHolder: 'Engagement',
-    //         },
-    //         {
-    //           type: 'property',
-    //           selectKey: 'mwplttpaap-property',
-    //           placeHolder: 'Property',
-    //         },
-    //       ]}
-    //       container="platform"
-    //     />
-    //   </React.Fragment>
-    // )
-
     return (
       <React.Fragment>
-        <CVScoresComparison
-          moduleKey="MarketView/Time/CVScoreComparison"
-          title="CV Score By Videos Produced Comparison"
+        <Slider
+          title="Top Performing Videos By Platform"
+          moduleKey="MarketView/Platform/Slider"
           filters={[
             {
-              type: 'property',
-              selectKey: 'MVPFCV-property',
-              placeHolder: 'property',
-            },
-            {
-              type: 'platform',
-              selectKey: 'MVPFCV-platform',
-              placeHolder: 'Platform',
+              type: 'platformEngagement',
+              selectKey: 'mwplttpaap-plateng',
+              placeHolder: 'Engagement by Platform',
             },
             {
               type: 'dateRange',
-              selectKey: 'MVPFCV-date',
-              placeHolder: 'Date',
+              selectKey: 'dateRange',
+              placeHolder: 'dateRange',
+            },
+          ]}
+          container="platform"
+        />
+        <TopVideosOverTime
+          moduleKey="MarketView/Platform/TopVideosOverTime"
+          title="Top Videos Over Time By Platform"
+          filters={[
+            {
+              type: 'property',
+              selectKey: 'mwplttpaap-property',
+              placeHolder: 'Property',
+            },
+            {
+              type: 'metric',
+              selectKey: 'mwplttpaap-engagement',
+              placeHolder: 'Engagement',
+            },
+            {
+              type: 'dateRange',
+              selectKey: 'dateRange',
+              placeHolder: 'dateRange',
+            },
+          ]}
+          container="platform"
+          references={[
+            {
+              className: 'bg-cool-blue',
+              text: 'Facebook',
+            },
+            {
+              className: 'bg-lighter-purple',
+              text: 'Instagram',
+            },
+            {
+              className: 'bg-coral-pink',
+              text: 'Twitter',
+            },
+            {
+              className: 'bg-cool-grey',
+              text: 'Youtube',
+            },
+          ]}
+          container="platform"
+        />
+        <TopSimilarProperties
+          moduleKey="MarketView/Platform/TopSimilarProperties"
+          title="Top Similar Properties Of Top Videos"
+          action={this.getSimilarProperties}
+          filters={[
+            {
+              type: 'dateRange',
+              selectKey: 'dateRange',
+              placeHolder: 'dateRange',
+            },
+          ]}
+          container="platform"
+        />
+        <TopPerformingProperty
+          moduleKey="MarketView/Platform/TopPerformingProperty"
+          title="Top Performing Property Across All Platforms"
+          filters={[
+            {
+              type: 'metric',
+              selectKey: 'mwplttpaap-engagement',
+              placeHolder: 'Engagement',
+            },
+            {
+              type: 'property',
+              selectKey: 'mwplttpaap-property',
+              placeHolder: 'Property',
             },
           ]}
           container="platform"
         />
       </React.Fragment>
     )
+    // return (
+    //   <React.Fragment>
+    //     <CVScoresComparison
+    //       moduleKey="MarketView/Time/CVScoreComparison"
+    //       title="CV Score By Videos Produced Comparison"
+    //       filters={[
+    //         {
+    //           type: 'property',
+    //           selectKey: 'MVPFCV-property',
+    //           placeHolder: 'property',
+    //         },
+    //         {
+    //           type: 'platform',
+    //           selectKey: 'MVPFCV-platform',
+    //           placeHolder: 'Platform',
+    //         },
+    //         {
+    //           type: 'dateRange',
+    //           selectKey: 'MVPFCV-date',
+    //           placeHolder: 'Date',
+    //         },
+    //       ]}
+    //       container="platform"
+    //     />
+    //   </React.Fragment>
+    // )
   }
 }
 
