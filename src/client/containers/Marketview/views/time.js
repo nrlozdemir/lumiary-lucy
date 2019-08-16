b9f143f11da57aaa55caf1466533ffca7e645f5b
Jazzy Guo <jazzy@quickframe.com>
2019-08-16 10:56:55 -0400

    feat: fk these cv charts on god

--- src/client/containers/Marketview/views/time.js
+++ src/client/containers/Marketview/views/time.js
@@ -28,84 +28,57 @@ export class Time extends React.Component {
 
   render() {
     const { activeDay } = this.state
-    // return (
-    //   <React.Fragment>
-    //     <DaySelection
-    //       onDayChange={(day) => this.changeActiveDay(day)}
-    //       activeDay={activeDay}
-    //     />
-    //     <Slider
-    //       moduleKey="MarketView/Time/Slider"
-    //       title={`Top Performing ${activeDay
-    //         .charAt(0)
-    //         .toUpperCase()}${activeDay.slice(1)} Videos`}
-    //       activeDay={activeDay}
-    //       filters={[
-    //         {
-    //           type: 'metric',
-    //           selectKey: 'Mwplt-metric',
-    //           placeHolder: 'metric',
-    //         },
-    //         {
-    //           type: 'dateRange',
-    //           selectKey: 'Mwplt-date',
-    //           placeHolder: 'Date',
-    //         },
-    //       ]}
-    //       container="time"
-    //     />
-    //     <TopSimilarProperties
-    //       moduleKey="MarketView/Time/SimilarProperties"
-    //       title="Top Similar Properties Of Top Videos"
-    //       activeDay={activeDay}
-    //       filters={[
-    //         {
-    //           type: 'dateRange',
-    //           selectKey: 'dateRange',
-    //           placeHolder: 'dateRange',
-    //         },
-    //       ]}
-    //       container="time"
-    //     />
-    //     <TopVideosOverTime
-    //       moduleKey="MarketView/Time/TopVideosCard"
-    //       title="Top Performing Property Across All Days Of The"
-    //       filters={[
-    //         {
-    //           type: 'property',
-    //           selectKey: 'property',
-    //           placeHolder: 'property',
-    //         },
-    //         {
-    //           type: 'dateRange',
-    //           selectKey: 'Mwvlt-date',
-    //           placeHolder: 'Date',
-    //         },
-    //       ]}
-    //       container="time"
-    //     />
-    //   </React.Fragment>
-    // )
-
     return (
       <React.Fragment>
-        <CVScoresComparison
-          moduleKey="MarketView/Time/CVScoreComparison"
-          title="CV Score By Videos Produced Comparison"
+        <DaySelection
+          onDayChange={(day) => this.changeActiveDay(day)}
+          activeDay={activeDay}
+        />
+        <Slider
+          moduleKey="MarketView/Time/Slider"
+          title={`Top Performing ${activeDay
+            .charAt(0)
+            .toUpperCase()}${activeDay.slice(1)} Videos`}
+          activeDay={activeDay}
           filters={[
             {
-              type: 'platform',
-              selectKey: 'MVTimeCV-platform',
-              placeHolder: 'Platform',
-            },
-            {
-              type: 'onDay',
-              selectKey: 'MVTimeCV-onDay',
-              placeHolder: 'Day',
+              type: 'metric',
+              selectKey: 'Mwplt-metric',
+              placeHolder: 'metric',
             },
             {
               type: 'dateRange',
-              selectKey: 'MVTimeCV-date',
+              selectKey: 'Mwplt-date',
+              placeHolder: 'Date',
+            },
+          ]}
+          container="time"
+        />
+        <TopSimilarProperties
+          moduleKey="MarketView/Time/SimilarProperties"
+          title="Top Similar Properties Of Top Videos"
+          activeDay={activeDay}
+          filters={[
+            {
+              type: 'dateRange',
+              selectKey: 'dateRange',
+              placeHolder: 'dateRange',
+            },
+          ]}
+          container="time"
+        />
+        <TopVideosOverTime
+          moduleKey="MarketView/Time/TopVideosCard"
+          title="Top Performing Property Across All Days Of The"
+          filters={[
+            {
+              type: 'property',
+              selectKey: 'property',
+              placeHolder: 'property',
+            },
+            {
+              type: 'dateRange',
+              selectKey: 'Mwvlt-date',
               placeHolder: 'Date',
             },
           ]}
@@ -113,6 +86,33 @@ export class Time extends React.Component {
         />
       </React.Fragment>
     )
+
+    // return (
+    //   <React.Fragment>
+    //     <CVScoresComparison
+    //       moduleKey="MarketView/Time/CVScoreComparison"
+    //       title="CV Score By Videos Produced Comparison"
+    //       filters={[
+    //         {
+    //           type: 'platform',
+    //           selectKey: 'MVTimeCV-platform',
+    //           placeHolder: 'Platform',
+    //         },
+    //         {
+    //           type: 'onDay',
+    //           selectKey: 'MVTimeCV-onDay',
+    //           placeHolder: 'Day',
+    //         },
+    //         {
+    //           type: 'dateRange',
+    //           selectKey: 'MVTimeCV-date',
+    //           placeHolder: 'Date',
+    //         },
+    //       ]}
+    //       container="time"
+    //     />
+    //   </React.Fragment>
+    // )
   }
 }
 
