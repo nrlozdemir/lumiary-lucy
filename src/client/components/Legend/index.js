6fa4d0a33e354da6c303d1f37f43ed447406a8f4
Nurullah Ozdemir <nrlozdemir@gmail.com>
2019-08-20 20:19:29 +0300

    reports page legends

--- src/client/components/Legend/index.js
+++ src/client/components/Legend/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import classnames from 'classnames';
+import React from 'react'
+import classnames from 'classnames'
 
-import style from "./style.scss";
+import style from './style.scss'
 
 export default ({ label, color }) => {
   return (
     <div className={style.legend}>
-      <span className={classnames(style.legendDot, style[`dot-${color}`])}></span>
+      <span className={classnames(style.legendDot, style[`dot-${color}`])} />
       <p>{label}</p>
     </div>
   )
