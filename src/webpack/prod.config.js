acffe3f84eda28afe0e8205331880b5302bb8e03
Renato Araujo <renato.araujo@toptal.com>
2019-08-22 15:39:57 +0000

    Fix prod webpack

--- src/webpack/prod.config.js
+++ src/webpack/prod.config.js
@@ -14,40 +14,40 @@ const S3Plugin = require("webpack-s3-plugin");
 
 const s3_region = "us-east-1";
 const s3_bucket = "quickframe-static";
-const s3_path = "bundles";
+const s3_path = "bundles/lumiere";
 const breakpoints = {
-	xsmall: 320,
-	small: 414,
-	medium: 768,
-	large: 1024,
-	xlarge: 1280,
-	slarge: 1440
+  xsmall: 320,
+  small: 414,
+  medium: 768,
+  large: 1024,
+  xlarge: 1280,
+  slarge: 1440
 };
 
 module.exports = {
-	context: __dirname,
-	entry: ["@babel/polyfill", "../client/index"],
+  context: __dirname,
+  entry: ["@babel/polyfill", "../client/index"],
 
-	output: {
-		path: path.resolve(__dirname, "..", "build"),
-		filename: "[name].[chunkhash].js",
-		publicPath: `${static_url}bundles/`
-	},
+  output: {
+    path: path.resolve(__dirname, "..", "build"),
+    filename: "[name].[chunkhash].js",
+    publicPath: `${static_url}bundles/lumiere/`
+  },
 
-	devtool: "source-map",
+  devtool: "source-map",
 
-	plugins: [
-		new webpack.DefinePlugin({
-			"process.env": {
-				NODE_ENV: JSON.stringify("production"),
-				STATIC_URL: JSON.stringify(static_url),
-				MEDIA_URL: JSON.stringify(media_url),
-				BASENAME: JSON.stringify("/"),
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+        STATIC_URL: JSON.stringify(static_url),
+        MEDIA_URL: JSON.stringify(media_url),
+        BASENAME: JSON.stringify("/"),
         API_ROOT: JSON.stringify(process.env.API_ROOT),
         API_VERSION: JSON.stringify(process.env.API_VERSION),
         QAPI_ROOT: JSON.stringify(process.env.QAPI_ROOT),
         QAPI_VERSION: JSON.stringify(process.env.QAPI_VERSION),
-				BREAKPOINTS: breakpoints,
+        BREAKPOINTS: breakpoints,
 
         INSTAGRAM_CLIENT_ID: JSON.stringify(process.env.INSTAGRAM_CLIENT_ID),
         INSTAGRAM_CLIENT_SECRET: JSON.stringify(process.env.INSTAGRAM_CLIENT_SECRET),
@@ -62,171 +62,162 @@ module.exports = {
 
         FACEBOOK_APP_ID: JSON.stringify(process.env.FACEBOOK_APP_ID),
         FACEBOOK_SCOPES: JSON.stringify(process.env.FACEBOOK_SCOPES),
+      }
+    }),
 
-			}
-		}),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor",
+      filename: "vendor.[chunkhash].js",
+      children: false,
+      minChunks(module) {
+        return module.context && module.context.indexOf("node_modules") >= 0;
+      }
+    }),
+    new ExtractTextPlugin("bundle.min.css", { allChunks: false }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new UglifyJsPlugin({
+      test: /\.(js|jsx)$/i,
+      uglifyOptions: {
+        warnings: false, // Suppress uglification warnings
+        mangle: true,
+        compress: {
+          pure_getters: true,
+          conditionals: true,
+          unused: true,
+          comparisons: true,
+          sequences: true,
+          dead_code: true,
+          evaluate: true,
+          if_return: true,
+          join_vars: true
+        },
+        ie8: false,
+        output: {
+          comments: false
+        }
+      },
+      exclude: [/\.min\.js$/gi] // skip pre-minified libs
+    }),
+    new webpack.HashedModuleIdsPlugin(),
 
-		new webpack.optimize.CommonsChunkPlugin({
-			name: "vendor",
-			filename: "vendor.[chunkhash].js",
-			children: false,
-			minChunks(module) {
-				return module.context && module.context.indexOf("node_modules") >= 0;
-			}
-		}),
-		new ExtractTextPlugin("bundle.min.css", { allChunks: false }),
-		new webpack.optimize.ModuleConcatenationPlugin(),
-		new webpack.optimize.AggressiveMergingPlugin(),
-		new webpack.optimize.OccurrenceOrderPlugin(),
-		new UglifyJsPlugin({
-			test: /\.(js|jsx)$/i,
-			uglifyOptions: {
-				warnings: false, // Suppress uglification warnings
-				mangle: true,
-				compress: {
-					pure_getters: true,
-					conditionals: true,
-					unused: true,
-					comparisons: true,
-					sequences: true,
-					dead_code: true,
-					evaluate: true,
-					if_return: true,
-					join_vars: true
-				},
-				ie8: false,
-				output: {
-					comments: false
-				}
-			},
-			exclude: [/\.min\.js$/gi] // skip pre-minified libs
-		}),
-		new webpack.HashedModuleIdsPlugin(),
+    new S3Plugin({
+      include: /.*\.(css|js)/,
+      s3Options: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_DEFAULT_REGION
+      },
 
-		new S3Plugin({
-			include: /.*\.(css|js)/,
-			s3Options: {
-				accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-				secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-				region: process.env.AWS_DEFAULT_REGION
-			},
+      s3UploadOptions: {
+        Bucket: s3_bucket
+      },
+      basePath: s3_path
+    }),
 
-			s3UploadOptions: {
-				Bucket: s3_bucket
-			},
-			basePath: s3_path
-		}),
+    function() {
+      this.plugin("done", statsData => {
+        const stats = statsData.toJson();
+        const tmpl = path.join(__dirname, "../server/views", "index.tmpl");
+        const pug = path.join(__dirname, "../server/views", "index.pug");
 
-		// https://s3.amazonaws.com/quickframe-static/bundles/vendor.8f8c6bc35e82bf656a0d.js
-		// https://s3.amazonaws.com/quickframe-static-dev/bundles/vendor.8f8c6bc35e82bf656a0d.js
+        console.log(stats.assetsByChunkName);
 
-		function() {
-			this.plugin("done", statsData => {
-				const stats = statsData.toJson();
-				const tmpl = path.join(__dirname, "../server/views", "index.tmpl");
-				const pug = path.join(__dirname, "../server/views", "index.pug");
+        if (!stats.errors.length) {
+          var html = fs.readFileSync(tmpl, "utf8");
 
-				console.log(stats.assetsByChunkName);
+          var htmlOutput = html
+            .replace(
+              "bundle.min.js",
+              `${static_url}bundles/lumiere/${stats.assetsByChunkName.main[0]}`
+            )
+            .replace(
+              "vendor.min.js",
+              `${static_url}bundles/lumiere/${stats.assetsByChunkName.vendor}`
+            )
+            .replace(
+              "bundle.min.css",
+              `${static_url}bundles/lumiere/${stats.assetsByChunkName.main[1]}`
+            );
 
-				if (!stats.errors.length) {
-					var html = fs.readFileSync(tmpl, "utf8");
+          fs.writeFileSync(pug, htmlOutput);
+        } else {
+          console.log(stats.errors);
+          throw new Error(stats.errors);
+        }
+      });
+    }
+  ],
 
-					var htmlOutput = html
-						.replace(
-							"bundle.min.js",
-							`${static_url}bundles/${stats.assetsByChunkName.main[0]}`
-						)
-						.replace(
-							"vendor.min.js",
-							`${static_url}bundles/${stats.assetsByChunkName.vendor}`
-						)
-						.replace(
-							"bundle.min.css",
-							`${static_url}bundles/${stats.assetsByChunkName.main[1]}`
-						);
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [{ loader: "babel-loader" }]
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                minimize: true,
+                importLoaders: 2,
+                modules: true,
+                localIdentName: "[path][name]__[local]---[hash:base64:5]"
+              }
+            },
+            {
+              loader: "sass-loader",
+              //query: { sourceMap: false },
+              options: {
+                includePaths: [
+                  neat.includePaths,
+                  bourbon.includePaths,
+                  path.join(__dirname, "../client/scss")
+                ]
+              }
+            },
+            {
+              loader: "@epegzz/sass-vars-loader",
+              options: {
+                vars: {
+                  staticUrl: `"${static_url}"`,
+                  breakpointxsmall: `"${breakpoints.xsmall}"`,
+                  breakpointsmall: `"${breakpoints.small}"`,
+                  breakpointmedium: `"${breakpoints.medium}"`,
+                  breakpointlarge: `"${breakpoints.large}"`,
+                  breakpointxlarge: `"${breakpoints.xlarge}"`,
+                  breakpointslarge: `"${breakpoints.slarge}"`
+                }
+              }
+            },
+            {
+              loader: "sass-resources-loader",
+              options: {
+                resources: [
+                  path.resolve(__dirname, "../client/scss/variables.scss"),
+                  path.resolve(__dirname, "../client/scss/fonts.scss"),
+                  path.resolve(__dirname, "../client/scss/_misc/misc.scss")
+                ]
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2|gif|svg|png|jpeg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [{ loader: "file-loader" }]
+      }
+    ]
+  },
 
-					fs.writeFileSync(pug, htmlOutput);
-				}
-			});
-		}
-	],
-
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules\/(?!(dom7|swiper)\/).*/,
-				use: [{ loader: "babel-loader" }]
-			},
-			{
-				test: /\.(scss|css)$/,
-				use: ExtractTextPlugin.extract({
-					fallback: "style-loader",
-					use: [
-						{
-							loader: "css-loader",
-							options: {
-								minimize: true,
-								importLoaders: 2,
-								modules: true,
-								localIdentName: "[path][name]__[local]---[hash:base64:5]"
-							}
-						},
-						{
-							loader: "sass-loader",
-							//query: { sourceMap: false },
-							options: {
-								includePaths: [
-									neat.includePaths,
-									bourbon.includePaths,
-									path.join(__dirname, "../client/scss")
-								]
-							}
-						},
-						{
-							loader: "@epegzz/sass-vars-loader",
-							options: {
-								vars: {
-									staticUrl: `"${static_url}"`,
-									breakpointxsmall: `"${breakpoints.xsmall}"`,
-									breakpointsmall: `"${breakpoints.small}"`,
-									breakpointmedium: `"${breakpoints.medium}"`,
-									breakpointlarge: `"${breakpoints.large}"`,
-									breakpointxlarge: `"${breakpoints.xlarge}"`,
-									breakpointslarge: `"${breakpoints.slarge}"`
-								}
-							}
-						},
-						{
-							loader: "sass-resources-loader",
-							options: {
-								resources: [
-								  path.resolve(__dirname, '../client/scss/variables.scss'),
-								  // path.resolve(__dirname, '../client/scss/fonts.scss'),
-								  // path.resolve(__dirname, '../client/scss/_misc/misc.scss'),
-								  path.resolve(
-								    __dirname,
-								    '../../node_modules/bourbon/app/assets/stylesheets/_bourbon.scss'
-								  ),
-								  path.resolve(
-								    __dirname,
-								    '../../node_modules/bourbon-neat/core/_neat.scss'
-								  ),
-								],
-							}
-						}
-					]
-				})
-			},
-			{
-				test: /\.(ttf|eot|woff|woff2||gif|svg|png|jpeg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-				use: [{ loader: "file-loader" }]
-			}
-		]
-	},
-
-	resolve: {
-		modules: ["node_modules", "bower_components"],
-		extensions: [".js", ".jsx", ".scss"]
-	}
+  resolve: {
+    modules: ["node_modules", "bower_components"],
+    extensions: [".js", ".jsx", ".scss"]
+  }
 };
