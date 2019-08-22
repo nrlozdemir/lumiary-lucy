d3e266dcb2bccfed75344b703e34dfba8c7477b8
Jazzy Guo <jazzy@quickframe.com>
2019-08-22 14:43:07 -0400

    fix: null check

--- src/client/configureStore.js
+++ src/client/configureStore.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, compose, createStore } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import { routerMiddleware } from 'connected-react-router'
+import history from './history'
 
 import rootReducer from 'Reducers/rootReducer'
 import sagaManager from 'Sagas/sagaManager'
