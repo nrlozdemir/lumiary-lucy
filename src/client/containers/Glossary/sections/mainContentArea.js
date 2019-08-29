4f38d8effc99d2b268579c4e650702f03aefcbe5
Yusuf Bakır <f.yusufbakir@gmail.com>
2019-08-29 15:34:19 +0300

    fixed duplicate, sort and scrollto

--- src/client/containers/Glossary/sections/mainContentArea.js
+++ src/client/containers/Glossary/sections/mainContentArea.js
@@ -8,33 +8,85 @@ class MainContentArea extends Component {
     super(props)
     this.contentRef = []
   }
+
+  componentDidMount() {
+    this.scrollToElement()
+  }
+
   scrollToElement() {
     const { term } = this.props
     const selectedElem = this.contentRef[`content-${term}`]
-    
-    if(!!term && !!selectedElem) {
+
+    if (!!term && !!selectedElem) {
       let topPos = selectedElem.offsetTop
 
       //calculate the letter bar height
       const letterBarElement = document.getElementById('glossaryLetterBar')
-      if(letterBarElement && letterBarElement.clientHeight) {
+      if (letterBarElement && letterBarElement.clientHeight) {
         topPos += letterBarElement.clientHeight
       }
-      window.scrollTo(0, topPos)
+
+      window.scroll({ top: topPos, left: 0, behavior: 'smooth' })
     }
   }
-  componentDidMount() {
-    this.scrollToElement()
+
+  renderContentItems(content, refActive) {
+    const { colors } = this.props.themeContext
+
+    return content.map((item, index) => {
+      return (
+        <div
+          {...(refActive
+            ? {
+                ref: (contentRef) => {
+                  this.contentRef[`content-${item.slug}`] = contentRef
+                },
+              }
+            : {})}
+          id={`content-${item.slug}`}
+          className={style.mainContentItem}
+          key={`mainContentItem-${index}`}
+        >
+          {!!item.term && (
+            <h3 className={style.mainContentItemTerm}>{item.term}</h3>
+          )}
+          {!!item.tags && !!item.tags.length && (
+            <ul className={style.mainContentItemTags}>
+              {item.tags.map((tag, idx) => (
+                <li
+                  key={`tags-${idx}`}
+                  style={{
+                    borderColor: `${colors.moduleBorder}`,
+                  }}
+                  className={style.mainContentItemTag}
+                >
+                  {tag.name}
+                </li>
+              ))}
+            </ul>
+          )}
+          {!!item.text && (
+            <div
+              className={style.mainContentItemContent}
+              dangerouslySetInnerHTML={{ __html: item.text }}
+            />
+          )}
+        </div>
+      )
+    })
   }
-  componentDidUpdate() {
-    this.scrollToElement()
-  }
+
   render() {
     const { content, menu, letter } = this.props
     const { colors } = this.props.themeContext
+
+    ;(letter ? (menu ? content : content[letter]) : content).sort((a, b) =>
+      a.slug > b.slug ? 1 : a.slug < b.slug ? -1 : 0
+    )
+
     return (
       <div
-        id='glossaryMain'
+        id="glossaryMain"
         className={style.glossaryMain}
         style={{
           background: colors.moduleBackground,
@@ -56,76 +108,10 @@ class MainContentArea extends Component {
             </div>
           )}
           {letter
-            ? 
-            [...(menu ? content : content[letter])].map((item, index) => {
-                return (
-                  <div
-                    id={`content-${item.slug}`}
-                    ref={contentRef => this.contentRef[`content-${item.slug}`] = contentRef}
-                    className={style.mainContentItem}
-                    key={`mainContentItem-${index}`}
-                  >
-                    {!!item.term && (
-                      <h3 className={style.mainContentItemTerm}>{item.term}</h3>
-                    )}
-                    {!!item.tags && !!item.tags.length && (
-                      <ul className={style.mainContentItemTags}>
-                        {item.tags.map((tag, idx) => (
-                          <li
-                            key={`tags-${idx}`}
-                            style={{
-                              borderColor: `${colors.moduleBorder}`,
-                            }}
-                            className={style.mainContentItemTag}
-                          >
-                            {tag.name}
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                    {!!item.text && (
-                      <div
-                        className={style.mainContentItemContent}
-                        dangerouslySetInnerHTML={{ __html: item.text }}
-                      />
-                    )}
-                  </div>
-                )
-              })
-            : Object.keys(content).map((key) =>
-                content[key].map((item, index) => (
-                  <div
-                    id={`content-${item.slug}`}
-                    className={style.mainContentItem}
-                    key={`mainContentItem-${index}`}
-                  >
-                    {!!item.term && (
-                      <h3 className={style.mainContentItemTerm}>{item.term}</h3>
-                    )}
-                    {!!item.tags && !!item.tags.length && (
-                      <ul className={style.mainContentItemTags}>
-                        {item.tags.map((tag, idx) => (
-                          <li
-                            key={`tags-${idx}`}
-                            style={{
-                              borderColor: `${colors.moduleBorder}`,
-                            }}
-                            className={style.mainContentItemTag}
-                          >
-                            {tag.name}
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                    {!!item.text && (
-                      <div
-                        className={style.mainContentItemContent}
-                        dangerouslySetInnerHTML={{ __html: item.text }}
-                      />
-                    )}
-                  </div>
-                ))
-              )}
+            ? this.renderContentItems(menu ? content : content[letter], true)
+            : Object.keys(content).map((key) => {
+                this.renderContentItems(content[key], false)
+              })}
         </div>
       </div>
     )
