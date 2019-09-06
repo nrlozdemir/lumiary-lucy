ae41cbe9a5f4127cfb22e9e84e6cf6b90079c458
Nurullah Ozdemir <nrlozdemir@gmail.com>
2019-09-06 14:38:17 +0300

    tooltip.js modification for circle charts

--- src/client/components/ToolTip/index.js
+++ src/client/components/ToolTip/index.js
@@ -3,28 +3,45 @@ import classnames from 'classnames'
 import style from './style.scss'
 import { ThemeContext } from 'ThemeContext/themeContext'
 import ReactTooltip from 'react-tooltip'
+import { CircleChartTemplate } from 'Utils/tooltip'
 
 export default (props) => {
+  console.log('props.template: ', props.template)
+  const tooltipComponents = {
+    CicleChart: CircleChartTemplate,
+  }
+
+  const TooltipTemplate = tooltipComponents[props.template]
+
   return (
     <ThemeContext.Consumer>
       {({ themeContext: { colors } }) => {
-        let className
-        if (!!props.largeTooltip) {
-          className = 'largeTooltip'
-        } else if (!!props.mediumTooltip) {
-          className = 'mediumTooltip'
-        } else if (!!props.smallTooltip) {
-          className = 'smallTooltip'
-        } else if (!!props.xSmallTooltip) {
-          className = 'xSmallTooltip'
+        let className = ''
+        if (!props.template) {
+          if (!!props.largeTooltip) {
+            className = 'largeTooltip'
+          } else if (!!props.mediumTooltip) {
+            className = 'mediumTooltip'
+          } else if (!!props.smallTooltip) {
+            className = 'smallTooltip'
+          } else if (!!props.xSmallTooltip) {
+            className = 'xSmallTooltip'
+          }
         }
 
         return (
           <ReactTooltip
             {...props}
             type={colors.themeType}
-            className={className}
-          />
+            className={classnames(className, {
+              removePadding: !!props.template,
+              [`${props.template}`]: !!props.template,
+            })}
+          >
+            {!!props.template && (
+              <CircleChartTemplate {...props.tooltipProps} />
+            )}
+          </ReactTooltip>
         )
       }}
     </ThemeContext.Consumer>
