30216a7c00d649105ce94224ca07212a300b8362
Ertuğrul Göktuğ Göktaş <ertugrulgoktuggoktas@gmail.com>
2019-09-16 12:17:41 +0300

    feat: Assetlayer tests added

---
+++ __snapshots__/AssetLayer.spec.js
@@ -0,0 +1,185 @@
+exports['Asset Layer Component Rendering matches snapshot 1'] = {
+  "type": "div",
+  "props": {
+    "className": "col-3 ml-0",
+    "style": {
+      "width": "100%",
+      "height": "auto",
+      "boxShadow": "0 2px 6px 0 undefined",
+      "border": "0.9px solid undefined"
+    }
+  },
+  "children": [
+    {
+      "type": "div",
+      "props": {
+        "className": ""
+      },
+      "children": [
+        {
+          "type": "p",
+          "props": {},
+          "children": [
+            "Childiren"
+          ]
+        }
+      ]
+    },
+    {
+      "type": "div",
+      "props": {
+        "className": "",
+        "style": {}
+      },
+      "children": null
+    },
+    {
+      "type": "div",
+      "props": {
+        "className": "",
+        "style": {
+          "border": "0.9px solid undefined"
+        }
+      },
+      "children": [
+        {
+          "type": "div",
+          "props": {},
+          "children": [
+            ""
+          ]
+        },
+        {
+          "type": "div",
+          "props": {},
+          "children": [
+            {
+              "type": "div",
+              "props": {},
+              "children": [
+                {
+                  "type": "p",
+                  "props": {},
+                  "children": [
+                    "Child"
+                  ]
+                }
+              ]
+            },
+            {
+              "type": "div",
+              "props": {},
+              "children": null
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+exports['Asset Layer Component Rendering matches snapshot full props 1'] = {
+  "type": "div",
+  "props": {
+    "className": "col-3 ml-0",
+    "style": {
+      "width": "100%",
+      "height": "auto",
+      "boxShadow": "0 2px 6px 0 undefined",
+      "border": "0.9px solid undefined"
+    }
+  },
+  "children": [
+    {
+      "type": "div",
+      "props": {
+        "className": ""
+      },
+      "children": [
+        {
+          "type": "p",
+          "props": {},
+          "children": [
+            "Childiren"
+          ]
+        }
+      ]
+    },
+    {
+      "type": "div",
+      "props": {
+        "className": "",
+        "style": {}
+      },
+      "children": null
+    },
+    {
+      "type": "div",
+      "props": {
+        "className": "",
+        "style": {
+          "border": "0.9px solid undefined"
+        }
+      },
+      "children": [
+        {
+          "type": "div",
+          "props": {},
+          "children": [
+            {
+              "type": "span",
+              "props": {
+                "className": "icon-Instagram-Bubble"
+              },
+              "children": null
+            },
+            {
+              "type": "p",
+              "props": {},
+              "children": null
+            }
+          ]
+        },
+        {
+          "type": "div",
+          "props": {},
+          "children": [
+            "New Value"
+          ]
+        },
+        {
+          "type": "div",
+          "props": {},
+          "children": [
+            {
+              "type": "div",
+              "props": {},
+              "children": [
+                {
+                  "type": "p",
+                  "props": {},
+                  "children": [
+                    "Child"
+                  ]
+                }
+              ]
+            },
+            {
+              "type": "div",
+              "props": {},
+              "children": [
+                {
+                  "type": "span",
+                  "props": {},
+                  "children": [
+                    "RightValue"
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
