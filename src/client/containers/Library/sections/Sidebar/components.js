4b4ab5ac7d4ff7f3ff67f4127267edaa824260fe
Nurullah Ozdemir <nrlozdemir@gmail.com>
2019-09-19 15:35:48 +0300

    development commit

--- src/client/containers/Library/sections/Sidebar/components.js
+++ src/client/containers/Library/sections/Sidebar/components.js
@@ -6,22 +6,23 @@ import classnames from 'classnames'
 import Range from 'Components/Form/Range'
 import { Field, reduxForm } from 'redux-form'
 import ColorRadioBoxes from 'Components/Form/ColorRadioBoxes/index'
+
+import Button from 'Components/Form/Button'
+
 import style from './style.scss'
 
 const OrderedByComponent = ({ props }) => {
   return (
-    !!selectOptions && (
-      <div className="w-100 mt-48">
-        <Select
-          id="OrderedBy"
-          name="OrderedBy"
-          placeholder="Select One"
-          options={selectOptions.orderByOptions}
-          label="Ordered By"
-          {...props}
-        />
-      </div>
-    )
+    <div className="w-100 mt-48">
+      <Select
+        id="OrderedBy"
+        name="OrderedBy"
+        placeholder="Select One"
+        options={!!selectOptions && selectOptions.orderByOptions}
+        label="Ordered By"
+        {...props}
+      />
+    </div>
   )
 }
 const SocialCheckBoxesComponent = ({ colors, props = false }) => {
@@ -36,34 +37,32 @@ const SocialCheckBoxesComponent = ({ colors, props = false }) => {
 
 const AgeGenderComponent = ({ props = false }) => {
   return (
-    !!selectOptions && (
-      <div className="w-100 d-flex justify-space-between mt-48">
-        <div
-          className={classnames('w-50 ml-0 pr-8', style.sidebarSelectContainer)}
-        >
-          <Select
-            label="Audience Age"
-            name="AgeRange"
-            id="AgeRange"
-            placeholder="Select One"
-            options={selectOptions.audienceAge}
-            {...props}
-          />
-        </div>
-        <div
-          className={classnames('w-50 ml-0 pl-8', style.sidebarSelectContainer)}
-        >
-          <Select
-            options={selectOptions.audienceGender}
-            id="Gender"
-            name="Gender"
-            label="Audience Gender"
-            placeholder="Select One"
-            {...props}
-          />
-        </div>
+    <div className="w-100 d-flex justify-space-between mt-48">
+      <div
+        className={classnames('w-50 ml-0 pr-8', style.sidebarSelectContainer)}
+      >
+        <Select
+          label="Audience Age"
+          name="AgeRange"
+          id="AgeRange"
+          placeholder="Select One"
+          options={!!selectOptions && selectOptions.audienceAge}
+          {...props}
+        />
       </div>
-    )
+      <div
+        className={classnames('w-50 ml-0 pl-8', style.sidebarSelectContainer)}
+      >
+        <Select
+          options={!!selectOptions && selectOptions.audienceGender}
+          id="Gender"
+          name="Gender"
+          label="Audience Gender"
+          placeholder="Select One"
+          {...props}
+        />
+      </div>
+    </div>
   )
 }
 
@@ -85,49 +84,117 @@ const DurationComponent = ({ props = false }) => {
 
 const ColorRadioBoxesComponent = ({ colors, props = false }) => {
   return (
-    !!colors && (
-      <div className="w-100 mt-48">
-        <ColorRadioBoxes
-          colors={colors}
-          {...props}
-        />
-      </div>
-    )
+    <div className="w-100 mt-48">
+      <ColorRadioBoxes colors={!!colors && colors} {...props} />
+    </div>
   )
 }
 
 const VideoFormatComponent = ({ props }) => {
   return (
-    !!selectOptions && (
-      <div className="w-100 mt-48">
-        <Select
-          id="VideoFormat"
-          name="VideoFormat"
-          placeholder="Select One"
-          options={selectOptions.videoFormat}
-          label="Video Format"
-          {...props}
-        />
-      </div>
-    )
+    <div className="w-100 mt-48">
+      <Select
+        id="VideoFormat"
+        name="VideoFormat"
+        placeholder="Select One"
+        options={!!selectOptions && selectOptions.videoFormat}
+        label="Video Format"
+        {...props}
+      />
+    </div>
   )
 }
 
 const AspectRatioComponent = ({ props }) => {
-	return (
-		!!selectOptions && (
-			<div className="w-100 mt-48">
-				<Select
-					id="AspectRatio"
-					name="AspectRatio"
-					placeholder="Select One"
-					options={selectOptions.aspectRatio}
-					label="Aspect Ratio"
-					{...props}
-				/>
-			</div>
-		)
-	)
+  return (
+    <div className="w-100 mt-48">
+      <Select
+        id="AspectRatio"
+        name="AspectRatio"
+        placeholder="Select One"
+        options={!!selectOptions && selectOptions.aspectRatio}
+        label="Aspect Ratio"
+        {...props}
+      />
+    </div>
+  )
+}
+
+const FramesPerSecondComponent = ({ props }) => {
+  return (
+    <div className="w-100 mt-48">
+      <Select
+        id="FramesPerSecond"
+        name="FramesPerSecond"
+        placeholder="Select One"
+        options={!!selectOptions && selectOptions.frameRate}
+        label="Frame Rate"
+        {...props}
+      />
+    </div>
+  )
+}
+
+const ResolutionComponent = ({ props }) => {
+  return (
+    <div className="w-100 mt-48">
+      <Select
+        id="Resolution"
+        name="Resolution"
+        placeholder="Select One"
+        options={!!selectOptions && selectOptions.resolution}
+        label="Resolution"
+        {...props}
+      />
+    </div>
+  )
+}
+
+const PacingComponent = ({ props }) => {
+  return (
+    <div className="w-100 mt-48">
+      <Select
+        id="Pacing"
+        name="Pacing"
+        placeholder="Select One"
+        options={!!selectOptions && selectOptions.pacing}
+        label="Pacing"
+        {...props}
+      />
+    </div>
+  )
+}
+
+const ButtonsComponent = ({ props, colors }) => {
+  return (
+    <React.Fragment>
+      <div className="w-100 d-flex align-items-center justify-content-center">
+        <Button
+          customClass={classnames('mt-48', style.sidebarApplyButton, {
+            [style.formChange]: !!props && !!props.formChange && props.formChange === true,
+          })}
+          /*onClick={() => formChange === true && setSidebarVisible(false)}
+          textColor={
+            !formChange
+              ? colors.customSelectActiveBorder
+              : colors.searchInputActiveColor
+          }*/
+          buttonText="Apply Filters"
+        />
+      </div>
+      <div className="w-100 d-flex align-items-center justify-content-center">
+        <span
+          className={style.cancel}
+          /*onClick={() => setSidebarVisible(false)}*/
+          style={{
+            color: colors.filterText,
+          }}
+        >
+          Cancel
+        </span>
+      </div>
+    </React.Fragment>
+  )
 }
 
 export {
@@ -136,6 +203,10 @@ export {
   AgeGenderComponent,
   DurationComponent,
   ColorRadioBoxesComponent,
-	VideoFormatComponent,
-	AspectRatioComponent,
+  VideoFormatComponent,
+  AspectRatioComponent,
+  FramesPerSecondComponent,
+  ResolutionComponent,
+  PacingComponent,
+  ButtonsComponent,
 }
--- src/client/containers/Library/sections/Sidebar/index.js
+++ src/client/containers/Library/sections/Sidebar/index.js
@@ -1,20 +1,85 @@
 import React from 'react'
 import classnames from 'classnames'
 import PropTypes from 'prop-types'
-import { Field, reduxForm } from 'redux-form'
-import Select from 'Components/Form/SelectField'
-import Button from 'Components/Form/Button'
-import { selectOptions } from './options'
+import { reduxForm } from 'redux-form'
 import {
   OrderedByComponent,
   SocialCheckBoxesComponent,
   AgeGenderComponent,
   DurationComponent,
-	ColorRadioBoxesComponent,
-	VideoFormatComponent,
-	AspectRatioComponent,
+  ColorRadioBoxesComponent,
+  VideoFormatComponent,
+  AspectRatioComponent,
+  FramesPerSecondComponent,
+  ResolutionComponent,
+  PacingComponent,
+  ButtonsComponent,
 } from './components'
 import style from './style.scss'
+
+const FormWrapper = (props) => {
+	console.log("props", props)
+  const {
+		children,
+    colors,
+    fixedHeader,
+    handleSubmit,
+    sidebarVisible,
+		setSidebarVisible,
+		formChange,
+  } = props
+
+  return (
+    <form /* onSubmit={(e) => handleSubmit(e)} */>
+      <div
+        className={classnames(style.sidebar, {
+          [style.sidebarVisible]: sidebarVisible,
+          [style.fixed]: fixedHeader,
+        })}
+        style={{ backgroundColor: colors.duskBackground }}
+      >
+        <div
+          className={classnames(style.sidebarMain, {
+            [style.fixed]: fixedHeader,
+          })}
+        >
+          <div
+            className={classnames(style.sidebarHeader, {
+              [style.fixed]: fixedHeader,
+            })}
+            style={{ backgroundColor: colors.filterHeader }}
+          >
+            <p className={style.text}>
+              <span
+                className="float-left"
+                style={{
+                  color: colors.filterHeaderText,
+                }}
+              >
+                Filter Videos
+              </span>
+              <span
+                className="float-right color-cool-blue"
+                /* onClick={this.resetFormValues} */
+              >
+                Reset
+              </span>
+            </p>
+            <div className="clearFix" />
+          </div>
+          <div
+            className={classnames(style.sidebarContent, 'ph-32', {
+              [style.fixed]: fixedHeader,
+              'mt-80': fixedHeader,
+            })}
+          >
+            {children}
+          </div>
+        </div>
+      </div>
+    </form>
+  )
+}
 
 class Sidebar extends React.Component {
   constructor(props) {
@@ -25,7 +90,7 @@ class Sidebar extends React.Component {
     }
 
     this.resetFormValues = this.resetFormValues.bind(this)
-    this.formChange = this.formChange.bind(this)
+    this.formChanged = this.formChanged.bind(this)
   }
 
   // componentDidUpdate(prevProps) {
@@ -41,140 +106,31 @@ class Sidebar extends React.Component {
     changeFilter()
   }
 
-  formChange() {
+  formChanged() {
     this.setState({
       formChange: true,
     })
   }
 
   render() {
-    const { formChange } = this.state
-    const {
-      colors,
-      fixedHeader,
-      handleSubmit,
-      sidebarVisible,
-      setSidebarVisible,
-    } = this.props
-
     return (
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <div
-          className={classnames(style.sidebar, {
-            [style.sidebarVisible]: sidebarVisible,
-            [style.fixed]: fixedHeader,
-          })}
-          style={{ backgroundColor: colors.duskBackground }}
-        >
-          <div
-            className={classnames(style.sidebarMain, {
-              [style.fixed]: fixedHeader,
-            })}
-          >
-            <div
-              className={classnames(style.sidebarHeader, {
-                [style.fixed]: fixedHeader,
-              })}
-              style={{ backgroundColor: colors.filterHeader }}
-            >
-              <p className={style.text}>
-                <span
-                  className="float-left"
-                  style={{
-                    color: colors.filterHeaderText,
-                  }}
-                >
-                  Filter Videos
-                </span>
-                <span
-                  className="float-right color-cool-blue"
-                  onClick={this.resetFormValues}
-                >
-                  Reset
-                </span>
-              </p>
-              <div className="clearFix" />
-            </div>
-            <div
-              className={classnames(style.sidebarContent, 'ph-32', {
-                [style.fixed]: fixedHeader,
-                'mt-80': fixedHeader,
-              })}
-            >
-							{!!colors && (
-								<React.Fragment>
-									<OrderedByComponent />
-									<SocialCheckBoxesComponent colors={colors} />
-									<AgeGenderComponent />
-									<DurationComponent />
-									<ColorRadioBoxesComponent colors={colors} />
-									<VideoFormatComponent />
-									<AspectRatioComponent />
-								</React.Fragment>
-							)}
-
-
-              <div className="w-100 mt-48">
-                <Select
-                  id="FramesPerSecond"
-                  name="FramesPerSecond"
-                  placeholder="Select One"
-                  options={!!selectOptions && selectOptions.frameRate}
-                  label="Frame Rate"
-                  onChange={this.formChange}
-                />
-              </div>
-              <div className="w-100 mt-48">
-                <Select
-                  id="Resolution"
-                  name="Resolution"
-                  placeholder="Select One"
-                  options={!!selectOptions && selectOptions.resolution}
-                  label="Resolution"
-                  onChange={this.formChange}
-                />
-              </div>
-              <div className="w-100 mt-48">
-                <Select
-                  id="Pacing"
-                  name="Pacing"
-                  placeholder="Select One"
-                  options={!!selectOptions && selectOptions.pacing}
-                  label="Pacing"
-                  onChange={this.formChange}
-                />
-              </div>
-              <div className="w-100 d-flex align-items-center justify-content-center">
-                <Button
-                  customClass={classnames('mt-48', style.sidebarApplyButton, {
-                    [style.formChange]: formChange === true,
-                  })}
-                  onClick={() =>
-                    formChange === true && setSidebarVisible(false)
-                  }
-                  textColor={
-                    !formChange
-                      ? colors.customSelectActiveBorder
-                      : colors.searchInputActiveColor
-                  }
-                  buttonText="Apply Filters"
-                />
-              </div>
-              <div className="w-100 d-flex align-items-center justify-content-center">
-                <span
-                  className={style.cancel}
-                  onClick={() => setSidebarVisible(false)}
-                  style={{
-                    color: colors.filterText,
-                  }}
-                >
-                  Cancel
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </form>
+      <FormWrapper formChange={this.state.formChange} {...this.props}>
+        {!!this.props.colors && (
+          <React.Fragment>
+            <OrderedByComponent />
+            <SocialCheckBoxesComponent colors={this.props.colors} />
+            <AgeGenderComponent />
+            <DurationComponent />
+            <ColorRadioBoxesComponent colors={this.props.colors} />
+            <VideoFormatComponent />
+            <AspectRatioComponent />
+            <FramesPerSecondComponent />
+            <ResolutionComponent />
+            <PacingComponent />
+            <ButtonsComponent formChange={this.state.formChange} colors={this.props.colors} />
+          </React.Fragment>
+        )}
+      </FormWrapper>
     )
   }
 }
