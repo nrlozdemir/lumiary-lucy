8976c482f0651e59a4bab1130df95816c78f61f2
Jazzy Guo <jazzy@quickframe.com>
2019-09-20 12:51:18 -0400

    feat: action tester

---
+++ src/client/reducers/__tests__/audience.spec.js
@@ -0,0 +1,17 @@
+import expect from 'expect'
+import {
+  types,
+  actions,
+  audienceReducer,
+  initialState,
+} from 'Reducers/audience'
+import { actionTester } from './utils'
+
+describe('Audience Reducer', () => {
+  describe('Actions', () => {
+
+    actionTester(actions)
+  })
+
+  describe('Reducer', () => {})
+})
--- src/client/reducers/__tests__/utils.js
+++ src/client/reducers/__tests__/utils.js
@@ -9,14 +9,26 @@ import expect from 'expect'
     paramName: string,
   }
  */
-export const actionTester = (actions = []) => {
-  return actions.forEach(({ type, payload = {}, description = '', action, paramName='payload' }) =>
-    it(`it should create an action to ${description}`, () => {
-      const expectedAction = {
-        type,
-        [paramName]: payload,
-      }
-      expect(action(payload)).toEqual(expectedAction)
-    })
+export const actionTester = (actions = {}) => {
+  const actionsToTest = Object.keys(actions).map((actionFunc) => {
+    const { type, payload, error, data } = actions[actionFunc]({})
+    return {
+      type,
+      payload: {},
+      description: actionFunc,
+      action: actions[actionFunc],
+      paramName: payload ? 'payload' : data ? 'data' : error ? 'error' : 'none',
+    }
+  })
+  
+  return actionsToTest.forEach(
+    ({ type, payload = {}, description = '', action, paramName = 'payload' }) =>
+      it(`it should create an action to ${description}`, () => {
+        const expectedAction = {
+          type,
+          ...(paramName === 'none' ? {} : { [paramName]: payload }),
+        }
+        expect(action(payload)).toEqual(expectedAction)
+      })
   )
 }
